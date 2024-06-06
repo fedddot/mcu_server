82d209bd793d68540bca05307bde81909897bd9e
fedddot <idanrommm@gmail.com>
2024-06-06 18:56:51 +0300

    files added

---
+++ automation/run_ut_tests.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import subprocess
+
+if len(sys.argv) < 2:
+    raise Exception("invalid arguments received")
+
+cnc_path = sys.argv[1]
+build_path = os.path.join(cnc_path, "build")
+
+if not os.path.isdir(cnc_path): 
+    raise Exception("invalid cnc directory path")
+
+if not os.path.isdir(build_path): 
+    os.makedirs(build_path)
+
+subprocess.run(["cmake", ".."], cwd=build_path).check_returncode()
+
+uts = [
+    "data_tests",
+    "engine_tests",
+    "parse_tests",
+    "server_tests",
+    "utils_tests"
+]
+
+def find_executable(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+    raise Exception("exec file " + name + " was not found")
+
+for ut in uts:
+    print("building test " + ut)
+    subprocess.run(["make", ut], cwd=build_path).check_returncode()
+    print("running test " + ut)
+    subprocess.run([find_executable(ut, build_path)], cwd=build_path).check_returncode()
