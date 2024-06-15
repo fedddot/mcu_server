5113282c80b87cd14a31b4ee4437ec05a4b39b97
fedddot <idanrommm@gmail.com>
2024-06-15 09:45:12 +0300

    updated

--- include/connection.hpp
+++ include/connection.hpp
@@ -2,12 +2,12 @@
 #define	CONNECTION_HPP
 
 namespace mcu_control {
-	template <typename Tdata, typename Ttime>
+	template <typename Tdata>
 	class Connection {
 	public:
 		virtual ~Connection() noexcept = default;
 		virtual void send_data(const Tdata& data) const = 0;
-		virtual bool poll_data(const Ttime& delay) const = 0;
+		virtual bool is_readable() const = 0;
 		virtual Tdata read_data() const = 0;
 	};
 }
--- utils/include/functional_connection.hpp
+++ utils/include/functional_connection.hpp
@@ -6,44 +6,44 @@
 #include "connection.hpp"
 
 namespace mcu_control_utl {
-	template <typename Tdata, typename Ttime>
-	class FunctionalConnection: public mcu_control::Connection<Tdata, Ttime> {
+	template <typename Tdata>
+	class FunctionalConnection: public mcu_control::Connection<Tdata> {
 	public:
 		using SendAction = std::function<void(const Tdata&)>;
-		using PollAction = std::function<bool(const Ttime&)>;
+		using IsReadableAction = std::function<bool()>;
 		using ReadAction = std::function<Tdata()>;
 
-		FunctionalConnection(const SendAction& send_action, const PollAction& poll_action, const ReadAction& read_action);
+		FunctionalConnection(const SendAction& send_action, const IsReadableAction& is_readable_action, const ReadAction& read_action);
 		FunctionalConnection(const FunctionalConnection& other) = delete;
 		FunctionalConnection& operator=(const FunctionalConnection& other) = delete;
 
 		void send_data(const Tdata& data) const override;
-		bool poll_data(const Ttime& delay) const override;
+		bool is_readable() const override;
 		Tdata read_data() const override;
 
 	private:
 		SendAction m_send_action;
-		PollAction m_poll_action;
+		IsReadableAction m_is_readable_action;
 		ReadAction m_read_action;
 	};
 
-	template <typename Tdata, typename Ttime>
-	inline FunctionalConnection<Tdata, Ttime >::FunctionalConnection(const SendAction& send_action, const PollAction& poll_action, const ReadAction& read_action): m_send_action(send_action), m_poll_action(poll_action), m_read_action(read_action) {
+	template <typename Tdata>
+	inline FunctionalConnection<Tdata>::FunctionalConnection(const SendAction& send_action, const IsReadableAction& is_readable_action, const ReadAction& read_action): m_send_action(send_action), m_is_readable_action(is_readable_action), m_read_action(read_action) {
 
 	}
 
-	template <typename Tdata, typename Ttime>
-	inline void FunctionalConnection<Tdata, Ttime >::send_data(const Tdata& data) const {
+	template <typename Tdata>
+	inline void FunctionalConnection<Tdata>::send_data(const Tdata& data) const {
 		m_send_action(data);
 	}
 
-	template <typename Tdata, typename Ttime>
-	inline bool FunctionalConnection<Tdata, Ttime >::poll_data(const Ttime& delay) const {
-		return m_poll_action(delay);
+	template <typename Tdata>
+	inline bool FunctionalConnection<Tdata>::is_readable() const {
+		return m_is_readable_action();
 	}
 
-	template <typename Tdata, typename Ttime>
-	inline Tdata FunctionalConnection<Tdata, Ttime >::read_data() const {
+	template <typename Tdata>
+	inline Tdata FunctionalConnection<Tdata>::read_data() const {
 		return m_read_action();
 	}
 }
