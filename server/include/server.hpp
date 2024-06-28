358e73b25016396e037feee5d4b9665291a7e76f
fedddot <idanrommm@gmail.com>
2024-06-28 18:08:20 +0300

    server updated

--- server/include/server.hpp
+++ server/include/server.hpp
@@ -1,90 +1,75 @@
 #ifndef	SERVER_HPP
 #define	SERVER_HPP
 
-#include <exception>
 #include <memory>
-#include <string>
-
-#include "data.hpp"
-#include "integer.hpp"
-#include "object.hpp"
-#include "string.hpp"
+#include <stdexcept>
 
 #include "engine.hpp"
-#include "functional_creator.hpp"
-
-#include "parser.hpp"
-#include "functional_parser.hpp"
-
-#include "gpio.hpp"
-#include "inventory.hpp"
-
-#include "server_tasks.hpp"
-#include "server_factory.hpp"
+#include "connection.hpp"
+#include "server_task_engine.hpp"
 
 namespace server {
 	template <typename Traw_data, typename Tgpio_id>
 	class Server {
 	public:
-		using RawDataParser = engine::Parser<engine::Data *(const Traw_data&)>;
-		using RawDataSerializer = engine::Serializer<Traw_data(const engine::Data&)>;
+		using TaskIdParser = typename ServerTaskEngine<Traw_data, Tgpio_id>::TaskIdParser;
+		using FailureReportCreator = typename ServerTaskEngine<Traw_data, Tgpio_id>::EngineFailureReportCreator;
+		using RawDataParser = typename ServerTaskEngine<Traw_data, Tgpio_id>::EngineRawDataParser;
+		using ReportSerializer = typename ServerTaskEngine<Traw_data, Tgpio_id>::EngineReportSerializer;
+	
+		using GpioIdParser = typename ServerTaskEngine<Traw_data, Tgpio_id>::GpioIdParser;
+		using GpioDirParser = typename ServerTaskEngine<Traw_data, Tgpio_id>::GpioDirParser;
+		using GpioStateParser = typename ServerTaskEngine<Traw_data, Tgpio_id>::GpioStateParser;
+		
+		using GpioCreator = typename ServerTaskEngine<Traw_data, Tgpio_id>::GpioCreator;
 
-		using GpioCreator = engine::Creator<Gpio *(const Tgpio_id&, const Gpio::Direction&)>;
-		using GpioIdParser = engine::Parser<Tgpio_id(const engine::Data&)>;
 
-		Server(const RawDataParser& raw_data_parser, const RawDataSerializer& raw_data_serializer, const GpioCreator& gpio_creator, const GpioIdParser& gpio_id_parser);
+		Server(mcu_control::Connection<Traw_data> *connection, const TaskIdParser& task_id_parser, const FailureReportCreator& failure_report_creator, const RawDataParser& raw_data_parser, const ReportSerializer& report_serializer, const GpioIdParser& gpio_id_parser, const GpioDirParser& gpio_dir_parser, const GpioStateParser& gpio_state_parser, const GpioCreator& gpio_creator);
 		Server(const Server& other) = delete;
 		Server& operator=(const Server& other) = delete;
 	
-		Traw_data run(const Traw_data& data);
+		void run();
+		bool is_running() const;
+		void stop();
 	private:
-		Inventory<Tgpio_id, Gpio> m_gpio_inventory;
-		
+		mcu_control::Connection<Traw_data> *m_connection;
 		std::unique_ptr<engine::Engine<Traw_data, Traw_data>> m_engine;
+		bool m_is_running;
 	};
 
 	template <typename Traw_data, typename Tgpio_id>
-	inline Server<Traw_data, Tgpio_id>::Server(const RawDataParser& raw_data_parser, const RawDataSerializer& raw_data_serializer, const GpioCreator& gpio_creator, const GpioIdParser& gpio_id_parser): m_engine(nullptr) {
-		ServerFactory<Tgpio_id> factory(
-			engine_utl::FunctionalParser<TaskId(const engine::Data&)>(
-				[](const engine::Data& data)-> TaskId {
-					return static_cast<TaskId>(engine::Data::cast<engine::Integer>(engine::Data::cast<engine::Object>(data).access("task_id")).get());
-				}
-			),
-			&m_gpio_inventory,
-			gpio_creator,
-			gpio_id_parser,
-			engine_utl::FunctionalParser<Gpio::Direction(const engine::Data&)>(
-				[](const engine::Data& data)-> Gpio::Direction {
-					return static_cast<Gpio::Direction>(engine::Data::cast<engine::Integer>(engine::Data::cast<engine::Object>(data).access("gpio_dir")).get());
-				}
-			),
-			engine_utl::FunctionalParser<Gpio::State(const engine::Data&)>(
-				[](const engine::Data& data)-> Gpio::State {
-					return static_cast<Gpio::State>(engine::Data::cast<engine::Integer>(engine::Data::cast<engine::Object>(data).access("gpio_state")).get());
-				}
-			)
-		);
+	inline Server<Traw_data, Tgpio_id>::Server(mcu_control::Connection<Traw_data> *connection, const TaskIdParser& task_id_parser, const FailureReportCreator& failure_report_creator, const RawDataParser& raw_data_parser, const ReportSerializer& report_serializer, const GpioIdParser& gpio_id_parser, const GpioDirParser& gpio_dir_parser, const GpioStateParser& gpio_state_parser, const GpioCreator& gpio_creator): m_connection(connection), m_is_running(false) {
+		if (!m_connection) {
+			throw std::invalid_argument("invalid connection ptr received");
+		}
 		m_engine = std::unique_ptr<engine::Engine<Traw_data, Traw_data>>(
-			new engine::Engine<Traw_data, Traw_data>(
-				factory,
-				engine_utl::FunctionalCreator<engine::Data *(const std::exception&)>(
-					[](const std::exception& e) -> engine::Data * {
-						engine::Object report;
-						report.add("result", engine::Integer(-1));
-						report.add("what", engine::String(std::string(e.what())));
-						return report.clone();
-					}
-				),
-				raw_data_parser,
-				raw_data_serializer
-			)
+			new ServerTaskEngine<Traw_data, Tgpio_id>(task_id_parser, failure_report_creator, raw_data_parser, report_serializer, gpio_id_parser, gpio_dir_parser, gpio_state_parser, gpio_creator)
 		);
 	}
 
 	template <typename Traw_data, typename Tgpio_id>
-	inline Traw_data Server<Traw_data, Tgpio_id>::run(const Traw_data& data) {
-		return m_engine->run_task(data);
+	inline void Server<Traw_data, Tgpio_id>::run() {
+		if (m_is_running) {
+			throw std::runtime_error("server is already running");
+		}
+		m_is_running = true;
+		while (m_is_running) {
+			if (!m_connection->is_readable()) {
+				continue;
+			}
+			Traw_data data = m_connection->read_data();
+			m_connection->send_data(m_engine->run_task(data));
+		}
+	}
+
+	template <typename Traw_data, typename Tgpio_id>
+	inline bool Server<Traw_data, Tgpio_id>::is_running() const {
+		return m_is_running;
+	}
+
+	template <typename Traw_data, typename Tgpio_id>
+	inline void Server<Traw_data, Tgpio_id>::stop() {
+		m_is_running = false;
 	}
 }
 
