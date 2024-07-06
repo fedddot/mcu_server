4bba59ccd4ec77259638612e0aae33e54d620457
fedddot <idanrommm@gmail.com>
2024-07-06 18:30:47 +0300

    automation script added

---
+++ automation/run_ut_tests.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import subprocess
+
+if len(sys.argv) < 2:
+    raise Exception("invalid arguments received")
+
+root_path = sys.argv[1]
+build_path = os.path.join(root_path, "build")
+
+if not os.path.isdir(root_path): 
+    raise Exception("invalid root directory path")
+
+if not os.path.isdir(build_path): 
+    os.makedirs(build_path)
+
+subprocess.run(["cmake", ".."], cwd=build_path).check_returncode()
+
+uts = [
+    # "factory_tests",
+    # "mcu_task_engine_tests",
+    # "mcu_utils_tests"
+]
+
+def find_executable(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+    raise Exception("exec file " + name + " was not found")
+
+for ut in uts:
+    print("building test " + ut)
+    subprocess.run(["make", ut], cwd=build_path).check_returncode()
+    print("running test " + ut)
+    subprocess.run([find_executable(ut, build_path)], cwd=build_path).check_returncode()
