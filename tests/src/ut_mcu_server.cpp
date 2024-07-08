43604216399b51551fe4e50a10130db68ac62b49
fedddot <idanrommm@gmail.com>
2024-07-08 19:57:19 +0300

    test added

--- tests/src/ut_mcu_server.cpp
+++ tests/src/ut_mcu_server.cpp
@@ -1,9 +1,19 @@
 #include "gtest/gtest.h"
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "custom_sender.hpp"
+#include "data.hpp"
+#include "integer.hpp"
 #include "mcu_server.hpp"
 #include "mcu_server_fixture.hpp"
+#include "object.hpp"
 
+using namespace engine;
+using namespace mcu_task_engine;
 using namespace mcu_server;
 using namespace mcu_server_uts;
 using namespace mcu_server_utl;
@@ -37,4 +47,35 @@ TEST_F(McuServerFixture, ctor_dtor_sanity) {
 	ASSERT_NO_THROW(delete instance_ptr);
 
 	instance_ptr = nullptr;
+}
+
+TEST_F(McuServerFixture, feed_sanity) {
+	// GIVEN
+	CustomSender<RawData> sender(
+		[this](const RawData& data) {
+			std::cout << "received data from server: " << data << std::endl;
+			std::unique_ptr<Data> report(parser().parse(data));
+			ASSERT_EQ(0, Data::cast<Integer>(Data::cast<Object>(*report).access("result")).get());
+		}
+	);
+
+	const std::vector<std::pair<std::string, std::string>> test_cases {
+		{"create_gpio", serializer().serialize(create_gpio_data(1, Gpio::Direction::OUT))}
+	};
+
+	// WHEN
+	TestMcuServer instance(
+		mcu_engine(),
+		&sender,
+		receiver(),
+		parser(),
+		serializer()
+	);
+
+	// THEN
+	for (auto test_case: test_cases) {
+		std::cout << "running TC: " << test_case.first << std::endl;
+		std::cout << "feeding data: " << test_case.second << std::endl;
+		ASSERT_NO_THROW(instance.feed(head() + test_case.second + tail()));
+	}
 }
