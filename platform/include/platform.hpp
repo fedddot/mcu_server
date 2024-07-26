2636d8f4718ffddde10bf4e44344b6374bb8c7f5
fedddot <idanrommm@gmail.com>
2024-07-26 16:12:32 +0300

    platform updated

--- platform/include/platform.hpp
+++ platform/include/platform.hpp
@@ -2,14 +2,23 @@
 #define	PLATFORM_HPP
 
 #include "gpio.hpp"
+#include "inventory.hpp"
+#include "message_receiver.hpp"
+#include "message_sender.hpp"
+
 namespace mcu_platform {
 	template <typename Tdata, typename Tgpio_id>
 	class Platform {
 	public:
 		virtual ~Platform() noexcept = default;
+		
+		virtual MessageReceiver<Tdata> *message_receiver() const = 0;
+		virtual MessageSender<Tdata> *message_sender() const = 0;
+
 		virtual void delay(unsigned int timeout_ms) const = 0;
 		virtual Gpio *create_gpio(const Tgpio_id& id, const Gpio::Direction& dir) const = 0;
-		
+
+		virtual Inventory<Tgpio_id, Gpio> *gpio_inventory() const = 0;
 	};
 }
 #endif // PLATFORM_HPP
