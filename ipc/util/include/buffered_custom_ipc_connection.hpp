7469fe952058b58838c739dd1884e528daca2c60
fedddot <idanrommm@gmail.com>
2024-08-01 19:26:58 +0300

    tests added

--- ipc/util/include/buffered_custom_ipc_connection.hpp
+++ ipc/util/include/buffered_custom_ipc_connection.hpp
@@ -1,13 +1,18 @@
 #ifndef	BUFFERED_CUSTOM_IPC_CONNECTION_HPP
 #define	BUFFERED_CUSTOM_IPC_CONNECTION_HPP
 
-#include "buffered_ipc_connection.hpp"
+#include "ipc_connection.hpp"
+#include <cstddef>
+
 namespace mcu_ipc_utl {
 	template <typename Tdata>
 	class BufferedCustomIpcConnection: public mcu_ipc::IpcConnection<Tdata> {
 	public:
-		virtual ~IpcConnection() noexcept = default;
-		virtual bool readable() const = 0;
+		BufferedCustomIpcConnection(const Tdata& head, const Tdata& tail, const std::size_t& max_buff_size);
+		BufferedCustomIpcConnection(const BufferedCustomIpcConnection& other);
+		BufferedCustomIpcConnection& operator=(const BufferedCustomIpcConnection& other) = delete;
+		
+		virtual bool readable() const;
 		virtual Tdata read() = 0;
 		virtual void send(const Tdata& data) const = 0;
 	};
