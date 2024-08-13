cbb5cd4146749347fb13fe169e7cd5740adae48c
fedddot <idanrommm@gmail.com>
2024-08-13 19:24:53 +0300

    steps task updated

--- stepper_motor_tasks_factory/tasks/include/steps_task.hpp
+++ stepper_motor_tasks_factory/tasks/include/steps_task.hpp
@@ -1,50 +1,61 @@
-#ifndef	SET_GPIO_TASK_HPP
-#define	SET_GPIO_TASK_HPP
+#ifndef	STEPS_TASK_HPP
+#define	STEPS_TASK_HPP
 
 #include <memory>
-#include <stdexcept>
 
 #include "creator.hpp"
 #include "data.hpp"
-#include "gpio.hpp"
-#include "gpo.hpp"
 #include "inventory.hpp"
+#include "stepper_motor.hpp"
 #include "task.hpp"
 
 namespace mcu_factory {
-	template <typename Tgpio_id>
-	class SetGpioTask: public mcu_server::Task<mcu_server::Data *(void)> {
+	template <typename Tstepper_id, typename Tgpio_id>
+	class StepsTask: public mcu_server::Task<mcu_server::Data *(void)> {
 	public:
-		using GpioInventory = mcu_platform::Inventory<Tgpio_id, mcu_platform::Gpio>;
-		using ReportCreator = mcu_server::Creator<mcu_server::Data *(int)>;
+		using StepperMotorInventory = mcu_platform::Inventory<Tstepper_id, mcu_platform::StepperMotor<Tgpio_id>>;
+		using Direction = typename mcu_platform::StepperMotor<Tgpio_id>::Direction;
+		using ReportCreator = typename mcu_server::Creator<mcu_server::Data *(int)>;
 		
-		SetGpioTask(GpioInventory *inventory, const Tgpio_id& id, const mcu_platform::Gpio::State& state, const ReportCreator& report_ctor);
-		SetGpioTask(const SetGpioTask& other) = delete;
-		SetGpioTask& operator=(const SetGpioTask& other) = delete;
+		StepsTask(
+			StepperMotorInventory *inventory,
+			const Tstepper_id& id,
+			const Direction& direction,
+			const unsigned int steps_number,
+			const unsigned int step_duration_ms,
+			const ReportCreator& report_ctor
+		);
+		StepsTask(const StepsTask& other) = delete;
+		StepsTask& operator=(const StepsTask& other) = delete;
 		
 		mcu_server::Data *execute() const override;
 	private:
-		GpioInventory * const m_inventory;
-		const Tgpio_id m_id;
-		const mcu_platform::Gpio::State m_state;
+		StepperMotorInventory * const m_inventory;
+		const Tstepper_id m_id;
+		const Direction m_direction;
+		const unsigned int m_steps_number;
+		const unsigned int m_step_duration_ms;
 		const std::unique_ptr<ReportCreator> m_report_ctor;
 	};
 
-	template <class Tgpio_id>
-	inline SetGpioTask<Tgpio_id>::SetGpioTask(GpioInventory *inventory, const Tgpio_id& id, const mcu_platform::Gpio::State& state, const ReportCreator& report_ctor): m_inventory(inventory), m_id(id), m_state(state), m_report_ctor(report_ctor.clone()) {
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline StepsTask<Tstepper_id, Tgpio_id>::StepsTask(
+		StepperMotorInventory *inventory,
+		const Tstepper_id& id,
+		const Direction& direction,
+		const unsigned int steps_number,
+		const unsigned int step_duration_ms,
+		const ReportCreator& report_ctor
+	): m_inventory(inventory), m_id(id), m_direction(direction), m_steps_number(steps_number), m_step_duration_ms(step_duration_ms), m_report_ctor(report_ctor.clone()) {
 		if (!m_inventory) {
 			throw std::invalid_argument("invalid inventory ptr received");
 		}
 	}
 
-	template <class Tgpio_id>
-	inline mcu_server::Data *SetGpioTask<Tgpio_id>::execute() const {
-		auto gpio_ptr = m_inventory->access(m_id);
-		if (mcu_platform::Gpio::Direction::OUT != gpio_ptr->direction()) {
-			throw std::runtime_error("gpio with specified ID is not an output");
-		}
-		mcu_platform::Gpio::cast<mcu_platform::Gpo>(*gpio_ptr).set_state(m_state);
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline mcu_server::Data *StepsTask<Tstepper_id, Tgpio_id>::execute() const {
+		m_inventory->access(m_id)->steps(m_direction, m_steps_number, m_step_duration_ms);
 		return m_report_ctor->create(0);
 	}
 }
-#endif // SET_GPIO_TASK_HPP
+#endif // STEPS_TASK_HPP
