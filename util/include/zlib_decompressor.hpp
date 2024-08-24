a3b81c609bcba842486d7d098e5e8e13557b2aab
fedddot <idanrommm@gmail.com>
2024-08-24 15:39:20 +0300

    decompressor added

---
+++ util/include/zlib_decompressor.hpp
@@ -0,0 +1,42 @@
+#ifndef	ZLIB_DECOMPRESSOR_HPP
+#define	ZLIB_DECOMPRESSOR_HPP
+
+#include <cstddef>
+#include <memory>
+#include <stdexcept>
+#include <vector>
+
+#include "transformer.hpp"
+#include "zlib.h"
+
+namespace mcu_server_utl {
+	class ZlibDecompressor: public mcu_server::Transformer<std::vector<char>(const std::vector<char>&, const std::size_t&)> {
+	public:
+		ZlibDecompressor() = default;
+		ZlibDecompressor(const ZlibDecompressor& other) = default;
+		ZlibDecompressor& operator=(const ZlibDecompressor& other) = default;
+
+		std::vector<char> transform(const std::vector<char>& data, const std::size_t& decompressed_data_size) const override;
+		mcu_server::Transformer<std::vector<char>(const std::vector<char>&, const std::size_t&)> *clone() const override;
+	};
+
+	inline std::vector<char> ZlibDecompressor::transform(const std::vector<char>& data, const std::size_t& decompressed_data_size) const {
+		const auto compressed_data_size(data.size());
+		std::unique_ptr<Bytef[]> compressed_data_buff(new Bytef[compressed_data_size]);
+		if (!std::copy(data.begin(), data.end(), compressed_data_buff.get())) {
+			throw std::runtime_error("failed to copy compressed data");
+		}
+		std::unique_ptr<Bytef[]> decompressed_data_buff(new Bytef[decompressed_data_size]);
+		uLong actual_decompressed_data_size(decompressed_data_size);
+		if (Z_OK != uncompress(decompressed_data_buff.get(), &actual_decompressed_data_size, compressed_data_buff.get(), compressed_data_size)) {
+			throw std::runtime_error("failed to decompress data");
+		}
+		return std::vector<char>(decompressed_data_buff.get(), decompressed_data_buff.get() + actual_decompressed_data_size);
+	}
+
+	inline mcu_server::Transformer<std::vector<char>(const std::vector<char>&, const std::size_t&)> *ZlibDecompressor::clone() const {
+		return new ZlibDecompressor(*this);
+	}
+}
+
+#endif // ZLIB_DECOMPRESSOR_HPP
--- util/tests/src/ut_zlib_compressor.cpp
+++ util/tests/src/ut_zlib_compressor.cpp
@@ -3,23 +3,12 @@
 
 #include "gtest/gtest.h"
 #include <vector>
-#include "zlib.h"
+
+#include "zlib_decompressor.hpp"
 #include "zlib_compressor.hpp"
 
 using namespace mcu_server_utl;
 
-std::vector<char> uncompressData(const std::vector<char>& data, const std::size_t& uncompressed_data_size) {
-	std::unique_ptr<Bytef[]> data_buff(new Bytef[data.size()]);
-	if (!std::copy(data.begin(), data.end(), data_buff.get())) {
-		throw std::runtime_error("failed to copy data");
-	}
-	std::unique_ptr<Bytef[]> uncompressed_data_buff(new Bytef[uncompressed_data_size]);
-	uLong actual_uncompressed_data_size(uncompressed_data_size);
-	if (Z_OK != uncompress(uncompressed_data_buff.get(), &actual_uncompressed_data_size, data_buff.get(), data.size())) {
-		throw std::runtime_error("failed to uncompress data");
-	}
-	return std::vector<char>(uncompressed_data_buff.get(), uncompressed_data_buff.get() + actual_uncompressed_data_size);
-}
 
 TEST(ut_zlib_compressor, some_sanity) {
 	// GIVEN
@@ -31,5 +20,5 @@ TEST(ut_zlib_compressor, some_sanity) {
 
 	// THEN
 	auto result = compressor.transform(test_data);
-	ASSERT_EQ(test_data, uncompressData(result, test_data.size()));
+	ASSERT_EQ(test_data, ZlibDecompressor().transform(result, test_data.size()));
 }
