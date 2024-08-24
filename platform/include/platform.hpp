7aecf250b67b34f749ffa1dc588d356fa474dbc8
fedddot <idanrommm@gmail.com>
2024-08-24 19:36:48 +0300

    platform updated

--- platform/include/platform.hpp
+++ platform/include/platform.hpp
@@ -1,24 +1,16 @@
 #ifndef	PLATFORM_HPP
 #define	PLATFORM_HPP
 
-#include "data.hpp"
-#include "delay.hpp"
 #include "gpio.hpp"
-#include "inventory.hpp"
-#include "task.hpp"
 
 namespace mcu_platform {
-	template <typename Tgpio_id, typename Ttask_id>
+	template <typename Tgpio_id>
 	class Platform {
 	public:
-		using PersistentTask = server::Task<server::Data *(void)>;
-
 		virtual ~Platform() noexcept = default;
 
-		virtual Delay *create_delay() const = 0;
+		virtual void delay(const unsigned int delay_ms) const = 0;
 		virtual Gpio *create_gpio(const Tgpio_id& id, const Gpio::Direction& dir) const = 0;
-		virtual Inventory<Tgpio_id, Gpio> *gpio_inventory() const = 0;
-		virtual Inventory<Ttask_id, PersistentTask> *task_inventory() const = 0;
 	};
 }
 #endif // PLATFORM_HPP
