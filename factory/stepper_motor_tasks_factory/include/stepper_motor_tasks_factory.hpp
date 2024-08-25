e3f7aec852a04e33194d499b34fc5d8defd763d1
fedddot <idanrommm@gmail.com>
2024-08-25 18:16:43 +0300

    stepper_motor_tasks_factory updated

--- factory/stepper_motor_tasks_factory/include/stepper_motor_tasks_factory.hpp
+++ factory/stepper_motor_tasks_factory/include/stepper_motor_tasks_factory.hpp
@@ -4,21 +4,18 @@
 #include <memory>
 #include <stdexcept>
 
-#include "create_stepper_motor_task.hpp"
 #include "creator.hpp"
+#include "custom_task.hpp"
 #include "data.hpp"
-#include "delay.hpp"
-#include "delete_stepper_motor_task.hpp"
 #include "inventory.hpp"
 #include "stepper_motor.hpp"
-#include "stepper_motor_data_parser.hpp"
-#include "steps_task.hpp"
-#include "steps_sequence_task.hpp"
+#include "stepper_motor_tasks_data_retriever.hpp"
 #include "task.hpp"
+#include "task_creator.hpp"
 
 namespace mcu_factory {
 	template <typename Tstepper_id, typename Tgpio_id>
-	class StepperMotorTasksFactory: public server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> {
+	class StepperMotorTasksFactory: public TaskCreator {
 	public:
 		enum class TaskType: int {
 			CREATE_STEPPER_MOTOR,
@@ -26,106 +23,166 @@ namespace mcu_factory {
 			STEPS,
 			STEPS_SEQUENCE
 		};
-		using StepperMotorInventory = mcu_platform::Inventory<Tstepper_id, mcu_platform::StepperMotor<Tgpio_id>>;
-		using DataParser = StepperMotorDataParser<Tstepper_id, Tgpio_id, TaskType>;
-		using GpoCreator = typename server::Creator<mcu_platform::Gpo *(const Tgpio_id&)>;
-		using ReportCreator = typename server::Creator<server::Data *(int)>;
-		
+
+		using FactoryPlatform = mcu_platform::Platform<Tgpio_id>;
+		using DataRetriever = StepperMotorTasksDataRetriever<TaskType, Tgpio_id, Tstepper_id>;
+		using ResultReporter = server::Creator<server::Data *(int)>;
+		using MotorTask = server::Task<server::Data *(void)>;
+		using MotorInventory = mcu_platform::Inventory<Tstepper_id, mcu_platform::StepperMotor<Tgpio_id>>;
+
 		StepperMotorTasksFactory(
-			StepperMotorInventory *inventory,
-			const DataParser& stepper_motor_data_parser,
-			const GpoCreator& gpo_creator,
-			const mcu_platform::Delay& delay,
-			const ReportCreator& report_creator
+			MotorInventory *inventory,
+			const FactoryPlatform *platform,
+			const DataRetriever& retriever,
+			const ResultReporter& result_reporter
 		);
 		StepperMotorTasksFactory(const StepperMotorTasksFactory& other);
 		StepperMotorTasksFactory& operator=(const StepperMotorTasksFactory& other) = delete;
 
-		server::Task<server::Data *(void)> *create(const server::Data& data) const override;
-		server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> *clone() const override;
+		MotorTask *create(const server::Data& data) const override;
+		bool is_creatable(const server::Data& data) const override;
+		server::Creator<MotorTask *(const server::Data&)> *clone() const override;
+		TaskCreator *clone_task_creator() const override;
 	private:
-		StepperMotorInventory *m_inventory;
-		std::unique_ptr<DataParser> m_stepper_motor_data_parser;
-		std::unique_ptr<GpoCreator> m_gpo_creator;
-		std::unique_ptr<mcu_platform::Delay> m_delay;
-		std::unique_ptr<ReportCreator> m_report_creator;
+		mutable MotorInventory *m_inventory;
+		const FactoryPlatform *m_platform;
+		const std::unique_ptr<DataRetriever> m_retriever;
+		const std::unique_ptr<ResultReporter> m_result_reporter;
 		
-		using TaskCtor = server::Creator<server::Task<server::Data *(void)> *(const server::Data&)>;
-		const std::map<TaskType, std::unique_ptr<TaskCtor>> m_ctors;
+		using Shoulders = typename mcu_platform::StepperMotor<Tgpio_id>::Shoulders;
+		using States = typename mcu_platform::StepperMotor<Tgpio_id>::States;
+		using Direction = typename mcu_platform::StepperMotor<Tgpio_id>::Direction;
+
+		MotorTask *create_task(const Tstepper_id& id, const Shoulders& shoulders, const States& states) const;
+		MotorTask *delete_task(const Tstepper_id& id) const;
+		MotorTask *steps_task(const Tstepper_id& id, const Direction& direction, const unsigned int steps_num, const unsigned int step_duration) const;
+		MotorTask *steps_sequence_task(const Tgpio_id& id) const;
 	};
 
 	template <typename Tstepper_id, typename Tgpio_id>
 	inline StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::StepperMotorTasksFactory(
-		StepperMotorInventory *inventory,
-		const DataParser& stepper_motor_data_parser,
-		const GpoCreator& gpo_creator,
-		const mcu_platform::Delay& delay,
-		const ReportCreator& report_creator
+		MotorInventory *inventory,
+		const FactoryPlatform *platform,
+		const DataRetriever& retriever,
+		const ResultReporter& result_reporter
 	):
 		m_inventory(inventory),
-		m_stepper_motor_data_parser(stepper_motor_data_parser.clone()),
-		m_gpo_creator(gpo_creator.clone()),
-		m_delay(delay.clone()),
-		m_report_creator(report_creator.clone()) {
+		m_platform(platform),
+		m_retriever(retriever.clone()),
+		m_result_reporter(result_reporter.clone()) {
 		
 		if (!m_inventory) {
-			throw std::invalid_argument("invalid inventory ptr received");
+			throw std::invalid_argument("invalid inventory pointer received");
+		}
+		if (!m_platform) {
+			throw std::invalid_argument("invalid platform pointer received");
 		}
 	}
 
 	template <typename Tstepper_id, typename Tgpio_id>
 	inline StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::StepperMotorTasksFactory(const StepperMotorTasksFactory& other):
 		m_inventory(other.m_inventory),
-		m_stepper_motor_data_parser(other.m_stepper_motor_data_parser->clone()),
-		m_gpo_creator(other.m_gpo_creator->clone()),
-		m_delay(other.m_delay->clone()),
-		m_report_creator(other.m_report_creator->clone()) {
+		m_platform(other.m_platform),
+		m_retriever(other.m_retriever->clone()),
+		m_result_reporter(other.m_result_reporter->clone()) {
 		
 	}
 
 	template <typename Tstepper_id, typename Tgpio_id>
-	inline server::Task<server::Data *(void)> *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::create(const server::Data& data) const {
-		auto task_type = m_stepper_motor_data_parser->parse_task_type(data);
-		switch (task_type) {
+	inline typename StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::MotorTask *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::create(const server::Data& data) const {
+		switch (m_retriever->retrieve_task_type(data)) {
 		case TaskType::CREATE_STEPPER_MOTOR:
-			return new CreateStepperMotorTask<Tstepper_id, Tgpio_id>(
-				m_inventory,
-				m_stepper_motor_data_parser->parse_stepper_id(data),
-				m_stepper_motor_data_parser->parse_shoulders(data),
-				m_stepper_motor_data_parser->parse_states(data),
-				*m_gpo_creator,
-				*m_delay,
-				*m_report_creator
+			return create_task(
+				m_retriever->retrieve_stepper_id(data),
+				m_retriever->retrieve_shoulders(data),
+				m_retriever->retrieve_states(data)
 			);
 		case TaskType::DELETE_STEPPER_MOTOR:
-			return new DeleteStepperMotorTask<Tstepper_id, Tgpio_id>(
-				m_inventory,
-				m_stepper_motor_data_parser->parse_stepper_id(data),
-				*m_report_creator
-			);
+			return delete_task(m_retriever->retrieve_stepper_id(data));
 		case TaskType::STEPS:
-			return new StepsTask<Tstepper_id, Tgpio_id>(
-				m_inventory,
-				m_stepper_motor_data_parser->parse_stepper_id(data),
-				m_stepper_motor_data_parser->parse_steps_direction(data),
-				m_stepper_motor_data_parser->parse_steps_number(data),
-				m_stepper_motor_data_parser->parse_step_duration(data),
-				*m_report_creator
+			return steps_task(
+				m_retriever->retrieve_stepper_id(data),
+				m_retriever->retrieve_step_duration(data),
+				m_retriever->retrieve_steps_number(data),
+				m_retriever->retrieve_step_duration(data)
 			);
 		case TaskType::STEPS_SEQUENCE:
-			return new StepsSequenceTask<Tstepper_id, Tgpio_id>(
-				m_inventory,
-				m_stepper_motor_data_parser->parse_steps_sequence(data),
-				*m_report_creator
-			);
+			return steps_sequence_task(m_retriever->retrieve_stepper_id(data));
 		default:
-			throw std::invalid_argument("invalid task type received");
+			throw std::invalid_argument("unsupported task type received");
 		}
+	}
+
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline bool StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::is_creatable(const server::Data& data) const {
+		return m_retriever->is_stepper_motor_task_creatable(data);
 	}
 	
 	template <typename Tstepper_id, typename Tgpio_id>
 	inline server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::clone() const {
+		return clone_task_creator();
+	}
+
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline TaskCreator *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::clone_task_creator() const {
 		return new StepperMotorTasksFactory(*this);
+	}
+
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline typename StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::MotorTask *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::create_task(const Tstepper_id& id, const Shoulders& shoulders, const States& states) const {
+		using namespace server;
+		using namespace server_utl;
+		using namespace mcu_platform;
+		auto inventory(m_inventory);
+		auto platform(m_platform);
+		std::shared_ptr<ResultReporter> reporter(m_result_reporter->clone());
+		return new CustomTask<Data *(void)>(
+			[id, shoulders, states, inventory, platform, reporter](void)-> Data * {
+				auto motor_ptr(new StepperMotor<Tgpio_id>(shoulders, states, platform));
+				try {
+					inventory->put(id, motor_ptr);
+					return reporter->create(0);
+				} catch (...) {
+					delete motor_ptr;
+					throw;
+				}
+			}
+		);
+	}
+
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline typename StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::MotorTask *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::delete_task(const Tstepper_id& id) const {
+		using namespace server;
+		using namespace server_utl;
+		using namespace mcu_platform;
+		auto inventory(m_inventory);
+		std::shared_ptr<ResultReporter> reporter(m_result_reporter->clone());
+		return new CustomTask<Data *(void)>(
+			[id, inventory, reporter](void)-> Data * {
+				delete inventory->pull(id);
+				return reporter->create(0);
+			}
+		);
+	}
+
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline typename StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::MotorTask *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::steps_task(const Tstepper_id& id, const Direction& direction, const unsigned int steps_num, const unsigned int step_duration) const {
+		using namespace server;
+		using namespace server_utl;
+		using namespace mcu_platform;
+		auto inventory(m_inventory);
+		std::shared_ptr<ResultReporter> reporter(m_result_reporter->clone());
+		return new CustomTask<Data *(void)>(
+			[id, direction, steps_num, step_duration, inventory, reporter](void)-> Data * {
+				inventory->access(id)->steps(direction, steps_num, step_duration);
+				return reporter->create(0);
+			}
+		);
+	}
+
+	template <typename Tstepper_id, typename Tgpio_id>
+	inline typename StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::MotorTask *StepperMotorTasksFactory<Tstepper_id, Tgpio_id>::steps_sequence_task(const Tgpio_id& id) const {
+		throw std::runtime_error("NOT_IMPLEMENTED");
 	}
 }
 #endif // STEPPER_MOTOR_TASKS_FACTORY_HPP
