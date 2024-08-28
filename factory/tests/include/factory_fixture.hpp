0121a55b5c467cbb96d03db88afb8e0dac520ead
fedddot <idanrommm@gmail.com>
2024-08-28 21:23:25 +0300

    tests updated

--- factory/tests/include/factory_fixture.hpp
+++ factory/tests/include/factory_fixture.hpp
@@ -3,12 +3,21 @@
 
 #include "gtest/gtest.h"
 
+#include "gpio_test_data_creator.hpp"
+#include "stepper_motor_test_data_creator.hpp"
+#include "task_creator.hpp"
+
 namespace mcu_factory_uts {
 	class FactoryFixture: public testing::Test {
 	public:
 		FactoryFixture() = default;
 		FactoryFixture(const FactoryFixture&) = delete;
 		FactoryFixture& operator=(const FactoryFixture&) = delete;
+
+		const StepperMotorTestDataCreator& stepper_motor_data_ctor() const;
+		const GpioTestDataCreator& gpio_data_ctor() const;
+		const mcu_factory::TaskCreator& gpio_tasks_ctor() const;
+		const mcu_factory::TaskCreator& stepper_motor_tasks_ctor() const;
 	};
 }
 
