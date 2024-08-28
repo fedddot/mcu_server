da0abe4af469d9327149c631a803dde28f8cc927
fedddot <idanrommm@gmail.com>
2024-08-28 20:43:50 +0300

    class name updated

--- factory/include/mcu_factory.hpp
+++ factory/include/mcu_factory.hpp
@@ -1,5 +1,5 @@
-#ifndef	MCU_FACTORY_HPP
-#define	MCU_FACTORY_HPP
+#ifndef	FACTORY_HPP
+#define	FACTORY_HPP
 
 #include <memory>
 #include <stdexcept>
@@ -11,11 +11,11 @@
 #include "task_creator.hpp"
 
 namespace mcu_factory {
-	class McuFactory: public server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> {
+	class Factory: public server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> {
 	public:
-		McuFactory() = default;
-		McuFactory(const McuFactory& other);
-		McuFactory& operator=(const McuFactory& other) = delete;
+		Factory() = default;
+		Factory(const Factory& other);
+		Factory& operator=(const Factory& other) = delete;
 
 		server::Task<server::Data *(void)> *create(const server::Data& data) const override;
 		server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> *clone() const override;
@@ -24,13 +24,13 @@ namespace mcu_factory {
 		std::vector<std::unique_ptr<TaskCreator>> m_ctors;
 	};
 
-	inline McuFactory::McuFactory(const McuFactory& other) {
+	inline Factory::Factory(const Factory& other) {
 		for (const auto& ctor: other.m_ctors) {
 			m_ctors.push_back(std::unique_ptr<TaskCreator>(ctor->clone_task_creator()));
 		}
 	}
 
-	inline server::Task<server::Data *(void)> *McuFactory::create(const server::Data& data) const {
+	inline server::Task<server::Data *(void)> *Factory::create(const server::Data& data) const {
 		for (const auto& ctor_ptr: m_ctors) {
 			if (!(ctor_ptr->is_creatable(data))) {
 				continue;
@@ -40,12 +40,12 @@ namespace mcu_factory {
 		throw std::invalid_argument("increatable data received");
 	}
 	
-	inline server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> *McuFactory::clone() const {
-		return new McuFactory(*this);
+	inline server::Creator<server::Task<server::Data *(void)> *(const server::Data&)> *Factory::clone() const {
+		return new Factory(*this);
 	}
 
-	inline void McuFactory::add_task_creator(const TaskCreator& creator) {
+	inline void Factory::add_task_creator(const TaskCreator& creator) {
 		m_ctors.push_back(std::unique_ptr<TaskCreator>(creator.clone_task_creator()));
 	}
 }
-#endif // MCU_FACTORY_HPP
+#endif // FACTORY_HPP
