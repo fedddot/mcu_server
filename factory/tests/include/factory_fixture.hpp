752cd8e0bf2157ac129a1206c8360d5149a15763
fedddot <idanrommm@gmail.com>
2024-08-28 21:46:34 +0300

    updated

--- factory/tests/include/factory_fixture.hpp
+++ factory/tests/include/factory_fixture.hpp
@@ -2,23 +2,94 @@
 #define FACTORY_FIXTURE_HPP
 
 #include "gtest/gtest.h"
+#include <memory>
 
+#include "custom_creator.hpp"
+#include "data.hpp"
+#include "default_gpio_tasks_data_retriever.hpp"
+#include "default_stepper_motor_tasks_data_retriever.hpp"
+#include "gpio.hpp"
+#include "gpio_tasks_factory.hpp"
 #include "gpio_test_data_creator.hpp"
+#include "integer.hpp"
+#include "inventory.hpp"
+#include "object.hpp"
+#include "stepper_motor.hpp"
+#include "stepper_motor_tasks_factory.hpp"
 #include "stepper_motor_test_data_creator.hpp"
 #include "task_creator.hpp"
+#include "test_platform.hpp"
 
 namespace mcu_factory_uts {
 	class FactoryFixture: public testing::Test {
 	public:
-		FactoryFixture() = default;
+		using GpioId = int;
+		using StepperId = int;
+		
+		FactoryFixture();
 		FactoryFixture(const FactoryFixture&) = delete;
 		FactoryFixture& operator=(const FactoryFixture&) = delete;
 
-		const StepperMotorTestDataCreator& stepper_motor_data_ctor() const;
-		const GpioTestDataCreator& gpio_data_ctor() const;
-		const mcu_factory::TaskCreator& gpio_tasks_ctor() const;
-		const mcu_factory::TaskCreator& stepper_motor_tasks_ctor() const;
+		const StepperMotorTestDataCreator& stepper_motor_data_ctor() const {
+			return m_stepper_motor_data_ctor;
+		}
+		const GpioTestDataCreator& gpio_data_ctor() const {
+			return m_gpio_data_ctor;
+		}
+		const mcu_factory::TaskCreator& gpio_tasks_ctor() const {
+			return *m_gpio_tasks_ctor;
+		}
+		const mcu_factory::TaskCreator& stepper_motor_tasks_ctor() const {
+			return *m_stepper_motor_tasks_ctor;
+		}
+	private:
+		using GpioInventory = mcu_platform::Inventory<GpioId, mcu_platform::Gpio>;
+		using StepperMotorInventory = mcu_platform::Inventory<StepperId, mcu_platform::StepperMotor<GpioId>>;
+		GpioInventory m_gpio_inventory;
+		StepperMotorInventory m_stepper_motor_inventory;
+		mcu_platform_uts::TestPlatform<GpioId> m_platform;
+		server_utl::CustomCreator<server::Data *(int)> m_report_ctor;
+		server_utl::CustomCreator<server::Data *(int, const mcu_platform::Gpio::State&)> m_state_report_ctor;
+		StepperMotorTestDataCreator m_stepper_motor_data_ctor;
+		GpioTestDataCreator m_gpio_data_ctor;
+		std::unique_ptr<mcu_factory::TaskCreator> m_gpio_tasks_ctor;
+		std::unique_ptr<mcu_factory::TaskCreator> m_stepper_motor_tasks_ctor;
 	};
+
+	inline FactoryFixture::FactoryFixture():
+		m_report_ctor(
+			[](int result) {
+				server::Object report;
+				report.add("result", server::Integer(result));
+				return report.clone();
+			}
+		),
+		m_state_report_ctor(
+			[](int result, const mcu_platform::Gpio::State& state) {
+				server::Object report;
+				report.add("result", server::Integer(result));
+				report.add("gpio_state", server::Integer(static_cast<int>(state)));
+				return report.clone();
+			}
+		) {
+			m_gpio_tasks_ctor = std::unique_ptr<mcu_factory::TaskCreator>(
+				new mcu_factory::GpioTasksFactory<GpioId>(
+					&m_gpio_inventory,
+					&m_platform,
+					mcu_factory::DefaultGpioTasksDataRetriever<GpioId>(),
+					m_report_ctor,
+					m_state_report_ctor
+				)
+			);
+			m_stepper_motor_tasks_ctor = std::unique_ptr<mcu_factory::TaskCreator>(
+				new mcu_factory::StepperMotorTasksFactory<StepperId, GpioId>(
+					&m_stepper_motor_inventory,
+					&m_platform,
+					mcu_factory::DefaultStepperMotorDataRetriever<GpioId, StepperId>(),
+					m_report_ctor
+				)
+			);
+		}
 }
 
 #endif
--- factory/tests/src/ut_factory.cpp
+++ factory/tests/src/ut_factory.cpp
@@ -1,8 +1,11 @@
+#include <memory>
+
 #include "gtest/gtest.h"
+
+#include "data.hpp"
 #include "factory_fixture.hpp"
 #include "factory.hpp"
-#include "gpio_tasks_factory.hpp"
-#include "inventory.hpp"
+#include "task.hpp"
 
 using namespace server;
 using namespace mcu_factory;
@@ -35,28 +38,17 @@ using namespace mcu_platform;
 
 TEST_F(FactoryFixture, create_sanity) {
 	// GIVEN
-	Inventory<class Tkey, class Titem>
+	const GpioId test_gpio_id(11);
+	const auto test_gpio_dir(Gpio::Direction::OUT);
+	const StepperId test_stepper_id(12);
+	
 	// WHEN
 	Factory instance;
-	instance.add_task_creator(
-		GpioTasksFactory<int>(
-			
-		)
-	)
-	// THEN
-	ASSERT_NO_THROW(instance_ptr = new Factory());
-	ASSERT_NE(nullptr, instance_ptr);
-	
-	ASSERT_NO_THROW(
-		(
-			instance_ptr_copy = new Factory(*instance_ptr)
-		)
-	);
-	ASSERT_NE(nullptr, instance_ptr_copy);
+	instance.add_task_creator(gpio_tasks_ctor());
+	instance.add_task_creator(stepper_motor_tasks_ctor());
+	std::unique_ptr<Task<Data *(void)>> task_ptr(nullptr);
 
-	ASSERT_NO_THROW(delete instance_ptr);
-	ASSERT_NO_THROW(delete instance_ptr_copy);
-	
-	instance_ptr = nullptr;
-	instance_ptr_copy = nullptr;
+	// THEN
+	ASSERT_NO_THROW(task_ptr = std::unique_ptr<Task<Data *(void)>>(instance.create(gpio_data_ctor().create_gpio_data(test_gpio_id, test_gpio_dir))));
+	ASSERT_NE(nullptr, task_ptr);
 }
