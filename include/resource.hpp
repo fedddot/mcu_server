3bf6f1e957aa5ed0fc1a79de4f28c1bb24e6ae60
fedddot <idanrommm@gmail.com>
2024-09-25 19:36:33 +0300

    resource updated

--- include/resource.hpp
+++ include/resource.hpp
@@ -1,15 +1,16 @@
 #ifndef	RESOURCE_HPP
 #define	RESOURCE_HPP
 
+#include <string>
+
 #include "request.hpp"
 #include "response.hpp"
 
 namespace server {
-	template <typename Tid>
 	class Resource {
 	public:
 		virtual ~Resource() noexcept = default;
-		virtual Tid id() const = 0;
+		virtual std::string id() const = 0;
 		virtual Response run_request(const Request&) = 0;
 	};
 }
