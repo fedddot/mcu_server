2309034b2e923270af3bd4dd41f1c9354863bac7
fedddot <idanrommm@gmail.com>
2024-09-25 19:52:31 +0300

    clone method added

--- include/resource.hpp
+++ include/resource.hpp
@@ -12,6 +12,7 @@ namespace server {
 		virtual ~Resource() noexcept = default;
 		virtual std::string id() const = 0;
 		virtual Response run_request(const Request&) = 0;
+		virtual Resource *clone() const = 0;
 	};
 }
 
