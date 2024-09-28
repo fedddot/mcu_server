0a497cbfd378bd4926e0b1b7d5981efd69285216
fedddot <idanrommm@gmail.com>
2024-09-28 17:14:42 +0300

    id deleted

--- include/resource.hpp
+++ include/resource.hpp
@@ -2,14 +2,13 @@
 #define	RESOURCE_HPP
 
 namespace server {
-	template <typename Tid, typename Signature>
+	template <typename Signature>
 	class Resource;
 
-	template <typename Tid, typename Tresponse, typename... Args>
-	class Resource<Tid, Tresponse(Args...)> {
+	template <typename Tresponse, typename... Args>
+	class Resource<Tresponse(Args...)> {
 	public:
 		virtual ~Resource() noexcept = default;
-		virtual Tid id() const = 0;
 		virtual Tresponse run_request(Args...) const = 0;
 		virtual Resource *clone() const = 0;
 	};
