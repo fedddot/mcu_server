4610df739e270f5d0c293a82cdd56f0104690431
fedddot <idanrommm@gmail.com>
2024-09-28 16:35:36 +0300

    resource templatized

--- include/resource.hpp
+++ include/resource.hpp
@@ -1,17 +1,16 @@
 #ifndef	RESOURCE_HPP
 #define	RESOURCE_HPP
 
-#include <string>
-
-#include "request.hpp"
-#include "response.hpp"
-
 namespace server {
-	class Resource {
+	template <typename Tid, typename Signature>
+	class Resource;
+
+	template <typename Tid, typename Tresponse, typename... Args>
+	class Resource<Tid, Tresponse(Args...)> {
 	public:
 		virtual ~Resource() noexcept = default;
-		virtual std::string id() const = 0;
-		virtual Response run_request(const Request&) const = 0;
+		virtual Tid id() const = 0;
+		virtual Tresponse run_request(Args...) const = 0;
 		virtual Resource *clone() const = 0;
 	};
 }
