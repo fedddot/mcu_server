653d758698fc7028439d5d256b04f30a67dd525c
fedddot <idanrommm@gmail.com>
2024-10-02 16:04:24 +0300

    partially implemented

--- manager/gpio_manager/include/gpio_manager.hpp
+++ manager/gpio_manager/include/gpio_manager.hpp
@@ -1,181 +1,33 @@
 #ifndef	GPIO_MANAGER_HPP
 #define	GPIO_MANAGER_HPP
 
-#include <exception>
-#include <functional>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <map>
+#include "gpio.hpp"
+#include "inventory_manager.hpp"
 
-#include "array.hpp"
-#include "data.hpp"
-#include "integer.hpp"
 #include "object.hpp"
-#include "request.hpp"
 #include "resource.hpp"
-#include "response.hpp"
-#include "string.hpp"
 
-namespace server {
-	template <typename Tstored>
-	class GpioManager: public Resource {
+namespace manager {
+	
+	class GpioManager: public InventoryManager<Gpio> {
 	public:
-		using Creator = std::function<Tstored *(const Data&)>;
-		using Duplicator = std::function<Tstored *(const Tstored&)>;
-		using Reader = std::function<Object(const Tstored&)>;
-		using Writer = std::function<void(Tstored *, const Data&)>;
-
-		GpioManager(const Creator& creator, const Duplicator& duplicator, const Reader& reader, const Writer& writer);
-		GpioManager(const GpioManager& other);
+		GpioManager(const Creator& creator);
+		GpioManager(const GpioManager& other) = default;
 		GpioManager& operator=(const GpioManager&) = delete;
 		
-		Response run_request(const Request& request) const override;
-		Resource *clone() const override;
+		server::Resource *clone() const override;
 	private:
-		Creator m_creator;
-		Duplicator m_duplicator;
-		Reader m_reader;
-		Writer m_writer;
-
-		using InstanceUniquePtr = std::unique_ptr<Tstored>;
-		using Instances = std::map<std::string, InstanceUniquePtr>;
-		mutable Instances m_instances;
-
-		Response run_create(const Request& request) const;
-		Response run_read(const Request& request) const;
-		Response run_update(const Request& request) const;
-		Response run_delete(const Request& request) const;
-
-		using ResponseCode = typename Response::ResponseCode;
-		Response report_failure(const ResponseCode& code, const std::string& what) const;
+		static Gpio *duplicate(const Gpio& instance);
+		static server::Object read_gpio(const Gpio& gpio);
+		static void write_gpio(Gpio *gpio_ptr, const server::Data& data);
 	};
 
-	template <typename Tstored>
-	inline GpioManager<Tstored>::GpioManager(const Creator& creator, const Duplicator& duplicator, const Reader& reader, const Writer& writer): m_creator(creator), m_duplicator(duplicator), m_reader(reader), m_writer(writer) {
-		if (!m_creator || !m_duplicator || !m_reader || !m_writer) {
-			throw std::invalid_argument("invalid action received");
-		}
+	inline GpioManager::GpioManager(const Creator& creator): InventoryManager<Gpio>(creator, Duplicator(GpioManager::duplicate), Reader(GpioManager::read_gpio), Writer(GpioManager::write_gpio)) {
+
 	}
 
-	template <typename Tstored>
-	inline GpioManager<Tstored>::GpioManager(const GpioManager& other): m_creator(other.m_creator), m_duplicator(other.m_duplicator), m_reader(other.m_reader), m_writer(other.m_writer) {
-		for (const auto& [id, instance_ptr]: other.m_instances) {
-			m_instances.insert({id, InstanceUniquePtr(m_duplicator(*instance_ptr))});
-		}
-	}
-
-	template <typename Tstored>
-	inline Response GpioManager<Tstored>::run_request(const Request& request) const {
-		try {
-			using Method = typename Request::Method;
-			switch (request.method()) {
-			case Method::CREATE:
-				return run_create(request);
-			case Method::READ:
-				return run_read(request);
-			case Method::UPDATE:
-				return run_update(request);
-			case Method::DELETE:
-				return run_delete(request);
-			default:
-				return report_failure(ResponseCode::BAD_REQUEST, "unsupported method request received");
-			}
-		} catch (const std::exception& e) {
-			return report_failure(ResponseCode::UNSPECIFIED, std::string(e.what()));
-		}
-	}
-
-	template <typename Tstored>
-	inline Resource *GpioManager<Tstored>::clone() const {
+	inline server::Resource *GpioManager::clone() const {
 		return new GpioManager(*this);
-	}
-
-	template <typename Tstored>
-	inline Response GpioManager<Tstored>::run_create(const Request& request) const {
-		using Body = typename Response::Body;
-		if (!request.path().empty()) {
-			return report_failure(ResponseCode::NOT_FOUND, "wrong create request path");
-		}
-		auto id(Data::cast<String>(request.body().access("id")).get());
-		if (m_instances.end() != m_instances.find(id)) {
-			return report_failure(ResponseCode::BAD_REQUEST, "component with id = " + id + " already exists");
-		}
-		m_instances.insert({id, std::unique_ptr<Tstored>(m_creator(request.body()))});
-		return Response(ResponseCode::OK, Body());
-	}
-
-	template <typename Tstored>
-	inline Response GpioManager<Tstored>::run_read(const Request& request) const {
-		auto read_instance = [this](const std::string& id) {
-			auto iter = m_instances.find(id);
-			if (m_instances.end() == iter) {
-				return report_failure(ResponseCode::NOT_FOUND, "resource " + id + " is not registered");
-			}
-			auto instance_data(m_reader(*(iter->second)));
-			instance_data.add("id", String(iter->first));
-			return Response(ResponseCode::OK, instance_data);
-		};
-
-		auto read_all_instances = [this](void) {
-			using Body = typename Response::Body;
-			Array members;
-			for (const auto& [id, instance_ptr]: m_instances) {
-				auto member_data(m_reader(*instance_ptr));
-				member_data.add("id", String(id));
-				members.push_back(member_data);
-			}
-			Body body;
-			body.add("members", members);
-			return Response(ResponseCode::OK, body);
-		};
-
-		if (1UL < request.path().size()) {
-			return report_failure(ResponseCode::NOT_FOUND, std::string("path not found"));
-		}
-
-		if (request.path().empty()) {
-			return read_all_instances();
-		}
-
-		auto id(request.path()[0]);
-		return read_instance(id);
-	}
-
-	template <typename Tstored>
-	inline Response GpioManager<Tstored>::run_update(const Request& request) const {
-		if (1UL != request.path().size()) {
-			return report_failure(ResponseCode::NOT_FOUND, std::string("path not found"));
-		}
-		auto id(request.path()[0]);
-		auto iter = m_instances.find(id);
-		if (m_instances.end() == iter) {
-			return report_failure(ResponseCode::NOT_FOUND, std::string("path not found"));
-		}
-		m_writer((iter->second).get(), request.body());
-		return Response(ResponseCode::OK, Response::Body());
-	}
-
-	template <typename Tstored>
-	inline Response GpioManager<Tstored>::run_delete(const Request& request) const {
-		if (1UL != request.path().size()) {
-			return report_failure(ResponseCode::NOT_FOUND, std::string("path not found"));
-		}
-		auto id(request.path()[0]);
-		auto iter = m_instances.find(id);
-		if (m_instances.end() == iter) {
-			return report_failure(ResponseCode::NOT_FOUND, std::string("path not found"));
-		}
-		m_instances.erase(iter);
-		return Response(ResponseCode::OK, Response::Body());
-	}
-
-	template <typename Tstored>
-	inline Response GpioManager<Tstored>::report_failure(const ResponseCode& code, const std::string& what) const {
-		using Body = typename Response::Body;
-		Body failure_body;
-		failure_body.add("what", String(what));
-		return Response(ResponseCode::UNSPECIFIED, failure_body);
 	}
 }
 
--- manager/gpio_manager/tests/src/ut_gpio_manager.cpp
+++ manager/gpio_manager/tests/src/ut_gpio_manager.cpp
@@ -1,17 +1,14 @@
 #include "gtest/gtest.h"
 #include <memory>
 #include <stdexcept>
-#include <string>
-
-#include "array.hpp"
 #include "data.hpp"
-#include "object.hpp"
+#include "gpio.hpp"
 #include "gpio_manager.hpp"
 #include "request.hpp"
 #include "response.hpp"
-#include "string.hpp"
 
 using namespace server;
+using namespace manager;
 
 using Method = typename Request::Method;
 using Path = typename Request::Path;
@@ -20,35 +17,24 @@ using ResponseCode = typename Response::ResponseCode;
 
 TEST(ut_gpio_manager, ctor_cctor_clone_dtor_id_sanity) {
 	// GIVEN
-	using TestManager = GpioManager;
-
-	auto test_creator = [](const Data& data)-> std::string * {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
-	auto test_copier = [](const std::string& data)-> std::string * {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
-	auto test_reader = [](const std::string& instance)-> Object {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
-	auto test_writer = [](std::string *instance_ptr, const Data& data) {
+	auto test_creator = [](const Data& data)-> Gpio * {
 		throw std::runtime_error("NOT IMPLEMENTED");
 	};
 
 	// WHEN
-	using TestManagerUnqPtr = std::unique_ptr<TestManager>;
+	using GpioManagerUnqPtr = std::unique_ptr<GpioManager>;
 	using ResourceUnqPtr = std::unique_ptr<Resource>;
-	TestManagerUnqPtr instance_ptr(nullptr);
-	TestManagerUnqPtr instance_ptr_copy(nullptr);
+	GpioManagerUnqPtr instance_ptr(nullptr);
+	GpioManagerUnqPtr instance_ptr_copy(nullptr);
 	ResourceUnqPtr instance_ptr_clone(nullptr);
 	
 	// THEN
 	// ctor
-	ASSERT_NO_THROW(instance_ptr = TestManagerUnqPtr(new TestManager(test_creator, test_copier, test_reader, test_writer)));
+	ASSERT_NO_THROW(instance_ptr = GpioManagerUnqPtr(new GpioManager(test_creator)));
 	ASSERT_NE(nullptr, instance_ptr);
 
 	// cctor
-	ASSERT_NO_THROW(instance_ptr_copy = TestManagerUnqPtr(new TestManager(*instance_ptr)));
+	ASSERT_NO_THROW(instance_ptr_copy = GpioManagerUnqPtr(new GpioManager(*instance_ptr)));
 	ASSERT_NE(nullptr, instance_ptr_copy);
 
 	// clone
@@ -61,130 +47,130 @@ TEST(ut_gpio_manager, ctor_cctor_clone_dtor_id_sanity) {
 	ASSERT_NO_THROW(instance_ptr_clone = nullptr);
 }
 
-TEST(ut_gpio_manager, run_request_sanity) {
-	// GIVEN
-	using TestManager = GpioManager;
+// TEST(ut_gpio_manager, run_request_sanity) {
+// 	// GIVEN
+// 	using GpioManager = GpioManager;
 	
-	auto test_creator = [](const Data& data) {
-		return new std::string(Data::cast<String>(Data::cast<Object>(data).access("data")).get());
-	};
-	auto test_copier = [](const std::string& data) {
-		return new std::string(data);
-	};
-	auto test_reader = [](const std::string& instance) {
-		Object data;
-		data.add("data", String(instance));
-		return data;
-	};
-	auto test_writer = [](std::string *instance_ptr, const Data& data) {
-		*instance_ptr = Data::cast<String>(Data::cast<Object>(data).access("data")).get();
-	};
+// 	auto test_creator = [](const Data& data) {
+// 		return new std::string(Data::cast<String>(Data::cast<Object>(data).access("data")).get());
+// 	};
+// 	auto test_copier = [](const std::string& data) {
+// 		return new std::string(data);
+// 	};
+// 	auto test_reader = [](const std::string& instance) {
+// 		Object data;
+// 		data.add("data", String(instance));
+// 		return data;
+// 	};
+// 	auto test_writer = [](std::string *instance_ptr, const Data& data) {
+// 		*instance_ptr = Data::cast<String>(Data::cast<Object>(data).access("data")).get();
+// 	};
 
-	Body create_instance_data;
-	create_instance_data.add("id", String("instance_id"));
-	create_instance_data.add("data", String("instance_data"));
+// 	Body create_instance_data;
+// 	create_instance_data.add("id", String("instance_id"));
+// 	create_instance_data.add("data", String("instance_data"));
 
-	Body update_instance_data;
-	update_instance_data.add("data", String("updated_instance_data"));
+// 	Body update_instance_data;
+// 	update_instance_data.add("data", String("updated_instance_data"));
 	
-	// WHEN
-	TestManager instance(test_creator, test_copier, test_reader, test_writer);
-	Response response(ResponseCode::OK, Body());
+// 	// WHEN
+// 	GpioManager instance(test_creator, test_copier, test_reader, test_writer);
+// 	Response response(ResponseCode::OK, Body());
 
-	// THEN
-	// Create
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::CREATE, 
-				{}, 
-				create_instance_data
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	// THEN
+// 	// Create
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::CREATE, 
+// 				{}, 
+// 				create_instance_data
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
 	
-	// ReadAll
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::READ, 
-				{}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-	ASSERT_TRUE(response.body().contains("members"));
-	ASSERT_EQ(Data::Type::ARRAY, response.body().access("members").type());
-	ASSERT_EQ(1UL, Data::cast<Array>(response.body().access("members")).size());
+// 	// ReadAll
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::READ, 
+// 				{}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	ASSERT_TRUE(response.body().contains("members"));
+// 	ASSERT_EQ(Data::Type::ARRAY, response.body().access("members").type());
+// 	ASSERT_EQ(1UL, Data::cast<Array>(response.body().access("members")).size());
 
-	// Read
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::READ, 
-				{Data::cast<String>(create_instance_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-	ASSERT_TRUE(response.body().contains("id"));
-	ASSERT_EQ(Data::cast<String>(create_instance_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
+// 	// Read
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::READ, 
+// 				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	ASSERT_TRUE(response.body().contains("id"));
+// 	ASSERT_EQ(Data::cast<String>(create_instance_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
 	
-	ASSERT_TRUE(response.body().contains("data"));
-	ASSERT_EQ(Data::cast<String>(create_instance_data.access("data")).get(), Data::cast<String>(response.body().access("data")).get());
+// 	ASSERT_TRUE(response.body().contains("data"));
+// 	ASSERT_EQ(Data::cast<String>(create_instance_data.access("data")).get(), Data::cast<String>(response.body().access("data")).get());
 
-	// Update
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::UPDATE, 
-				{Data::cast<String>(create_instance_data.access("id")).get()}, 
-				update_instance_data
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-	// Update validation
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::READ, 
-				{Data::cast<String>(create_instance_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-	ASSERT_TRUE(response.body().contains("id"));
-	ASSERT_EQ(Data::cast<String>(create_instance_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
+// 	// Update
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::UPDATE, 
+// 				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+// 				update_instance_data
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	// Update validation
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::READ, 
+// 				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	ASSERT_TRUE(response.body().contains("id"));
+// 	ASSERT_EQ(Data::cast<String>(create_instance_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
 	
-	ASSERT_TRUE(response.body().contains("data"));
-	ASSERT_EQ(Data::cast<String>(update_instance_data.access("data")).get(), Data::cast<String>(response.body().access("data")).get());
+// 	ASSERT_TRUE(response.body().contains("data"));
+// 	ASSERT_EQ(Data::cast<String>(update_instance_data.access("data")).get(), Data::cast<String>(response.body().access("data")).get());
 
-	// Delete
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::DELETE, 
-				{Data::cast<String>(create_instance_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	// Delete
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::DELETE, 
+// 				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
 	
-	// Delete validation
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::READ, 
-				{Data::cast<String>(create_instance_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_NE(ResponseCode::OK, response.code());
-}
+// 	// Delete validation
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::READ, 
+// 				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_NE(ResponseCode::OK, response.code());
+// }
