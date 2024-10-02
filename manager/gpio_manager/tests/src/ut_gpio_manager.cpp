2fea1dfdddab68e5fd499f7d147ed912fc49439b
fedddot <idanrommm@gmail.com>
2024-10-02 15:28:56 +0300

    gpio related files moved, gpio manager created

---
+++ manager/gpio_manager/tests/src/ut_gpio_manager.cpp
@@ -0,0 +1,190 @@
+#include "gtest/gtest.h"
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+#include "array.hpp"
+#include "data.hpp"
+#include "object.hpp"
+#include "gpio_manager.hpp"
+#include "request.hpp"
+#include "response.hpp"
+#include "string.hpp"
+
+using namespace server;
+
+using Method = typename Request::Method;
+using Path = typename Request::Path;
+using Body = typename Request::Body;
+using ResponseCode = typename Response::ResponseCode;	
+
+TEST(ut_gpio_manager, ctor_cctor_clone_dtor_id_sanity) {
+	// GIVEN
+	using TestManager = GpioManager;
+
+	auto test_creator = [](const Data& data)-> std::string * {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	};
+	auto test_copier = [](const std::string& data)-> std::string * {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	};
+	auto test_reader = [](const std::string& instance)-> Object {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	};
+	auto test_writer = [](std::string *instance_ptr, const Data& data) {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	};
+
+	// WHEN
+	using TestManagerUnqPtr = std::unique_ptr<TestManager>;
+	using ResourceUnqPtr = std::unique_ptr<Resource>;
+	TestManagerUnqPtr instance_ptr(nullptr);
+	TestManagerUnqPtr instance_ptr_copy(nullptr);
+	ResourceUnqPtr instance_ptr_clone(nullptr);
+	
+	// THEN
+	// ctor
+	ASSERT_NO_THROW(instance_ptr = TestManagerUnqPtr(new TestManager(test_creator, test_copier, test_reader, test_writer)));
+	ASSERT_NE(nullptr, instance_ptr);
+
+	// cctor
+	ASSERT_NO_THROW(instance_ptr_copy = TestManagerUnqPtr(new TestManager(*instance_ptr)));
+	ASSERT_NE(nullptr, instance_ptr_copy);
+
+	// clone
+	ASSERT_NO_THROW(instance_ptr_clone = ResourceUnqPtr(instance_ptr->clone()));
+	ASSERT_NE(nullptr, instance_ptr_clone);
+
+	// dtors
+	ASSERT_NO_THROW(instance_ptr = nullptr);
+	ASSERT_NO_THROW(instance_ptr_copy = nullptr);
+	ASSERT_NO_THROW(instance_ptr_clone = nullptr);
+}
+
+TEST(ut_gpio_manager, run_request_sanity) {
+	// GIVEN
+	using TestManager = GpioManager;
+	
+	auto test_creator = [](const Data& data) {
+		return new std::string(Data::cast<String>(Data::cast<Object>(data).access("data")).get());
+	};
+	auto test_copier = [](const std::string& data) {
+		return new std::string(data);
+	};
+	auto test_reader = [](const std::string& instance) {
+		Object data;
+		data.add("data", String(instance));
+		return data;
+	};
+	auto test_writer = [](std::string *instance_ptr, const Data& data) {
+		*instance_ptr = Data::cast<String>(Data::cast<Object>(data).access("data")).get();
+	};
+
+	Body create_instance_data;
+	create_instance_data.add("id", String("instance_id"));
+	create_instance_data.add("data", String("instance_data"));
+
+	Body update_instance_data;
+	update_instance_data.add("data", String("updated_instance_data"));
+	
+	// WHEN
+	TestManager instance(test_creator, test_copier, test_reader, test_writer);
+	Response response(ResponseCode::OK, Body());
+
+	// THEN
+	// Create
+	ASSERT_NO_THROW(
+		response = instance.run_request(
+			Request(
+				Method::CREATE, 
+				{}, 
+				create_instance_data
+			)
+		)
+	);
+	ASSERT_EQ(ResponseCode::OK, response.code());
+	
+	// ReadAll
+	ASSERT_NO_THROW(
+		response = instance.run_request(
+			Request(
+				Method::READ, 
+				{}, 
+				Body()
+			)
+		)
+	);
+	ASSERT_EQ(ResponseCode::OK, response.code());
+	ASSERT_TRUE(response.body().contains("members"));
+	ASSERT_EQ(Data::Type::ARRAY, response.body().access("members").type());
+	ASSERT_EQ(1UL, Data::cast<Array>(response.body().access("members")).size());
+
+	// Read
+	ASSERT_NO_THROW(
+		response = instance.run_request(
+			Request(
+				Method::READ, 
+				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+				Body()
+			)
+		)
+	);
+	ASSERT_EQ(ResponseCode::OK, response.code());
+	ASSERT_TRUE(response.body().contains("id"));
+	ASSERT_EQ(Data::cast<String>(create_instance_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
+	
+	ASSERT_TRUE(response.body().contains("data"));
+	ASSERT_EQ(Data::cast<String>(create_instance_data.access("data")).get(), Data::cast<String>(response.body().access("data")).get());
+
+	// Update
+	ASSERT_NO_THROW(
+		response = instance.run_request(
+			Request(
+				Method::UPDATE, 
+				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+				update_instance_data
+			)
+		)
+	);
+	ASSERT_EQ(ResponseCode::OK, response.code());
+	// Update validation
+	ASSERT_NO_THROW(
+		response = instance.run_request(
+			Request(
+				Method::READ, 
+				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+				Body()
+			)
+		)
+	);
+	ASSERT_EQ(ResponseCode::OK, response.code());
+	ASSERT_TRUE(response.body().contains("id"));
+	ASSERT_EQ(Data::cast<String>(create_instance_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
+	
+	ASSERT_TRUE(response.body().contains("data"));
+	ASSERT_EQ(Data::cast<String>(update_instance_data.access("data")).get(), Data::cast<String>(response.body().access("data")).get());
+
+	// Delete
+	ASSERT_NO_THROW(
+		response = instance.run_request(
+			Request(
+				Method::DELETE, 
+				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+				Body()
+			)
+		)
+	);
+	ASSERT_EQ(ResponseCode::OK, response.code());
+	
+	// Delete validation
+	ASSERT_NO_THROW(
+		response = instance.run_request(
+			Request(
+				Method::READ, 
+				{Data::cast<String>(create_instance_data.access("id")).get()}, 
+				Body()
+			)
+		)
+	);
+	ASSERT_NE(ResponseCode::OK, response.code());
+}
