01ba5140a6e97795e52a5034f5f7f203d1f51934
fedddot <idanrommm@gmail.com>
2024-10-02 17:37:36 +0300

    test updated

--- manager/stepper_motor_manager/tests/src/ut_stepper_motor_manager.cpp
+++ manager/stepper_motor_manager/tests/src/ut_stepper_motor_manager.cpp
@@ -4,45 +4,40 @@
 #include "gtest/gtest.h"
 
 #include "data.hpp"
-#include "gpio.hpp"
-#include "gpio_manager.hpp"
-#include "integer.hpp"
-#include "object.hpp"
+#include "stepper_motor.hpp"
+#include "stepper_motor_manager.hpp"
 #include "request.hpp"
 #include "response.hpp"
-#include "string.hpp"
-#include "test_gpi.hpp"
-#include "test_gpo.hpp"
 
 using namespace server;
 using namespace manager;
-using namespace manager_uts;
+// using namespace manager_uts;
 
 using Method = typename Request::Method;
 using Path = typename Request::Path;
 using Body = typename Request::Body;
 using ResponseCode = typename Response::ResponseCode;	
 
-TEST(ut_gpio_manager, ctor_cctor_clone_dtor_id_sanity) {
+TEST(ut_stepper_motor_manager, ctor_cctor_clone_dtor_id_sanity) {
 	// GIVEN
-	auto test_creator = [](const Data& data)-> Gpio * {
+	auto test_creator = [](const Data& data)-> StepperMotor * {
 		throw std::runtime_error("NOT IMPLEMENTED");
 	};
 
 	// WHEN
-	using GpioManagerUnqPtr = std::unique_ptr<GpioManager>;
+	using StepperMotorManagerUnqPtr = std::unique_ptr<StepperMotorManager>;
 	using ResourceUnqPtr = std::unique_ptr<Resource>;
-	GpioManagerUnqPtr instance_ptr(nullptr);
-	GpioManagerUnqPtr instance_ptr_copy(nullptr);
+	StepperMotorManagerUnqPtr instance_ptr(nullptr);
+	StepperMotorManagerUnqPtr instance_ptr_copy(nullptr);
 	ResourceUnqPtr instance_ptr_clone(nullptr);
 	
 	// THEN
 	// ctor
-	ASSERT_NO_THROW(instance_ptr = GpioManagerUnqPtr(new GpioManager(test_creator)));
+	ASSERT_NO_THROW(instance_ptr = StepperMotorManagerUnqPtr(new StepperMotorManager(test_creator)));
 	ASSERT_NE(nullptr, instance_ptr);
 
 	// cctor
-	ASSERT_NO_THROW(instance_ptr_copy = GpioManagerUnqPtr(new GpioManager(*instance_ptr)));
+	ASSERT_NO_THROW(instance_ptr_copy = StepperMotorManagerUnqPtr(new StepperMotorManager(*instance_ptr)));
 	ASSERT_NE(nullptr, instance_ptr_copy);
 
 	// clone
@@ -55,139 +50,139 @@ TEST(ut_gpio_manager, ctor_cctor_clone_dtor_id_sanity) {
 	ASSERT_NO_THROW(instance_ptr_clone = nullptr);
 }
 
-static inline Gpio *test_gpio_ctor(const Data& data) {
-	auto dir = static_cast<Gpio::Direction>(Data::cast<Integer>(Data::cast<Object>(data).access("dir")).get());
-	switch (dir) {
-	case Gpio::Direction::IN:
-		return new TestGpi();
-	case Gpio::Direction::OUT:
-		return new TestGpo();
-	default:
-		throw std::invalid_argument("unsupported Gpio direction received");
-	}
-}
+// static inline StepperMotor *test_stepper_motor_ctor(const Data& data) {
+// 	auto dir = static_cast<StepperMotor::Direction>(Data::cast<Integer>(Data::cast<Object>(data).access("dir")).get());
+// 	switch (dir) {
+// 	case StepperMotor::Direction::IN:
+// 		return new TestGpi();
+// 	case StepperMotor::Direction::OUT:
+// 		return new TestGpo();
+// 	default:
+// 		throw std::invalid_argument("unsupported StepperMotor direction received");
+// 	}
+// }
 
-TEST(ut_gpio_manager, run_request_sanity) {
-	// GIVEN
-	Body create_gpo_data;
-	create_gpo_data.add("id", String("gpo_id"));
-	create_gpo_data.add("dir", Integer(static_cast<int>(Gpio::Direction::OUT)));
+// TEST(ut_stepper_motor_manager, run_request_sanity) {
+// 	// GIVEN
+// 	Body create_gpo_data;
+// 	create_gpo_data.add("id", String("gpo_id"));
+// 	create_gpo_data.add("dir", Integer(static_cast<int>(StepperMotor::Direction::OUT)));
 
-	Body update_gpo_data;
-	update_gpo_data.add("state", Integer(static_cast<int>(Gpio::State::HIGH)));
+// 	Body update_gpo_data;
+// 	update_gpo_data.add("state", Integer(static_cast<int>(StepperMotor::State::HIGH)));
 
-	Body create_gpi_data;
-	create_gpi_data.add("id", String("gpi_id"));
-	create_gpi_data.add("dir", Integer(static_cast<int>(Gpio::Direction::IN)));
+// 	Body create_gpi_data;
+// 	create_gpi_data.add("id", String("gpi_id"));
+// 	create_gpi_data.add("dir", Integer(static_cast<int>(StepperMotor::Direction::IN)));
 	
-	// WHEN
-	GpioManager instance(test_gpio_ctor);
-	Response response(ResponseCode::OK, Body());
+// 	// WHEN
+// 	StepperMotorManager instance(test_stepper_motor_ctor);
+// 	Response response(ResponseCode::OK, Body());
 
-	// THEN
-	// Create GPO
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::CREATE, 
-				{}, 
-				create_gpo_data
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	// THEN
+// 	// Create GPO
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::CREATE, 
+// 				{}, 
+// 				create_gpo_data
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
 
-	// Create GPI
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::CREATE, 
-				{}, 
-				create_gpi_data
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	// Create GPI
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::CREATE, 
+// 				{}, 
+// 				create_gpi_data
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
 
-	// ReadAll
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::READ, 
-				{}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-	ASSERT_TRUE(response.body().contains("members"));
-	ASSERT_EQ(Data::Type::ARRAY, response.body().access("members").type());
-	ASSERT_EQ(2UL, Data::cast<Array>(response.body().access("members")).size());
+// 	// ReadAll
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::READ, 
+// 				{}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	ASSERT_TRUE(response.body().contains("members"));
+// 	ASSERT_EQ(Data::Type::ARRAY, response.body().access("members").type());
+// 	ASSERT_EQ(2UL, Data::cast<Array>(response.body().access("members")).size());
 
-	// Update
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::UPDATE, 
-				{Data::cast<String>(create_gpo_data.access("id")).get()}, 
-				update_gpo_data
-			)
-		)
-	);
+// 	// Update
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::UPDATE, 
+// 				{Data::cast<String>(create_gpo_data.access("id")).get()}, 
+// 				update_gpo_data
+// 			)
+// 		)
+// 	);
 	
-	// Read GPI
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::READ, 
-				{Data::cast<String>(create_gpi_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-	ASSERT_TRUE(response.body().contains("id"));
-	ASSERT_EQ(Data::cast<String>(create_gpi_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
-	ASSERT_TRUE(response.body().contains("state"));
-	ASSERT_EQ(Gpio::State::LOW, static_cast<Gpio::State>(Data::cast<Integer>(response.body().access("state")).get()));
+// 	// Read GPI
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::READ, 
+// 				{Data::cast<String>(create_gpi_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	ASSERT_TRUE(response.body().contains("id"));
+// 	ASSERT_EQ(Data::cast<String>(create_gpi_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
+// 	ASSERT_TRUE(response.body().contains("state"));
+// 	ASSERT_EQ(StepperMotor::State::LOW, static_cast<StepperMotor::State>(Data::cast<Integer>(response.body().access("state")).get()));
 
-	// Read GPO
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::READ, 
-				{Data::cast<String>(create_gpo_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-	ASSERT_TRUE(response.body().contains("id"));
-	ASSERT_EQ(Data::cast<String>(create_gpo_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
-	ASSERT_TRUE(response.body().contains("state"));
-	ASSERT_EQ(Gpio::State::HIGH, static_cast<Gpio::State>(Data::cast<Integer>(response.body().access("state")).get()));
+// 	// Read GPO
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::READ, 
+// 				{Data::cast<String>(create_gpo_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	ASSERT_TRUE(response.body().contains("id"));
+// 	ASSERT_EQ(Data::cast<String>(create_gpo_data.access("id")).get(), Data::cast<String>(response.body().access("id")).get());
+// 	ASSERT_TRUE(response.body().contains("state"));
+// 	ASSERT_EQ(StepperMotor::State::HIGH, static_cast<StepperMotor::State>(Data::cast<Integer>(response.body().access("state")).get()));
 
-	// Delete GPI
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::DELETE, 
-				{Data::cast<String>(create_gpi_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
+// 	// Delete GPI
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::DELETE, 
+// 				{Data::cast<String>(create_gpi_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
 
-	// Delete GPO
-	ASSERT_NO_THROW(
-		response = instance.run_request(
-			Request(
-				Method::DELETE, 
-				{Data::cast<String>(create_gpo_data.access("id")).get()}, 
-				Body()
-			)
-		)
-	);
-	ASSERT_EQ(ResponseCode::OK, response.code());
-}
+// 	// Delete GPO
+// 	ASSERT_NO_THROW(
+// 		response = instance.run_request(
+// 			Request(
+// 				Method::DELETE, 
+// 				{Data::cast<String>(create_gpo_data.access("id")).get()}, 
+// 				Body()
+// 			)
+// 		)
+// 	);
+// 	ASSERT_EQ(ResponseCode::OK, response.code());
+// }
