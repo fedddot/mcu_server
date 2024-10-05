c8057161ffa89d7d302e4a1256d5e49c9470e194
fedddot <idanrommm@gmail.com>
2024-10-05 14:37:40 +0300

    updated

--- util/include/json_data_parser.hpp
+++ util/include/json_data_parser.hpp
@@ -4,32 +4,26 @@
 #include <string>
 
 #include "data.hpp"
-#include "parser.hpp"
 
 namespace Json {
 	class Value;
 }
 
-namespace mcu_server_utl {
-	class JsonDataParser: public mcu_server::Parser<mcu_server::Data *(const std::string&)> {
+namespace server_utl {
+	class JsonDataParser {
 	public:
 		JsonDataParser() = default;
 		JsonDataParser(const JsonDataParser& other) = default;
 		JsonDataParser& operator=(const JsonDataParser& other) = default;
 		
-		mcu_server::Data *parse(const std::string& data) const override;
-		mcu_server::Parser<mcu_server::Data *(const std::string&)> *clone() const override;
+		server::Data *operator()(const std::string& data) const;
 	private:
-		static mcu_server::Data *parseValue(const Json::Value& root);
-		static mcu_server::Data *parseObject(const Json::Value& root);
-		static mcu_server::Data *parseArray(const Json::Value& root);
-		static mcu_server::Data *parseString(const Json::Value& root);
-		static mcu_server::Data *parseInteger(const Json::Value& root);
+		static server::Data *parseValue(const Json::Value& root);
+		static server::Data *parseObject(const Json::Value& root);
+		static server::Data *parseArray(const Json::Value& root);
+		static server::Data *parseString(const Json::Value& root);
+		static server::Data *parseInteger(const Json::Value& root);
 	};
-
-	inline mcu_server::Parser<mcu_server::Data *(const std::string&)> *JsonDataParser::clone() const {
-		return new JsonDataParser(*this);
-	}
 }
 
 #endif // JSON_DATA_PARSER_HPP
