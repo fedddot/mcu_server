9b53aba8a051eede0072c76d7346805a51d18b1a
fedddot <idanrommm@gmail.com>
2024-10-05 12:52:37 +0300

    templatized

--- ipc/include/ipc_connection.hpp
+++ ipc/include/ipc_connection.hpp
@@ -2,20 +2,17 @@
 #define	IPC_CONNECTION_HPP
 
 #include <functional>
-#include <string>
-
-#include "request.hpp"
-#include "response.hpp"
 
 namespace ipc {
+	template <typename Tsubscriber_id, typename Tincoming, typename Toutgoing>
 	class IpcConnection {
 	public:
-		using Callback = std::function<void(const server::Request& request)>;
+		using Callback = std::function<void(const Tincoming& incoming_data)>;
 		virtual ~IpcConnection() noexcept = default;
-		virtual void subscribe(const std::string& id, const Callback& cb) = 0;
-		virtual void unsubscribe(const std::string& id) = 0;
-		virtual bool is_subscribed(const std::string& id) const = 0;
-		virtual void send(const server::Response& response) const = 0;
+		virtual void subscribe(const Tsubscriber_id& id, const Callback& cb) = 0;
+		virtual void unsubscribe(const Tsubscriber_id& id) = 0;
+		virtual bool is_subscribed(const Tsubscriber_id& id) const = 0;
+		virtual void send(const Toutgoing& outgoing_data) const = 0;
 	};
 }
 
