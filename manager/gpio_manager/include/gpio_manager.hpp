13e1abd1c60fb9d688fa51b39beac62d8ecd972f
fedddot <idanrommm@gmail.com>
2024-10-18 16:29:39 +0300

    gpio manager updated

--- manager/gpio_manager/include/gpio_manager.hpp
+++ manager/gpio_manager/include/gpio_manager.hpp
@@ -3,147 +3,120 @@
 
 #include <functional>
 #include <memory>
+#include <stdexcept>
 #include <string>
 
+#include "gpi.hpp"
 #include "gpio.hpp"
+#include "gpo.hpp"
+#include "integer.hpp"
 #include "inventory.hpp"
-#include "inventory_manager.hpp"
-#include "request.hpp"
-#include "resource.hpp"
-#include "response.hpp"
+#include "manager.hpp"
+#include "object.hpp"
+#include "server_exception.hpp"
+#include "server_types.hpp"
 #include "string.hpp"
 
 namespace manager {
-	template <typename Tid>
-	class GpioManager: public InventoryManager<Tid, Gpio> {
+	class GpioManager: public server::Manager {
 	public:
-		using GpioCreator = std::function<Gpio *(const server::Request::Body&)>;
-		using GpioIdFromBodyRetriever = std::function<Tid(const server::Request::Body&)>;
-		using GpioIdFromPathRetriever = std::function<Tid(const server::Request::Path&)>;
-		using GpioReader = std::function<server::Response::Body(const Gpio&)>;
-		using GpioWriter = std::function<void(Gpio *, const server::Request::Body&)>;
-
-		GpioManager(
-			Inventory<Tid, Gpio> *gpio_inventory,
-			const GpioCreator& gpio_creator,
-			const GpioIdFromBodyRetriever& gpio_id_from_body_retriever,
-			const GpioIdFromPathRetriever& gpio_id_from_path_retriever,
-			const GpioReader& gpio_reader,
-			const GpioWriter& gpio_writer
-		);
-		GpioManager(const GpioManager& other) = default;
+		using GpioCreator = std::function<Gpio *(const server::Body&)>;
+		GpioManager(Inventory<server::ResourceId, Gpio> *gpio_inventory, const GpioCreator& gpio_creator);
+		GpioManager(const GpioManager& other) = delete;
 		GpioManager& operator=(const GpioManager&) = delete;
-		server::Resource *clone() const override;
+
+		void create_resource(const server::Body& create_request_body) override;
+		server::Body read_resource(const server::Path& route) const override;
+		server::Body read_all_resources() const override;
+		void update_resource(const server::Path& route, const server::Body& update_request_body) override;
+		void delete_resource(const server::Path& route) override;
+		bool contains(const server::Path& route) const override;
 	private:
+		Inventory<server::ResourceId, Gpio> *m_gpio_inventory;
 		GpioCreator m_gpio_creator;
-		GpioIdFromBodyRetriever m_gpio_id_from_body_retriever;
-		GpioIdFromPathRetriever m_gpio_id_from_path_retriever;
-		GpioReader m_gpio_reader;
-		GpioWriter m_gpio_writer;
 
-		server::Response handle_request(Inventory<Tid, Gpio> *inventory, const server::Request& request) const;
-		server::Response create_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const;
-		server::Response read_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const;
-		server::Response update_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const;
-		server::Response delete_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const;
-		
-		server::Response report_failure(const server::Response::ResponseCode& code, const std::string& what) const;
+		static server::ResourceId get_id_from_route(const server::Path& route);
 	};
-
-	template <typename Tid>
-	inline GpioManager<Tid>::GpioManager(
-		Inventory<Tid, Gpio> *gpio_inventory,
-		const GpioCreator& gpio_creator,
-		const GpioIdFromBodyRetriever& gpio_id_from_body_retriever,
-		const GpioIdFromPathRetriever& gpio_id_from_path_retriever,
-		const GpioReader& gpio_reader,
-		const GpioWriter& gpio_writer
-	): InventoryManager<Tid, Gpio>(
-		gpio_inventory,
-		[this](Inventory<Tid, Gpio> *inventory, const server::Request& request) {
-			return handle_request(inventory, request);
+	
+	inline GpioManager::GpioManager(Inventory<server::ResourceId, Gpio> *gpio_inventory, const GpioCreator& gpio_creator): m_gpio_inventory(gpio_inventory), m_gpio_creator(gpio_creator) {
+		if (!m_gpio_inventory || !m_gpio_creator) {
+			throw std::invalid_argument("invalid arguments received");
 		}
-	), m_gpio_creator(gpio_creator), m_gpio_id_from_body_retriever(gpio_id_from_body_retriever), m_gpio_id_from_path_retriever(gpio_id_from_path_retriever), m_gpio_reader(gpio_reader), m_gpio_writer(gpio_writer) {
-
-	}
-
-	template <typename Tid>
-	inline server::Response GpioManager<Tid>::create_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const {
-		using ResponseCode = typename server::Response::ResponseCode;
-		if (!request.path().empty()) {
-			return report_failure(ResponseCode::BAD_REQUEST, "create gpio handler requires an empty path relatively to the manager");
-		}
-		auto id(m_gpio_id_from_body_retriever(request.body()));
-		if (inventory->contains(id)) {
-			return report_failure(ResponseCode::BAD_REQUEST, "gpio with specified id already exists");
-		}
-		inventory->add(id, m_gpio_creator(request.body()));
-		return server::Response(ResponseCode::OK, server::Response::Body());
-	}
-
-	template <typename Tid>
-	inline server::Response GpioManager<Tid>::read_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const {
-		using ResponseCode = typename server::Response::ResponseCode;
-		const auto gpio_id(m_gpio_id_from_path_retriever(request.path()));
-		if (!(inventory->contains(gpio_id))) {
-			return report_failure(ResponseCode::NOT_FOUND, "gpio with specified id doesn't exist");
-		}
-		auto body(m_gpio_reader(inventory->access(gpio_id)));
-		return server::Response(ResponseCode::OK, body);
-	}
-
-	template <typename Tid>
-	inline server::Response GpioManager<Tid>::update_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const {
-		using ResponseCode = typename server::Response::ResponseCode;
-		const auto gpio_id(m_gpio_id_from_path_retriever(request.path()));
-		if (!(inventory->contains(gpio_id))) {
-			return report_failure(ResponseCode::NOT_FOUND, "gpio with specified id doesn't exist");
-		}
-		m_gpio_writer(&(inventory->access(gpio_id)), request.body());
-		return server::Response(ResponseCode::OK, server::Response::Body());
-	}
-
-	template <typename Tid>
-	inline server::Response GpioManager<Tid>::delete_gpio(Inventory<Tid, Gpio> *inventory, const server::Request& request) const {
-		using ResponseCode = typename server::Response::ResponseCode;
-		const auto gpio_id(m_gpio_id_from_path_retriever(request.path()));
-		if (!(inventory->contains(gpio_id))) {
-			return report_failure(ResponseCode::NOT_FOUND, "gpio with specified id doesn't exist");
-		}
-		std::unique_ptr<Gpio> removed_gpio(inventory->remove(gpio_id));
-		removed_gpio = nullptr;
-		return server::Response(ResponseCode::OK, server::Response::Body());
-	}
-
-	template <typename Tid>
-	inline server::Response GpioManager<Tid>::handle_request(Inventory<Tid, Gpio> *inventory, const server::Request& request) const {
-		using Method = typename server::Request::Method;
-		using ResponseCode = typename server::Response::ResponseCode;
-
-		switch (request.method()) {
-		case Method::CREATE:
-			return create_gpio(inventory, request);
-		case Method::READ:
-			return read_gpio(inventory, request);
-		case Method::UPDATE:
-			return update_gpio(inventory, request);
-		case Method::DELETE:
-			return delete_gpio(inventory, request);
-		default:
-			return report_failure(ResponseCode::BAD_REQUEST, "unsupported method");
-		}
-	}
-
-	template <typename Tid>
-	inline server::Response GpioManager<Tid>::report_failure(const server::Response::ResponseCode& code, const std::string& what) const {
-		server::Response::Body body;
-		body.add("what", server::String(what));
-		return server::Response(code, body);
 	}
 	
-	template <typename Tid>
-	inline server::Resource *GpioManager<Tid>::clone() const {
-		return new GpioManager(*this);
+	inline void GpioManager::create_resource(const server::Body& create_request_body) {
+		using namespace server;
+		auto id(static_cast<ResourceId>(Data::cast<String>(create_request_body.access("id")).get()));
+		if (m_gpio_inventory->contains(id)) {
+			throw ServerException(ResponseCode::BAD_REQUEST, "gpio with id " + id + " already exists");
+		}
+		m_gpio_inventory->add(id, m_gpio_creator(create_request_body));
+	}
+	
+	inline server::Body GpioManager::read_resource(const server::Path& route) const {
+		using namespace server;
+		const auto gpio_id(get_id_from_route(route));
+		if (!(m_gpio_inventory->contains(gpio_id))) {
+			throw ServerException(ResponseCode::NOT_FOUND, "gpio with specified id doesn't exist");
+		}
+		const auto& gpio(m_gpio_inventory->access(gpio_id));
+		Body response_body;
+		response_body.add("dir", Integer(static_cast<int>(gpio.direction())));
+		switch (gpio.direction()) {
+		case Gpio::Direction::IN:
+			response_body.add("state", Integer(static_cast<int>(Gpio::cast<Gpi>(gpio).state())));
+		case Gpio::Direction::OUT:
+			response_body.add("state", Integer(static_cast<int>(Gpio::cast<Gpo>(gpio).state())));
+		default:
+			throw ServerException(ResponseCode::UNSPECIFIED, "unsupported gpio direction");
+		}
+		return response_body;
+	}
+
+	inline server::Body GpioManager::read_all_resources() const {
+		using namespace server;
+		Body response_body;
+		for (const auto& id: m_gpio_inventory->ids()) {
+			response_body.add(id, read_resource({id}));
+		}
+		return response_body;
+	}
+
+	inline void GpioManager::update_resource(const server::Path& route, const server::Body& update_body) {
+		using namespace server;
+		const auto gpio_id(get_id_from_route(route));
+		if (!(m_gpio_inventory->contains(gpio_id))) {
+			throw ServerException(ResponseCode::NOT_FOUND, "gpio with specified id doesn't exist");
+		}
+		auto& gpio(m_gpio_inventory->access(gpio_id));
+		if (Gpio::Direction::OUT != gpio.direction()) {
+			throw ServerException(ResponseCode::BAD_REQUEST, "specified gpio is not an output");
+		}
+		const auto state(static_cast<Gpio::State>(Data::cast<Integer>(Data::cast<Object>(update_body.access("config")).access("state")).get()));
+		Gpio::cast<Gpo>(gpio).set_state(state);
+	}
+	
+	inline void GpioManager::delete_resource(const server::Path& route) {
+		using namespace server;
+		const auto gpio_id(get_id_from_route(route));
+		if (!(m_gpio_inventory->contains(gpio_id))) {
+			throw ServerException(ResponseCode::NOT_FOUND, "gpio with specified id doesn't exist");
+		}
+		std::unique_ptr<Gpio> gpio_ptr(m_gpio_inventory->remove(gpio_id));
+		gpio_ptr = nullptr;
+	}
+
+	inline bool GpioManager::contains(const server::Path& route) const {
+		using namespace server;
+		return m_gpio_inventory->contains(get_id_from_route(route));
+	}
+
+	inline server::ResourceId GpioManager::get_id_from_route(const server::Path& route) {
+		using namespace server;
+		if (1UL != route.size()) {
+			throw ServerException(ResponseCode::BAD_REQUEST, "bad route received");
+		}
+		return route[0];
 	}
 }
 
--- manager/gpio_manager/tests/src/ut_gpio_manager.cpp
+++ manager/gpio_manager/tests/src/ut_gpio_manager.cpp
@@ -1,134 +1,72 @@
-#include <functional>
 #include <memory>
-#include <stdexcept>
-#include <vector>
 
 #include "gtest/gtest.h"
 
-#include "array.hpp"
-#include "data.hpp"
+#include "gpio.hpp"
 #include "in_memory_inventory.hpp"
 #include "integer.hpp"
 #include "object.hpp"
 #include "gpio_manager.hpp"
-#include "request.hpp"
-#include "resource.hpp"
-#include "response.hpp"
-#include "string.hpp"
+#include "server_exception.hpp"
+#include "server_types.hpp"
+#include "test_gpi.hpp"
+#include "test_gpo.hpp"
 
 using namespace server;
 using namespace manager;
+using namespace manager_uts;
 
-using TestId = int;
+static Gpio *create_test_gpio(const Body& request_body) {
+	const auto direction(static_cast<Gpio::Direction>(Data::cast<Integer>(Data::cast<Object>(request_body.access("config")).access("dir")).get()));
+	switch (direction) {
+	case Gpio::Direction::IN:
+		return new TestGpi();
+	case Gpio::Direction::OUT:
+		return new TestGpo();
+	default:
+		throw ServerException(ResponseCode::BAD_REQUEST, "unsupported direction received");
+	}
+}
 
-TEST(ut_gpio_manager, ctor_cctor_clone_dtor_id_sanity) {
+TEST(ut_gpio_manager, ctor_dtor_sanity) {
 	// GIVEN
-	InMemoryInventory<TestId, Gpio> inventory;
+	InMemoryInventory<ResourceId, Gpio> inventory;
 
 	// WHEN
-	using GpioManagerUnqPtr = std::unique_ptr<GpioManager<TestId>>;
-	using ResourceUnqPtr = std::unique_ptr<Resource>;
+	using GpioManagerUnqPtr = std::unique_ptr<GpioManager>;
 	GpioManagerUnqPtr instance_ptr(nullptr);
-	GpioManagerUnqPtr instance_ptr_copy(nullptr);
-	ResourceUnqPtr instance_ptr_clone(nullptr);
 	
 	// THEN
 	// ctor
-	ASSERT_NO_THROW(
-		instance_ptr = GpioManagerUnqPtr(
-			new GpioManager<TestId>(
-				&inventory,
-				[](const Request::Body& body) -> Gpio * {
-					throw std::runtime_error("NOT IMPLEMENTED");
-				},
-				[](const Request::Body& body) -> TestId {
-					throw std::runtime_error("NOT IMPLEMENTED");
-				},
-				[](const Request::Path& path) -> TestId {
-					throw std::runtime_error("NOT IMPLEMENTED");
-				},
-				[](const Gpio&) -> Response::Body {
-					throw std::runtime_error("NOT IMPLEMENTED");
-				},
-				[](Gpio *, const Request::Body&) {
-					throw std::runtime_error("NOT IMPLEMENTED");
-				}
-			)
-		)
-	);
+	ASSERT_NO_THROW(instance_ptr = GpioManagerUnqPtr(new GpioManager(&inventory, create_test_gpio)));
 	ASSERT_NE(nullptr, instance_ptr);
 
-	// cctor
-	ASSERT_NO_THROW(instance_ptr_copy = GpioManagerUnqPtr(new GpioManager(*instance_ptr)));
-	ASSERT_NE(nullptr, instance_ptr_copy);
-
-	// clone
-	ASSERT_NO_THROW(instance_ptr_clone = ResourceUnqPtr(instance_ptr->clone()));
-	ASSERT_NE(nullptr, instance_ptr_clone);
-
-	// dtors
+	// dtor
 	ASSERT_NO_THROW(instance_ptr = nullptr);
-	ASSERT_NO_THROW(instance_ptr_copy = nullptr);
-	ASSERT_NO_THROW(instance_ptr_clone = nullptr);
 }
 
-TEST(ut_gpio_manager, run_request_sanity) {
+TEST(ut_gpio_manager, crud_methods_sanity) {
 	// GIVEN
-	InMemoryInventory<TestId, Gpio> inventory;
-	auto gpio_ctor = [](const Request::Body& body) -> Gpio * {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
-	auto id_from_body_retriever = [](const Request::Body& body) -> TestId {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
-	auto id_from_path_retriever = [](const Request::Path& path) -> TestId {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
-	auto gpio_reader = [](const Gpio&) -> Response::Body {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
-	auto gpio_writer = [](Gpio *, const Request::Body&) {
-		throw std::runtime_error("NOT IMPLEMENTED");
-	};
+	const auto dir(Gpio::Direction::OUT);
+	const auto id(ResourceId("test_gpo"));
+	const auto state(Gpio::State::HIGH);
 
-	const TestId test_id(10);
-	Object gpio_config;
-	gpio_config.add("dir", Integer(static_cast<int>(Gpio::Direction::OUT)));
-	gpio_config.add("pin_number", Integer(test_id));
-	Request::Body create_body;
-	create_body.add("id", Integer(test_id));
-	create_body.add("config", gpio_config);
-	const Request create_request(Request::Method::CREATE, {}, create_body);
+	Object create_cfg;
+	create_cfg.add("dir", Integer(static_cast<int>(dir)));
+	
+	Body create_body;
+	create_body.add("id", String(id));
+	create_body.add("config", create_cfg);
 
 	// WHEN
-	TestVendor vendor(
-		[test_config, data_to_method, data_to_path](const Gpio& request) -> Response {
-			if (request.method() != data_to_method(test_config.access("method"))) {
-				throw std::runtime_error("");
-			}
-			if (request.path() != data_to_path(test_config.access("path"))) {
-				throw std::runtime_error("");
-			}
-			return Response(Response::ResponseCode::OK, Response::Body());
-		}
-	);
-
-	GpioManager<TestId> instance(
-		&inventory,
-		&vendor,
-		request_retriever,
-		id_from_body_retriever,
-		id_from_path_retriever,
-		ids_retriever,
-		request_reader
-	);
-	Response response(Response::ResponseCode::UNSPECIFIED, Response::Body());
+	InMemoryInventory<ResourceId, Gpio> inventory;
+	GpioManager instance(&inventory, create_test_gpio);
 
 	// THEN
-	ASSERT_NO_THROW(response = instance.run_request(test_request));
-	ASSERT_EQ(Response::ResponseCode::OK, response.code());
-	ASSERT_NO_THROW(response = instance.run_request(update_request));
-	ASSERT_EQ(Response::ResponseCode::OK, response.code());
-	ASSERT_NO_THROW(response = instance.run_request(delete_request));
-	ASSERT_EQ(Response::ResponseCode::OK, response.code());
+	ASSERT_NO_THROW(instance.create_resource(create_body));
+	ASSERT_TRUE(inventory.contains(id));
+	ASSERT_EQ(dir, inventory.access(id).direction());
+
+	ASSERT_NO_THROW(instance.delete_resource({id}));
+	ASSERT_FALSE(inventory.contains(id));
 }
