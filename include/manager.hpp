37119974595c6b2ea17c1f1db658d8d74a0ea079
fedddot <idanrommm@gmail.com>
2024-10-24 10:29:42 +0300

    manager updated

--- include/manager.hpp
+++ include/manager.hpp
@@ -1,16 +1,18 @@
 #ifndef	MANAGER_HPP
 #define	MANAGER_HPP
 
+#include "data.hpp"
+#include "object.hpp"
 #include "server_types.hpp"
 
 namespace server {
 	class Manager {
 	public:
 		virtual ~Manager() noexcept = default;
-		virtual void create_resource(const Body& create_request_body) = 0;
-		virtual Body read_resource(const Path& route) const = 0;
-		virtual Body read_all_resources() const = 0;
-		virtual void update_resource(const Path& route, const Body& update_request_body) = 0;
+		virtual void create_resource(const Data& create_cfg) = 0;
+		virtual Object read_resource(const Path& route) const = 0;
+		virtual Object read_all_resources() const = 0;
+		virtual void update_resource(const Path& route, const Data& update_cfg) = 0;
 		virtual void delete_resource(const Path& route) = 0;
 		virtual bool contains(const Path& route) const = 0;
 	};
