49b47aee799f36d1ab15ecb8afce2656687f79f9
fedddot <idanrommm@gmail.com>
2024-10-24 16:43:33 +0300

    tests updated

--- manager/movement_manager/tests/include/test_movement.hpp
+++ manager/movement_manager/tests/include/test_movement.hpp
@@ -11,7 +11,7 @@ namespace manager_uts {
 	class TestMovement: public manager::Movement {
 	public:
 		using Action = std::function<void(const server::Data&)>;
-		TestMovement(const Action& action): m_action(action) {
+		TestMovement(const Action& action, const Type& type): m_action(action), m_type(type) {
 			if (!m_action) {
 				throw std::invalid_argument("invalid action received");
 			}
@@ -19,8 +19,12 @@ namespace manager_uts {
 		void perform(const server::Data& cfg) override {
 			m_action(cfg);
 		}
+		Type type() const override {
+			return m_type;
+		}
 	private:
 		Action m_action;
+		Type m_type;
 	};
 }
 
