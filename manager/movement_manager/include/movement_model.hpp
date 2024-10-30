45ca245d11cfa95191c519236b066184ff296dc5
fedddot <idanrommm@gmail.com>
2024-10-30 11:31:32 +0200

    movement model added

---
+++ manager/movement_manager/include/movement_model.hpp
@@ -0,0 +1,15 @@
+#ifndef	MOVEMENT_MODEL_HPP
+#define	MOVEMENT_MODEL_HPP
+
+#include "vector.hpp"
+
+namespace manager {
+	template <typename T, typename Ttime>
+	class MovementModel {
+	public:
+		virtual ~MovementModel() noexcept = default;
+		virtual Vector<T> evaluate(const Ttime& time) = 0;
+	};
+}
+
+#endif // MOVEMENT_MODEL_HPP
