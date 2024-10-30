128ba8cfc452a9d56899e3e2152f597cad948b41
fedddot <idanrommm@gmail.com>
2024-10-30 17:07:50 +0200

    updated

--- manager/movement_manager/tests/src/ut_circular_movement_model.cpp
+++ manager/movement_manager/tests/src/ut_circular_movement_model.cpp
@@ -37,18 +37,26 @@ TEST(ut_circular_movement_model, ctor_dtor_sanity) {
 	ASSERT_NO_THROW(instance_ptr = nullptr);
 }
 
+static inline void print_vector(const Vector<LengthUnit>& vector) {
+	using Axis = typename Vector<LengthUnit>::Axis;
+	std::cout << "(" << vector.projection(Axis::X) << ", " << vector.projection(Axis::Y) << ", " << vector.projection(Axis::Z) << ")" << std::endl;   
+}
+
 TEST(ut_circular_movement_model, evaluate_sanity) {
 	// GIVEN
-	const Vector<LengthUnit> target(10, 20, 30);
-	const Vector<LengthUnit> rotation_center(5, 10, 20);
+	const Vector<LengthUnit> target(10, 10, 0);
+	const Vector<LengthUnit> rotation_center(10, 0, 0);
 	const TestMovement::Direction direction(TestMovement::Direction::CCW);
 	const SpeedUnit speed(3); // 3 length units / 1 time unit
-	const TimeUnit time(2);
+	const TimeUnit times[] {0, 1, 2, 3, 4, 5};
 
 	// WHEN
 	TestMovement instance(target, rotation_center, direction, speed);
 	Vector<LengthUnit> result(0, 0, 0);
 	
 	// THEN
-	ASSERT_NO_THROW(result = instance.evaluate(time));
+	for (const auto& time: times) {
+		ASSERT_NO_THROW(result = instance.evaluate(time));
+		print_vector(result);
+	}
 }
