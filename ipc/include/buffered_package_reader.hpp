cd6565a1ea39acb4785167b14660aa850d1036f8
fedddot <idanrommm@gmail.com>
2024-12-20 14:20:45 +0000

    buffered_package_reader implemented

--- ipc/include/buffered_package_reader.hpp
+++ ipc/include/buffered_package_reader.hpp
@@ -1,14 +1,87 @@
 #ifndef	BUFFERED_PACKAGE_READER_HPP
 #define	BUFFERED_PACKAGE_READER_HPP
 
+#include <stdexcept>
+#include <vector>
+
+#include "data_reader.hpp"
+#include "ipc_types.hpp"
+
 namespace ipc {
-	template <typename Tdata>
-	class BufferedPackageReader {
+	class BufferedPackageReader: public DataReader<Package> {
 	public:
-		virtual ~BufferedPackageReader() noexcept = default;
-		virtual bool readable() const = 0;
-		virtual Tdata read() const = 0;
+		using Preamble = std::vector<char>;
+		BufferedPackageReader(const Preamble& preamble);
+		BufferedPackageReader(const BufferedPackageReader&) = delete;
+		BufferedPackageReader& operator=(const BufferedPackageReader&) = delete;
+
+		bool readable() const override;
+		Package read() const override;
+		void feed(const char ch);
+	private:
+		using RawData = std::vector<char>;
+		const Preamble m_preamble;
+		const std::size_t m_preamble_size;
+
+		mutable RawData m_buffer;
+
+		void reset_buffer();
+
+		static PackageSize parse_size(const RawData& raw_data);
 	};
+
+	inline BufferedPackageReader::BufferedPackageReader(const Preamble& preamble): m_preamble(preamble), m_preamble_size(preamble.size()) {
+		reset_buffer();
+	}
+
+	inline bool BufferedPackageReader::readable() const {
+		if (m_buffer.size() < m_preamble_size + sizeof(PackageSize)) {
+			return false;
+		}
+		const auto received_preamble = Preamble(m_buffer.begin(), m_buffer.begin() + m_preamble_size);
+		if (received_preamble != m_preamble) {
+			m_buffer.erase(m_buffer.begin());
+			return false;
+		}
+		const auto pckg_size_data = RawData(
+			m_buffer.begin() + m_preamble_size,
+			m_buffer.begin() + m_preamble_size + sizeof(PackageSize)
+		);
+		const auto package_size = parse_size(pckg_size_data);
+		return m_buffer.size() >= m_preamble_size + sizeof(PackageSize) + package_size;
+	}
+
+	inline Package BufferedPackageReader::read() const {
+		if (!readable()) {
+			throw std::runtime_error("package is not readable");
+		}
+		const auto package_data_start_pos = m_preamble_size + sizeof(PackageSize);
+		const auto pckg_size_data = RawData(
+			m_buffer.begin() + m_preamble_size,
+			m_buffer.begin() + m_preamble_size + sizeof(PackageSize)
+		);
+		const auto package_size = parse_size(pckg_size_data);
+		const auto package_data_end_pos = package_data_start_pos + package_size;
+		const auto package = Package(
+			m_buffer.begin() + package_data_start_pos,
+			m_buffer.begin() + package_data_end_pos
+		);
+		m_buffer.erase(m_buffer.begin(), m_buffer.begin() + package_data_end_pos);
+		return package;
+	}
+
+	inline void BufferedPackageReader::feed(const char ch) {
+		enum: int {RESIZE_ADDITION = 10};
+		if (m_buffer.size() == m_buffer.capacity()) {
+			m_buffer.reserve(m_buffer.size() + RESIZE_ADDITION);
+		}
+		m_buffer.push_back(ch);
+	}
+
+	inline void BufferedPackageReader::reset_buffer() {
+		m_buffer.clear();
+		m_buffer.reserve(m_preamble.size() + sizeof(PackageSize));
+	}
 }
 
 #endif // BUFFERED_PACKAGE_READER_HPP
--- ipc/include/ipc_types.hpp
+++ ipc/include/ipc_types.hpp
@@ -1,8 +1,12 @@
 #ifndef	IPC_TYPES_HPP
 #define	IPC_TYPES_HPP
 
+#include <cstddef>
+#include <vector>
+
 namespace ipc {
 	using Package = std::vector<char>;
+	using PackageSize = std::size_t;
 }
 
 #endif // IPC_TYPES_HPP
