824c5b2b56ee68a8858ae741c22774fe689b2f6f
fedddot <idanrommm@gmail.com>
2024-12-27 09:35:07 +0000

    test http server updated

--- servers/src/test_http_cnc_server.cpp
+++ servers/src/test_http_cnc_server.cpp
@@ -5,10 +5,9 @@
 using namespace cnc_server;
 
 int main(void) {
-    const std::string id = "cnc_server";
     const std::string uri = "http://127.0.0.1:5000";
     const unsigned int timeout_s = 20;
-    cnc_server::TestHttpCncServer<std::string> server(id, uri, timeout_s);
+    cnc_server::TestHttpCncServer server(uri, timeout_s, timeout_s);
     server.run();
     while (server.is_running()) {
         sleep(10);
