997ea19365e8672e3915720bfdab74aa239f7992
fedddot <idanrommm@gmail.com>
2024-12-27 14:04:37 +0000

    test updated

--- servers/tests/src/ut_test_http_cnc_server.cpp
+++ servers/tests/src/ut_test_http_cnc_server.cpp
@@ -1,23 +1,15 @@
-#include <string>
-
 #include "gtest/gtest.h"
 
-#include "test_http_cnc_server.hpp"
+#include "server_fixture.hpp"
 
 using namespace server;
+using namespace server_utl;
 using namespace cnc_server;
 
-TEST(ut_test_http_cnc_server, ctor_dtor_sanity) {
+TEST_F(ServerFixture, gpio_sanity) {
     // GIVEN
-    const std::string test_id("test_http_cnc_server");
-    const std::string uri("http://127.0.0.1:5000");
-    const unsigned int timeout_s(20);
     
     // WHEN
-    TestHttpCncServer<std::string> *instance_ptr(nullptr);
 
     // THEN
-    ASSERT_NO_THROW(instance_ptr = new TestHttpCncServer<std::string>(test_id, uri, timeout_s));
-    ASSERT_NE(nullptr, instance_ptr);
-    ASSERT_NO_THROW(delete instance_ptr);
 }
