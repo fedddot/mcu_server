eb7c9165c28d6e010e6baba1f9ea13027f0ada7c
fedddot <idanrommm@gmail.com>
2024-12-28 11:48:06 +0000

    updated

--- manager/gpio_manager/include/gpio_manager.hpp
+++ manager/gpio_manager/include/gpio_manager.hpp
@@ -2,13 +2,18 @@
 #define	GPIO_MANAGER_HPP
 
 #include <map>
+#include <memory>
 #include <stdexcept>
 
+#include "gpi.hpp"
 #include "gpio.hpp"
+#include "gpio_create_request.hpp"
 #include "gpio_provider.hpp"
 #include "gpio_request.hpp"
 #include "gpio_response.hpp"
 #include "gpio_state_response.hpp"
+#include "gpio_write_request.hpp"
+#include "gpo.hpp"
 #include "manager.hpp"
 #include "provider.hpp"
 
@@ -23,7 +28,7 @@ namespace manager {
 		GpioResponse *run(const GpioRequest<Tgpio_id>& request) override;
 	private:
 		GpioProvider<Tgpio_id> *m_provider;
-		std::map<Tgpio_id, Gpio *> m_gpios;
+		std::map<Tgpio_id, std::unique_ptr<Gpio>> m_gpios;
 		
 		void create_gpio(const GpioRequest<Tgpio_id>& request);
 		void delete_gpio(const GpioRequest<Tgpio_id>& request);
@@ -48,14 +53,61 @@ namespace manager {
 		case GpioRequest<Tgpio_id>::Operation::READ:
 			return new GpioStateResponse(GpioResponse::Result::SUCCESS, read_gpio(request));
 		case GpioRequest<Tgpio_id>::Operation::WRITE:
-			write_gpio(request.id(), request.state());
+			write_gpio(request);
 			return new GpioResponse(GpioResponse::Type::REGULAR, GpioResponse::Result::SUCCESS);
 		case GpioRequest<Tgpio_id>::Operation::DELETE:
-			delete_gpio(request.id());
+			delete_gpio(request);
 			return new GpioResponse(GpioResponse::Type::REGULAR, GpioResponse::Result::SUCCESS);
 		default:
 			throw std::runtime_error("unsupported operation");
 		}
+	}
+
+	template <typename Tgpio_id>
+	void GpioManager<Tgpio_id>::create_gpio(const GpioRequest<Tgpio_id>& request) {
+		const auto& create_request = dynamic_cast<const GpioCreateRequest<Tgpio_id>&>(request);
+		m_gpios.insert(
+			{
+				create_request.id(),
+				std::unique_ptr<Gpio>(m_provider->create_gpio(create_request.id(), create_request.direction()))
+			}
+		);
+	}
+
+	template <typename Tgpio_id>
+	void GpioManager<Tgpio_id>::delete_gpio(const GpioRequest<Tgpio_id>& request) {
+		const auto iter = m_gpios.find(request.id());
+		if (m_gpios.end() == iter) {
+			throw std::runtime_error("gpio with received ID is not created");
+		}
+		m_gpios.erase(iter);
+	}
+
+	template <typename Tgpio_id>
+	Gpio::State GpioManager<Tgpio_id>::read_gpio(const GpioRequest<Tgpio_id>& request) const {
+		const auto iter = m_gpios.find(request.id());
+		if (m_gpios.end() == iter) {
+			throw std::runtime_error("gpio with received ID is not created");
+		}
+		const auto& gpio = *(iter->second);
+		switch (gpio.direction()) {
+		case Gpio::Direction::IN:
+			return Gpio::cast<Gpi>(gpio).state();
+		case Gpio::Direction::OUT:
+			return Gpio::cast<Gpo>(gpio).state();
+		default:
+			throw std::runtime_error("unsupported gpio type");
+		}
+	}
+	
+	template <typename Tgpio_id>
+	void GpioManager<Tgpio_id>::write_gpio(const GpioRequest<Tgpio_id>& request) {
+		const auto iter = m_gpios.find(request.id());
+		if (m_gpios.end() == iter) {
+			throw std::runtime_error("gpio with received ID is not created");
+		}
+		const auto& write_request = dynamic_cast<const GpioWriteRequest<Tgpio_id>&>(request);
+		Gpio::cast<Gpo&>(*(iter->second)).set_state(write_request.state());
 	}
 }
 
--- manager/gpio_manager/tests/src/ut_gpio_manager.cpp
+++ manager/gpio_manager/tests/src/ut_gpio_manager.cpp
@@ -1,9 +1,12 @@
 #include <memory>
 
 #include "gtest/gtest.h"
+#include <stdexcept>
+#include <string>
 
 #include "gpio.hpp"
 #include "gpio_manager.hpp"
+#include "gpio_provider.hpp"
 #include "gpo.hpp"
 #include "test_gpi.hpp"
 #include "test_gpo.hpp"
@@ -11,11 +14,27 @@
 using namespace manager;
 using namespace manager_uts;
 
+using GpioId = std::string;
+
+class TestGpioProvider: public GpioProvider<GpioId> {
+public:
+	TestGpioProvider() = default;
+	TestGpioProvider(const TestGpioProvider&) = default;
+	TestGpioProvider& operator=(const TestGpioProvider&) = default;
+
+	Gpio *create_gpio(const GpioId& id, const Gpio::Direction& direction) const override {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	}
+};
+
 TEST(ut_gpio_manager, ctor_dtor_sanity) {
 	// GIVEN
-	
+	TestGpioProvider provider;
+
 	// WHEN
-	GpioManager *instance_ptr(nullptr);
+	GpioManager<GpioId> *instance_ptr(nullptr);
 	
 	// THEN
+	ASSERT_NO_THROW(instance_ptr = new GpioManager<GpioId>(&provider));
+	ASSERT_NO_THROW(delete instance_ptr);
 }
