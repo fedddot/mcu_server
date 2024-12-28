4736b6df561a57967c610921da39bd8a7f2ecc6f
fedddot <idanrommm@gmail.com>
2024-12-28 19:01:00 +0000

    updated

--- ipc/include/http_ipc_connection.hpp
+++ ipc/include/http_ipc_connection.hpp
@@ -2,36 +2,46 @@
 #define	HTTP_IPC_CONNECTION_HPP
 
 #include <condition_variable>
+#include <functional>
+#include <memory>
 #include <mutex>
 #include <string>
 
 #include "cpprest/http_msg.h"
 #include "cpprest/http_listener.h"
 
-#include "ipc_"
-#include "request.hpp"
-#include "response.hpp"
+#include "ipc_connection.hpp"
 
 namespace ipc {
 	template <typename Trequest, typename Tresponse>
 	class HttpIpcConnection: public mcu_app::IpcConnection<Trequest, Tresponse> {
 	public:
-		HttpIpcConnection(const std::string& uri, const unsigned int polling_timeout_s, const unsigned int response_timeout_s);
+		using HttpRequestToRequestTransformer = std::function<Trequest(const web::http::http_request&)>;
+		using ResponseToHttpResponseTransformer = std::function<web::http::http_response(const Tresponse&)>;
+		HttpIpcConnection(
+			const std::string& uri,
+			const unsigned int polling_timeout_s,
+			const unsigned int response_timeout_s,
+			const HttpRequestToRequestTransformer& http_request_to_request,
+			const ResponseToHttpResponseTransformer& response_to_http_response
+		);
 		HttpIpcConnection(const HttpIpcConnection&) = delete;
 		HttpIpcConnection& operator=(const HttpIpcConnection&) = delete;
 		
 		~HttpIpcConnection() noexcept override;
 
-		void write(const server::Response& response) const override;
+		void write(const Tresponse& response) const override;
 		bool readable() const override;
-		server::Request read() const override;
+		Trequest read() override;
 	private:
 		unsigned int m_polling_timeout_s;
 		unsigned int m_response_timeout_s;
+		HttpRequestToRequestTransformer m_http_request_to_request;
+		ResponseToHttpResponseTransformer m_response_to_http_response;
 		web::http::experimental::listener::http_listener m_listener;
 		
-		mutable server::Response m_response;
-		mutable server::Request m_request;
+		mutable std::unique_ptr<Tresponse> m_response;
+		mutable std::unique_ptr<Trequest> m_request;
 
 		mutable bool m_request_received;
 		mutable std::mutex m_request_mux;
@@ -41,10 +51,89 @@ namespace ipc {
 		mutable std::mutex m_response_mux;
 		mutable std::condition_variable m_response_cond;
 		
-		server::Request parse_request(const web::http::http_request& request);
-		web::http::http_response parse_response(const server::Response& response);
 		void request_handler(const web::http::http_request& request);
 	};
+
+	template <typename Trequest, typename Tresponse>
+	inline HttpIpcConnection<Trequest, Tresponse>::HttpIpcConnection(
+		const std::string& uri,
+		const unsigned int polling_timeout_s,
+		const unsigned int response_timeout_s,
+		const HttpRequestToRequestTransformer& http_request_to_request,
+		const ResponseToHttpResponseTransformer& response_to_http_response
+	):
+		m_polling_timeout_s(polling_timeout_s),
+		m_response_timeout_s(response_timeout_s),
+		m_http_request_to_request(http_request_to_request),
+		m_response_to_http_response(response_to_http_response),
+		m_listener(uri),
+		m_response(nullptr), m_request(nullptr) {
+
+		m_listener.support(
+			[this](web::http::http_request request) {
+				request_handler(request);
+			}
+		);
+		m_listener.open().wait();
+	}
+
+	template <typename Trequest, typename Tresponse>
+	inline HttpIpcConnection<Trequest, Tresponse>::~HttpIpcConnection() noexcept {
+		m_listener.close().wait();
+	}
+
+	template <typename Trequest, typename Tresponse>
+	inline void HttpIpcConnection<Trequest, Tresponse>::write(const Tresponse& outgoing_data) const {
+		std::unique_lock response_lock(m_response_mux);
+		m_response = std::make_unique<Tresponse>(outgoing_data);
+		m_response_cond.notify_one();
+	}
+
+	template <typename Trequest, typename Tresponse>
+	inline bool HttpIpcConnection<Trequest, Tresponse>::readable() const {
+		std::unique_lock lock(m_request_mux);
+		if (m_request) {
+			return true;
+		}
+		m_request_cond.wait_for(std::ref(lock), std::chrono::seconds(m_polling_timeout_s));
+		return nullptr != m_request;
+	}
+
+	template <typename Trequest, typename Tresponse>
+	Trequest HttpIpcConnection<Trequest, Tresponse>::read() {
+		std::unique_lock lock(m_request_mux);
+		if (!m_request) {
+			throw std::runtime_error("there is no request to read");
+		}
+		Trequest request(*m_request);
+		m_request = nullptr;
+		return request;
+	}
+
+	template <typename Trequest, typename Tresponse>
+	inline void HttpIpcConnection<Trequest, Tresponse>::request_handler(const web::http::http_request& request) {
+		{
+			std::unique_lock lock(m_request_mux);
+			if (m_request) {
+				request.reply(web::http::status_codes::TooManyRequests).wait();
+				m_request_cond.notify_all();
+				return;
+			}
+			m_request = std::make_unique<Trequest>(m_http_request_to_request(request));
+			m_request_cond.notify_one();
+		}
+		std::unique_lock lock(m_response_mux);
+		if (!m_response) {
+			m_response_cond.wait_for(lock, std::chrono::seconds(m_response_timeout_s));
+		}
+		if (!m_response) {
+			const auto timeout_response = web::http::http_response(web::http::status_codes::RequestTimeout);
+			request.reply(timeout_response).wait();
+			return;
+		}
+		request.reply(m_response_to_http_response(*m_response)).wait();
+		m_response = nullptr;
+	}
 }
 
 #endif // HTTP_IPC_CONNECTION_HPP
--- ipc/tests/src/ut_http_ipc_connection.cpp
+++ ipc/tests/src/ut_http_ipc_connection.cpp
@@ -1,19 +1,11 @@
-#include <string>
-
 #include "gtest/gtest.h"
 #include "cpprest/http_client.h"
 #include "cpprest/http_msg.h"
-#include "pplx/pplxtasks.h"
 
 #include "http_ipc_connection.hpp"
-#include "json_data_serializer.hpp"
-#include "request.hpp"
-#include "response.hpp"
-#include "server_types.hpp"
-#include "string.hpp"
 
 using namespace ipc;
-using namespace server;
+using TestConnection = HttpIpcConnection<web::http::http_request, web::http::http_response>;
 
 TEST(ut_http_ipc_connection, ctor_dtor_sanity) {
     // GIVEN
@@ -22,10 +14,22 @@ TEST(ut_http_ipc_connection, ctor_dtor_sanity) {
     const auto response_timeout = 3;
 
     // WHEN
-    HttpIpcConnection *instance_ptr(nullptr);
+    TestConnection *instance_ptr(nullptr);
 
     // THEN
-    ASSERT_NO_THROW(instance_ptr = new HttpIpcConnection(test_url, polling_timeout, response_timeout));
+    ASSERT_NO_THROW(
+        instance_ptr = new TestConnection(
+            test_url,
+            polling_timeout,
+            response_timeout,
+            [](const web::http::http_request& request) {
+                return request;
+            },
+            [](const web::http::http_response& response) {
+                return response;
+            }
+        )
+    );
     ASSERT_NO_THROW(delete instance_ptr);
     instance_ptr = nullptr;
 }
@@ -33,29 +37,35 @@ TEST(ut_http_ipc_connection, ctor_dtor_sanity) {
 TEST(ut_http_ipc_connection, write_readable_read_sanity) {
     // GIVEN
     const auto test_url = "http://127.0.0.1:5000";
-    const auto polling_timeout = 1;
+    const auto polling_timeout = 2;
     const auto response_timeout = 3;
-    auto test_body = Body();
-    test_body.add("key", String("val"));
-    const auto test_request = server::Request(server::Request::Method::DELETE, {"one", "two"}, test_body);
+    const auto test_request = web::http::http_request(web::http::methods::GET);
+    const auto test_response = web::http::http_response(web::http::status_codes::BadRequest);
 
     // WHEN
-    HttpIpcConnection instance(test_url, polling_timeout, response_timeout);
+    TestConnection instance(
+        test_url,
+        polling_timeout,
+        response_timeout,
+        [](const web::http::http_request& request) {
+            return request;
+        },
+        [](const web::http::http_response& response) {
+            return response;
+        }
+    );
+    web::http::http_request read_result;
 
     // THEN
     ASSERT_FALSE(instance.readable());
 
     // send test request
-    web::http::http_request test_http_request(web::http::methods::DEL);
-    test_http_request.set_request_uri("/one/two");
-    test_http_request.set_body(
-        server_utl::JsonDataSerializer()(test_body),
-        utf8string("application/json; charset=utf-8")
-    );
     web::http::client::http_client client(test_url);
-    auto request_task = client.request(test_http_request);
+    auto request_task = client.request(test_request);
     
     ASSERT_TRUE(instance.readable());
-    ASSERT_NO_THROW(instance.write(Response(ResponseCode::BAD_REQUEST, Body())));
+    ASSERT_NO_THROW(read_result = instance.read());
+    ASSERT_EQ(test_request.method(), read_result.method());
+    ASSERT_NO_THROW(instance.write(test_response));
     ASSERT_EQ(request_task.wait(), pplx::task_status::completed);
 }
