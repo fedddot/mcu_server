52f6d8a75d7f47cc3ee76c6fdc5d6f2dbc2563ab
fedddot <idanrommm@gmail.com>
2024-12-28 11:34:40 +0000

    updated

--- manager/gpio_manager/include/gpio_manager.hpp
+++ manager/gpio_manager/include/gpio_manager.hpp
@@ -1,44 +1,60 @@
 #ifndef	GPIO_MANAGER_HPP
 #define	GPIO_MANAGER_HPP
 
-#include "gpio_data.hpp"
-#include "gpio_provider.hpp"
-#include "gpio_request.hpp"
-#include "gpio_response.hpp"
-#include "manager.hpp"
-#include "provider.hpp"
 #include <map>
 #include <stdexcept>
 
+#include "gpio.hpp"
+#include "gpio_provider.hpp"
+#include "gpio_request.hpp"
+#include "gpio_response.hpp"
+#include "gpio_state_response.hpp"
+#include "manager.hpp"
+#include "provider.hpp"
+
 namespace manager {
-	class GpioManager: public mcu_app::Manager<GpioRequest, GpioResponse> {
+	template <typename Tgpio_id>
+	class GpioManager: public mcu_app::Manager<GpioRequest<Tgpio_id>, GpioResponse *> {
 	public:
 		GpioManager(mcu_app::Provider *provider);
 		GpioManager(const GpioManager& other) = delete;
 		GpioManager& operator=(const GpioManager&) = delete;
 
-		GpioResponse run(const GpioRequest& request) override;
+		GpioResponse *run(const GpioRequest<Tgpio_id>& request) override;
 	private:
-		GpioProvider *m_provider;
-		std::map<GpioData::GpioId, Gpio *> m_gpios;
+		GpioProvider<Tgpio_id> *m_provider;
+		std::map<Tgpio_id, Gpio *> m_gpios;
 		
-		void create_gpio(const GpioData& data);
-		void delete_gpio(const GpioData& data);
-		Gpio::State read_gpio(const GpioData& data) const;
-		void write_gpio(const GpioData& data);
+		void create_gpio(const GpioRequest<Tgpio_id>& request);
+		void delete_gpio(const GpioRequest<Tgpio_id>& request);
+		Gpio::State read_gpio(const GpioRequest<Tgpio_id>& request) const;
+		void write_gpio(const GpioRequest<Tgpio_id>& request);
 	};
 	
-	inline GpioManager::GpioManager(mcu_app::Provider *provider) {
-		m_provider = dynamic_cast<GpioProvider *>(provider);
+	template <typename Tgpio_id>
+	inline GpioManager<Tgpio_id>::GpioManager(mcu_app::Provider *provider) {
+		m_provider = dynamic_cast<GpioProvider<Tgpio_id> *>(provider);
 		if (!m_provider) {
 			throw std::invalid_argument("invalid provider received");
 		}
 	}
 
-	inline GpioResponse run(const GpioRequest& request) {
+	template <typename Tgpio_id>
+	inline GpioResponse *GpioManager<Tgpio_id>::run(const GpioRequest<Tgpio_id>& request) {
 		switch (request.operation()) {
-			case GpioRequest::GpioOperation::CREATE:
-				
+		case GpioRequest<Tgpio_id>::Operation::CREATE:
+			create_gpio(request);
+			return new GpioResponse(GpioResponse::Type::REGULAR, GpioResponse::Result::SUCCESS);
+		case GpioRequest<Tgpio_id>::Operation::READ:
+			return new GpioStateResponse(GpioResponse::Result::SUCCESS, read_gpio(request));
+		case GpioRequest<Tgpio_id>::Operation::WRITE:
+			write_gpio(request.id(), request.state());
+			return new GpioResponse(GpioResponse::Type::REGULAR, GpioResponse::Result::SUCCESS);
+		case GpioRequest<Tgpio_id>::Operation::DELETE:
+			delete_gpio(request.id());
+			return new GpioResponse(GpioResponse::Type::REGULAR, GpioResponse::Result::SUCCESS);
+		default:
+			throw std::runtime_error("unsupported operation");
 		}
 	}
 }
