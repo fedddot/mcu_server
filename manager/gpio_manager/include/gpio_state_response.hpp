e7623b235135eacd47cf852f9e9978c33d40b72b
fedddot <idanrommm@gmail.com>
2024-12-28 11:18:05 +0000

    gpio_state_response added

---
+++ manager/gpio_manager/include/gpio_state_response.hpp
@@ -0,0 +1,27 @@
+#ifndef	GPIO_STATE_RESPONSE_HPP
+#define	GPIO_STATE_RESPONSE_HPP
+
+#include "gpio.hpp"
+#include "gpio_response.hpp"
+
+namespace manager {
+	class GpioStateResponse: public GpioResponse {
+	public:
+		GpioStateResponse(const Result& result, const Gpio::State& state);
+		GpioStateResponse(const GpioStateResponse&) = default;
+		GpioStateResponse& operator=(const GpioStateResponse&) = default;
+		Gpio::State state() const;
+	private:
+		Gpio::State m_state;
+	};
+
+	inline GpioStateResponse::GpioStateResponse(const Result& result, const Gpio::State& state): GpioResponse(result), m_state(state) {
+
+	}
+
+	inline typename Gpio::State GpioStateResponse::state() const {
+		return m_state;
+	}
+}
+
+#endif // GPIO_STATE_RESPONSE_HPP
