0afe48dfcffa9dd30ad3cc0269682ced8306718b
fedddot <idanrommm@gmail.com>
2024-12-28 11:08:05 +0000

    GpioCreateRequest added

---
+++ manager/gpio_manager/include/gpio_create_request.hpp
@@ -0,0 +1,33 @@
+#ifndef	GPIO_CREATE_REQUEST_HPP
+#define	GPIO_CREATE_REQUEST_HPP
+
+#include "gpio.hpp"
+#include "gpio_request.hpp"
+
+namespace manager {
+	template <typename Tgpio_id>
+	class GpioCreateRequest: public GpioRequest<Tgpio_id> {
+	public:
+		using Operation = typename GpioRequest<Tgpio_id>::Operation;
+
+		GpioCreateRequest(const Operation& operation, const Tgpio_id& id, const Gpio::Direction& direction);
+		GpioCreateRequest(const GpioCreateRequest&) = default;
+		GpioCreateRequest& operator=(const GpioCreateRequest&) = default;
+
+		Gpio::Direction direction() const;
+	private:
+		Gpio::Direction m_direction;
+	};
+
+	template <typename Tgpio_id>
+	inline GpioCreateRequest<Tgpio_id>::GpioCreateRequest(const Operation& operation, const Tgpio_id& id, const Gpio::Direction& direction): GpioRequest<Tgpio_id>(operation, id), m_direction(direction) {
+
+	}
+
+	template <typename Tgpio_id>
+	inline typename Gpio::Direction GpioCreateRequest<Tgpio_id>::direction() const {
+		return m_direction;
+	}
+}
+
+#endif // GPIO_CREATE_REQUEST_HPP
