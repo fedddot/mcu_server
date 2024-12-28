6f116181ab557108304ec3934db697d357705f89
fedddot <idanrommm@gmail.com>
2024-12-28 11:34:54 +0000

    updated

--- manager/gpio_manager/include/gpio_provider.hpp
+++ manager/gpio_manager/include/gpio_provider.hpp
@@ -1,14 +1,14 @@
 #ifndef	GPIO_PROVIDER_HPP
 #define	GPIO_PROVIDER_HPP
 
-#include "gpio_data.hpp"
 #include "gpio.hpp"
 #include "provider.hpp"
 
 namespace manager {
+	template <typename Tgpio_id>
 	class GpioProvider: public mcu_app::Provider {
 	public:
-		virtual Gpio *create_gpio(const GpioData& data) const = 0;
+		virtual Gpio *create_gpio(const Tgpio_id& id, const Gpio::Direction& direction) const = 0;
 	};
 }
 
