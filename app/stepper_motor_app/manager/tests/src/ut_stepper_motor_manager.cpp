98a176ce9f85819f9eb2034aded1f312500963a9
fedddot <idanrommm@gmail.com>
2025-01-07 19:31:19 +0000

    serve_delete sanity tested

--- app/stepper_motor_app/manager/tests/src/ut_stepper_motor_manager.cpp
+++ app/stepper_motor_app/manager/tests/src/ut_stepper_motor_manager.cpp
@@ -14,14 +14,23 @@
 using namespace manager;
 using namespace manager_tests;
 
+using StepperCreateConfig = std::string;
+
 TEST(ut_stepper_motor_manager, sanity) {
 	// GIVEN
-	using StepperCreateConfig = std::string;
 	auto test_manager_cfg = StepperMotorManagerConfig("stepper motor manager");
-	host_tests::TestProviders<StepperMotorProviderType> providers;
-	const auto test_create_request_data = StepperMotorCreateRequestData<StepperMotorId, StepperCreateConfig>("test_motor", StepperCreateConfig("test_cfg"));
+	
+	const auto test_motor_id = StepperMotorId("test_motor");
+	const auto test_motor_create_cfg = StepperCreateConfig("test_motor_create_cfg");
+	const auto test_create_request_data = StepperMotorCreateRequestData<StepperMotorId, StepperCreateConfig>(test_motor_id, test_motor_create_cfg);
 	auto test_create_request = StepperMotorRequest(StepperMotorRequest::Type::CREATE_STEPPER);
 	test_create_request.set_data(test_create_request_data);
+
+	const auto test_regular_request_data = StepperMotorRegularRequestData<StepperMotorId>(test_motor_id);
+	auto test_delete_request = StepperMotorRequest(StepperMotorRequest::Type::DELETE_STEPPER);
+	test_delete_request.set_data(test_regular_request_data);
+
+	host_tests::TestProviders<StepperMotorProviderType> providers;
 
 	// WHEN
 	providers.add_provider(
@@ -40,9 +49,14 @@ TEST(ut_stepper_motor_manager, sanity) {
 	ASSERT_NO_THROW(instance_ptr = new StepperMotorManager<StepperCreateConfig>(&providers));
 	ASSERT_NE(nullptr, instance_ptr);
 
-	// run
+	// run create
 	ASSERT_NO_THROW(response = instance_ptr->run(test_create_request));
 	ASSERT_EQ(response.code(), StepperMotorResponse::ResultCode::OK);
+
+	// run delete
+	ASSERT_NO_THROW(response = instance_ptr->run(test_delete_request));
+	ASSERT_EQ(response.code(), StepperMotorResponse::ResultCode::OK);
+
 
 	// dtor
 	ASSERT_NO_THROW(delete instance_ptr);
