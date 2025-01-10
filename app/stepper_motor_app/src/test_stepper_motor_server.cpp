bc2b0f6d0f782fc6ed3cb1f819b5119496524580
fedddot <idanrommm@gmail.com>
2025-01-10 15:33:36 +0000

    test server updated

--- app/stepper_motor_app/src/test_stepper_motor_server.cpp
+++ app/stepper_motor_app/src/test_stepper_motor_server.cpp
@@ -9,11 +9,11 @@
 #include "json/writer.h"
 
 #include "cpprest/http_msg.h"
+#include "http_ipc_server.hpp"
 #include "pplx/pplxtasks.h"
 
 #include "host.hpp"
-#include "http_ipc_server.hpp"
-#include "ipc_server.hpp"
+#include "ipc_server_factory.hpp"
 #include "manager.hpp"
 #include "stepper_motor_manager.hpp"
 #include "stepper_motor_request.hpp"
@@ -27,18 +27,25 @@ using namespace host;
 using namespace ipc;
 
 using ProvidersConfig = std::string;
-using IpcConfig = std::string;
 using StepperCreateCfg = std::string;
 
-using StepperMotorHost = Host<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse, StepperMotorManagerConfig, IpcConfig>;
+using StepperMotorHost = Host<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse, StepperMotorManagerConfig>;
 
-static IpcServer<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse> *create_ipc(const IpcConfig& cfg);
 static Manager<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse> *create_manager(const StepperMotorManagerConfig& cfg);
+static StepperMotorRequest<StepperCreateCfg> transform_to_request(const web::http::http_request& http_request);
+static web::http::http_response transform_to_response(const StepperMotorResponse& response);
 
 int main(void) {
+    auto ipc_config = HttoIpcServerConfig<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse>();
+    ipc_config.uri = "http://127.0.0.1:5555";
+    ipc_config.polling_timeout_s = 1;
+    ipc_config.response_timeout_s = 60;
+    ipc_config.to_request = transform_to_request;
+    ipc_config.to_response = transform_to_response;
+    
     auto host = StepperMotorHost(
-        create_ipc,
-        "",
+        IpcServerFactory<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse>(),
+        ipc_config,
         create_manager,
         "",
         [](const std::exception& e) {
@@ -61,7 +68,7 @@ static inline StepperMotorId retrieve_motor_id(const Json::Value& root) {
     return static_cast<StepperMotorId>(value_ptr->asString());
 }
 
-static inline typename StepperMotorRequest<StepperCreateCfg>::Type retrieve_request_type(const web::http::http_request& http_request) {
+inline static typename StepperMotorRequest<StepperCreateCfg>::Type retrieve_request_type(const web::http::http_request& http_request) {
     using ReqType = typename StepperMotorRequest<StepperCreateCfg>::Type;
     const auto type_map = std::map<std::string, ReqType> {
         {"POST",  ReqType::CREATE_STEPPER},
@@ -76,7 +83,7 @@ static inline typename StepperMotorRequest<StepperCreateCfg>::Type retrieve_requ
     return iter->second;
 }
 
-static inline typename StepperMotorRequest<StepperCreateCfg>::Steps retrieve_steps(const Json::Value& steps_cfg) {
+inline static typename StepperMotorRequest<StepperCreateCfg>::Steps retrieve_steps(const Json::Value& steps_cfg) {
     using ReqSteps = typename StepperMotorRequest<StepperCreateCfg>::Steps;
     const auto type_map = std::map<std::string, StepperMotorDirection> {
         {"CW",  StepperMotorDirection::CW},
@@ -105,7 +112,7 @@ static inline typename StepperMotorRequest<StepperCreateCfg>::Steps retrieve_ste
     };
 }
 
-static StepperMotorRequest<StepperCreateCfg> transform_to_request(const web::http::http_request& http_request) {
+inline StepperMotorRequest<StepperCreateCfg> transform_to_request(const web::http::http_request& http_request) {
     const auto request_task = http_request.extract_vector();
     if (pplx::task_status::completed != request_task.wait()) {
         throw std::runtime_error("failed to extract request data");
@@ -135,7 +142,7 @@ static StepperMotorRequest<StepperCreateCfg> transform_to_request(const web::htt
     return request;
 }
 
-static web::http::http_response transform_to_response(const StepperMotorResponse& response) {
+inline web::http::http_response transform_to_response(const StepperMotorResponse& response) {
     using RespRes = typename StepperMotorResponse::ResultCode;
     const auto type_map = std::map<RespRes, web::http::status_code> {
         {RespRes::OK,           web::http::status_codes::OK},
@@ -160,17 +167,6 @@ static web::http::http_response transform_to_response(const StepperMotorResponse
     const auto resp_body_raw = Json::FastWriter().write(resp_body);
     http_resp.set_body(resp_body_raw, utf8string("application/json; charset=utf-8"));
     return http_resp;
-}
-
-IpcServer<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse> *create_ipc(const IpcConfig& cfg) {
-    // TODO: retrieve the params from cfg
-    return new HttpIpcServer<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse>(
-        "http://127.0.0.1:5555",
-        2,
-        100,
-        transform_to_request,
-        transform_to_response
-    );
 }
 
 Manager<StepperMotorRequest<StepperCreateCfg>, StepperMotorResponse> *create_manager(const StepperMotorManagerConfig& cfg) {
