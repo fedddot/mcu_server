16614eebc31c740f41e6cc9edb2d70898566ad86
fedddot <idanrommm@gmail.com>
2025-01-18 15:27:02 +0000

    ipcs updated

--- ipc/include/ipc_server_factory.hpp
+++ ipc/include/ipc_server_factory.hpp
@@ -3,40 +3,39 @@
 
 #include <stdexcept>
 
+#include "buffered_ipc_server.hpp"
+#include "http_ipc_server.hpp"
 #include "ipc_config.hpp"
 #include "ipc_server.hpp"
-#include "test_ipc_server.hpp"
-#include "http_ipc_server.hpp"
 
 namespace ipc {
-	template <typename Tincoming, typename Toutgoing>
+	template <typename Request, typename Response>
 	class IpcServerFactory {
 	public:
 		IpcServerFactory() = default;
 		virtual ~IpcServerFactory() noexcept = default;
-		IpcServer<Tincoming, Toutgoing> *operator()(const IpcConfig& config) const;
+		IpcServer<Request, Response> *operator()(const IpcConfig& config) const;
 	private:
-		template <typename Tconfig>
-		static const Tconfig& cast_config(const IpcConfig& config);
+		template <typename Config>
+		static const Config& cast_config(const IpcConfig& config);
 	};
 
-	template <typename Tincoming, typename Toutgoing>
-	inline IpcServer<Tincoming, Toutgoing> *IpcServerFactory<Tincoming, Toutgoing>::operator()(const IpcConfig& config) const {
-		if ("ipc.server.test" == config.type()) {
-			using namespace ipc_tests;
-			return new TestIpcServer<Tincoming, Toutgoing>(cast_config<TestIpcConfig<Toutgoing>>(config));
-		}
+	template <typename Request, typename Response>
+	inline IpcServer<Request, Response> *IpcServerFactory<Request, Response>::operator()(const IpcConfig& config) const {
 		if ("ipc.server.http" == config.type()) {
-			return new HttpIpcServer<Tincoming, Toutgoing>(cast_config<HttoIpcServerConfig<Tincoming, Toutgoing>>(config));
+			return new HttpIpcServer<Request, Response>(cast_config<HttpIpcServerConfig<Request, Response>>(config));
+		}
+		if ("ipc.server.buffered" == config.type()) {
+			return new BufferedIpcServer<Request, Response>(cast_config<BufferedIpcServerConfig<Request, Response>>(config));
 		}
 		throw std::invalid_argument("insupported ipc server type");
 	}
 
-	template <typename Tincoming, typename Toutgoing>
-	template <typename Tconfig>
-	const Tconfig& IpcServerFactory<Tincoming, Toutgoing>::cast_config(const IpcConfig& config) {
+	template <typename Request, typename Response>
+	template <typename Config>
+	const Config& IpcServerFactory<Request, Response>::cast_config(const IpcConfig& config) {
 		try {
-			return dynamic_cast<const Tconfig&>(config);
+			return dynamic_cast<const Config&>(config);
 		} catch (...) {
 			throw std::runtime_error("failed to cast ipc server config into target type: " + config.type());
 		}
