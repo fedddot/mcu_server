d88af7f90f3c000d2a5048eb070d0cf6fc527680
fedddot <idanrommm@gmail.com>
2025-01-23 15:02:48 +0000

    updated

--- ipc/include/buffered_ipc_server.hpp
+++ ipc/include/buffered_ipc_server.hpp
@@ -1,46 +1,74 @@
 #ifndef	BUFFERED_IPC_SERVER_HPP
 #define	BUFFERED_IPC_SERVER_HPP
 
+#include <cstddef>
+#include <functional>
+#include <stdexcept>
+#include <vector>
+
 #include "ipc_option.hpp"
 #include "ipc_server.hpp"
-#include "buffered_ipc_server_config.hpp"
 
 namespace ipc {
-	template <typename Request, typename Response, typename RawData>
+	template <typename Request, typename Response>
 	class BufferedIpcServer: public IpcServer<Request, Response> {
 	public:
-		BufferedIpcServer(const BufferedIpcServerConfig<Request, Response, RawData>& config);
+		using RequestReader = std::function<Option<Request>(std::vector<char> *)>;
+		using ResponseWriter = std::function<void(const Response&)>;
+		BufferedIpcServer(
+			const RequestReader& request_reader,
+			const ResponseWriter& response_writer,
+			const std::size_t& resize_factor
+		);
 		BufferedIpcServer(const BufferedIpcServer&) = delete;
 		BufferedIpcServer& operator=(const BufferedIpcServer&) = delete;
 
-		void write(const Response& response) const override;
-		Option<Request> read() override;
+		void serve(IpcRequestHandler<Request, Response> *request_handler) override;
+		void stop() override;
 
 		void feed(const char ch);
 	private:
-		BufferedIpcServerConfig<Request, Response, RawData> m_config;
-		RawData m_buffer;
+		const RequestReader m_request_reader;
+		const ResponseWriter m_response_writer;
+		const std::size_t m_resize_factor;
+		bool m_is_running;
+		std::vector<char> m_buffer;
 	};
 
-	template <typename Request, typename Response, typename RawData>
-	inline BufferedIpcServer<Request, Response, RawData>::BufferedIpcServer(const BufferedIpcServerConfig<Request, Response, RawData>& config): m_config(config) {
-		m_buffer.reserve(m_config.realloc_size());
+	template <typename Request, typename Response>
+	inline BufferedIpcServer<Request, Response>::BufferedIpcServer(
+		const RequestReader& request_reader,
+		const ResponseWriter& response_writer,
+		const std::size_t& resize_factor
+	): m_request_reader(request_reader), m_response_writer(response_writer), m_resize_factor(resize_factor), m_is_running(false) {
+		m_buffer.reserve(m_resize_factor);
 	}
 
-	template <typename Request, typename Response, typename RawData>
-	inline void BufferedIpcServer<Request, Response, RawData>::write(const Response& outgoing_data) const {
-		m_config.writer()(outgoing_data);
+	template <typename Request, typename Response>
+	void BufferedIpcServer<Request, Response>::serve(IpcRequestHandler<Request, Response> *request_handler) {
+		if (!request_handler) {
+			throw std::invalid_argument("invalid request handler received");
+		}
+		m_is_running = true;
+		while (m_is_running) {
+			const auto request = m_request_reader(&m_buffer);
+			if (!request.some()) {
+				continue;
+			}
+			const auto response = request_handler->handle(request.get());
+			m_response_writer(response);
+		}
+	}
+	
+	template <typename Request, typename Response>
+	void BufferedIpcServer<Request, Response>::stop() {
+		m_is_running = false;
 	}
 
-	template <typename Request, typename Response, typename RawData>
-	Option<Request> BufferedIpcServer<Request, Response, RawData>::read() {
-		return m_config.reader()(&m_buffer);
-	}
-
-	template <typename Request, typename Response, typename RawData>
-	void BufferedIpcServer<Request, Response, RawData>::feed(const char ch) {
+	template <typename Request, typename Response>
+	void BufferedIpcServer<Request, Response>::feed(const char ch) {
 		if (m_buffer.size() == m_buffer.capacity()) {
-			m_buffer.reserve(m_buffer.size() + m_config.realloc_size());
+			m_buffer.reserve(m_buffer.size() + m_resize_factor);
 		}
 		m_buffer.push_back(ch);
 	}
---
+++ ipc/include/ipc_option.hpp
@@ -0,0 +1,57 @@
+#ifndef	IPC_OPTION_HPP
+#define	IPC_OPTION_HPP
+
+#include <memory>
+#include <stdexcept>
+
+namespace ipc {
+	template <typename T>
+	class Option {
+	public:
+		Option(T *instance);
+		Option(const Option& other);
+		Option& operator=(const Option& other);
+		virtual ~Option() noexcept = default;
+		bool some() const;
+		T get() const;
+	private:
+		std::unique_ptr<T> m_instance;
+	};
+
+	template <typename T>
+	inline Option<T>::Option(T *instance): m_instance(instance) {
+
+	}
+
+	template <typename T>
+	inline Option<T>::Option(const Option& other): m_instance(nullptr) {
+		if (other.m_instance) {
+			m_instance = std::unique_ptr<T>(new T(*(other.m_instance)));
+		}
+	}
+
+	template <typename T>
+	inline Option<T>& Option<T>::operator=(const Option& other) {
+		m_instance = nullptr;
+		if (other.m_instance) {
+			m_instance = std::unique_ptr<T>(new T(*(other.m_instance)));
+		}
+		return *this;
+	}
+
+	template <typename T>
+	inline bool Option<T>::some() const {
+		return nullptr != m_instance;
+	}
+
+	template <typename T>
+	inline T Option<T>::get() const {
+		if (!m_instance) {
+			throw std::runtime_error("attempt to get an uninitialized option");
+		}
+		return *m_instance;
+	}
+
+}
+
+#endif // IPC_OPTION_HPP
