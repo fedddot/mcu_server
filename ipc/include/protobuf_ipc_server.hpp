f5661d4e1c09c3d822a6386c4d71fdb4bb6bfc8b
fedddot <idanrommm@gmail.com>
2025-02-01 16:13:11 +0000

    protobuf_ipc_server updated

--- ipc/include/protobuf_ipc_server.hpp
+++ ipc/include/protobuf_ipc_server.hpp
@@ -1,78 +1,94 @@
 #ifndef	BUFFERED_IPC_SERVER_HPP
 #define	BUFFERED_IPC_SERVER_HPP
 
-#include <cstddef>
 #include <functional>
 #include <stdexcept>
 #include <vector>
+
+#include "pb.h"
 
 #include "ipc_option.hpp"
 #include "ipc_server.hpp"
 
 namespace ipc {
 	template <typename Request, typename Response>
-	class BufferedIpcServer: public IpcServer<Request, Response> {
+	class ProtobufIpcServer: public IpcServer<Request, Response> {
 	public:
-		using RequestReader = std::function<Option<Request>(std::vector<char> *)>;
-		using ResponseWriter = std::function<void(const Response&)>;
+		using RequestStreamReader = std::function<Option<Request>(pb_istream_t *)>;
+		using ResponseStreamWriter = std::function<void(pb_ostream_t *, const Response&)>;
 		using Handler = typename IpcServer<Request, Response>::Handler;
 
-		BufferedIpcServer(
-			const RequestReader& request_reader,
-			const ResponseWriter& response_writer,
-			const std::size_t& resize_factor
+		ProtobufIpcServer(
+			const RequestStreamReader& request_stream_reader,
+			const ResponseStreamWriter& response_stream_writer
 		);
-		BufferedIpcServer(const BufferedIpcServer&) = delete;
-		BufferedIpcServer& operator=(const BufferedIpcServer&) = delete;
+		ProtobufIpcServer(const ProtobufIpcServer&) = delete;
+		ProtobufIpcServer& operator=(const ProtobufIpcServer&) = delete;
 
 		void serve(const Handler& handler) override;
 		void stop() override;
 
-		void feed(const char ch);
+		void feed(const pb_byte_t byte);
 	private:
-		const RequestReader m_request_reader;
-		const ResponseWriter m_response_writer;
-		const std::size_t m_resize_factor;
+		const RequestStreamReader m_request_stream_reader;
+		const ResponseStreamWriter m_response_stream_writer;
+		
 		bool m_is_running;
-		std::vector<char> m_buffer;
+		std::vector<pb_byte_t> m_buffer;
+		std::vector<Request> m_requests_queue;
+
+		enum: int {RESIZE_FACTOR = 10UL};
+
+		void serve_once(const Handler& handler);
 	};
 
 	template <typename Request, typename Response>
-	inline BufferedIpcServer<Request, Response>::BufferedIpcServer(
-		const RequestReader& request_reader,
-		const ResponseWriter& response_writer,
-		const std::size_t& resize_factor
-	): m_request_reader(request_reader), m_response_writer(response_writer), m_resize_factor(resize_factor), m_is_running(false) {
-		m_buffer.reserve(m_resize_factor);
+	inline ProtobufIpcServer<Request, Response>::ProtobufIpcServer(
+		const RequestStreamReader& request_stream_reader,
+		const ResponseStreamWriter& response_stream_writer
+	): m_request_stream_reader(request_stream_reader), m_response_stream_writer(response_stream_writer), m_is_running(false) {
+		m_buffer.reserve(RESIZE_FACTOR);
 	}
 
 	template <typename Request, typename Response>
-	void BufferedIpcServer<Request, Response>::serve(const Handler& handler) {
+	void ProtobufIpcServer<Request, Response>::serve(const Handler& handler) {
 		if (!handler) {
 			throw std::invalid_argument("invalid request handler received");
 		}
 		m_is_running = true;
 		while (m_is_running) {
-			const auto request = m_request_reader(&m_buffer);
-			if (!request.some()) {
-				continue;
-			}
-			const auto response = handler(request.get());
-			m_response_writer(response);
+			serve_once(handler);
 		}
+	}
+
+	template <typename Request, typename Response>
+	void ProtobufIpcServer<Request, Response>::serve_once(const Handler& handler) {
+		if (m_requests_queue.empty()) {
+			return;
+		}
+		const auto request_iter = m_requests_queue.begin();
+		const auto response = handler(*request_iter);
+		m_response_stream_writer(&m_buffer, response);
+		m_requests_queue.erase(request_iter);
 	}
 	
 	template <typename Request, typename Response>
-	void BufferedIpcServer<Request, Response>::stop() {
+	void ProtobufIpcServer<Request, Response>::stop() {
 		m_is_running = false;
 	}
 
 	template <typename Request, typename Response>
-	void BufferedIpcServer<Request, Response>::feed(const char ch) {
+	void ProtobufIpcServer<Request, Response>::feed(const pb_byte_t ch) {
 		if (m_buffer.size() == m_buffer.capacity()) {
-			m_buffer.reserve(m_buffer.size() + m_resize_factor);
+			m_buffer.reserve(m_buffer.size() + RESIZE_FACTOR);
 		}
 		m_buffer.push_back(ch);
+		const auto proto_request_opt = m_request_stream_reader(&m_buffer);
+		if (!proto_request_opt.some()) {
+			return;
+		}
+		const auto request = m_request_stream_reader(proto_request_opt.get());
+		m_requests_queue.push_back(request);
 	}
 }
 
