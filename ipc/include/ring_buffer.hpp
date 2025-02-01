0b420d53a4342420d2ea17117dafac359f86a067
fedddot <idanrommm@gmail.com>
2025-02-01 18:22:18 +0000

    ring buffer tested

--- ipc/include/ring_buffer.hpp
+++ ipc/include/ring_buffer.hpp
@@ -20,29 +20,41 @@ namespace ipc {
 		const T *raw_data() const;
 	private:
 		std::array<T, N> m_buffer;
-		std::size_t m_read_index, m_write_index;
+		std::size_t m_read_index, m_write_index, m_size;
 
 		static std::size_t next_index(const std::size_t& current_index);
 	};
 
 	template <typename T, std::size_t N>
-	inline RingBuffer<T, N>::RingBuffer(): m_read_index(0UL), m_write_index(0UL) {
+	inline RingBuffer<T, N>::RingBuffer(): m_read_index(0UL), m_write_index(0UL), m_size(0UL) {
 
 	}
 
 	template <typename T, std::size_t N>
 	inline void RingBuffer<T, N>::push_back(const T& element) {
-		throw std::runtime_error("not implemented");
+		if (m_size + 1UL > N) {
+			throw std::runtime_error("attempt to push to full buffer");
+		}
+		const auto current_write_index = m_write_index;
+		m_write_index = next_index(current_write_index);
+		++m_size;
+		m_buffer[current_write_index] = element;
 	}
 
 	template <typename T, std::size_t N>
 	inline T RingBuffer<T, N>::pop_front() {
-		throw std::runtime_error("not implemented");
+		if (!m_size) {
+			throw std::runtime_error("attempt to pop from empty buffer");
+		}
+		const auto current_read_index = m_read_index;
+		m_read_index = next_index(current_read_index);
+		--m_size;
+		return m_buffer[current_read_index];
 	}
 
 	template <typename T, std::size_t N>
 	inline std::size_t RingBuffer<T, N>::data_size() const {
-		throw std::runtime_error("not implemented");
+		return m_size;
 	}
 
 	template <typename T, std::size_t N>
--- ipc/tests/src/ut_ring_buffer.cpp
+++ ipc/tests/src/ut_ring_buffer.cpp
@@ -16,7 +16,17 @@ TEST(ut_ring_buffer, sanity) {
     auto instance = RingBuffer<char, buff_size>();
 
     // THEN
-    for (const auto ch: test_data) {
+    for (const auto& ch: test_data) {
         ASSERT_NO_THROW(instance.push_back(ch));
     }
+    ASSERT_EQ(test_data.size(), instance.data_size());
+    ASSERT_ANY_THROW(instance.push_back('y'));
+
+    for (const auto& ch: test_data) {
+        char popped_char = '\0';
+        ASSERT_NO_THROW(popped_char = instance.pop_front());
+        ASSERT_EQ(ch, popped_char);
+    }
+    ASSERT_EQ(0UL, instance.data_size());
+    ASSERT_ANY_THROW(instance.pop_front());
 }
