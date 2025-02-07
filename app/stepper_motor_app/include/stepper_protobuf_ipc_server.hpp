58e4cb63ce78f4e48efe76a11cdbaf25e77d550c
fedddot <idanrommm@gmail.com>
2025-02-07 18:04:52 +0000

    stepper_protobuf_ipc_server added

---
+++ app/stepper_motor_app/include/stepper_protobuf_ipc_server.hpp
@@ -0,0 +1,102 @@
+#ifndef	PROTOBUF_IPC_SERVER_HPP
+#define	PROTOBUF_IPC_SERVER_HPP
+
+#include <cstddef>
+#include <functional>
+#include <stdexcept>
+#include <vector>
+
+#include "pb.h"
+
+#include "ipc_option.hpp"
+#include "ipc_server.hpp"
+#include "pb_decode.h"
+#include "ring_buffer.hpp"
+
+namespace ipc {
+	template <typename Request, typename Response, std::size_t N>
+	class ProtobufIpcServer: public IpcServer<Request, Response> {
+	public:
+		using RequestStreamReader = std::function<Option<Request>(pb_istream_t *)>;
+		using ResponseStreamWriter = std::function<void(const Response&)>;
+		using Handler = typename IpcServer<Request, Response>::Handler;
+
+		ProtobufIpcServer(
+			const RequestStreamReader& request_stream_reader,
+			const ResponseStreamWriter& response_stream_writer
+		);
+		ProtobufIpcServer(const ProtobufIpcServer&) = delete;
+		ProtobufIpcServer& operator=(const ProtobufIpcServer&) = delete;
+
+		void serve(const Handler& handler) override;
+		void stop() override;
+
+		void serve_once(const Handler& handler);
+		void feed(const pb_byte_t byte);
+	private:
+		const RequestStreamReader m_request_stream_reader;
+		const ResponseStreamWriter m_response_stream_writer;
+		
+		bool m_is_running;
+		RingBuffer<pb_byte_t, N> m_buffer;
+
+		std::vector<Request> m_requests_queue;
+	};
+
+	template <typename Request, typename Response, std::size_t N>
+	inline ProtobufIpcServer<Request, Response, N>::ProtobufIpcServer(
+		const RequestStreamReader& request_stream_reader,
+		const ResponseStreamWriter& response_stream_writer
+	): m_request_stream_reader(request_stream_reader), m_response_stream_writer(response_stream_writer), m_is_running(false) {
+	}
+
+	template <typename Request, typename Response, std::size_t N>
+	inline void ProtobufIpcServer<Request, Response, N>::serve(const Handler& handler) {
+		if (!handler) {
+			throw std::invalid_argument("invalid request handler received");
+		}
+		m_is_running = true;
+		while (m_is_running) {
+			serve_once(handler);
+		}
+	}
+
+	template <typename Request, typename Response, std::size_t N>
+	inline void ProtobufIpcServer<Request, Response, N>::serve_once(const Handler& handler) {
+		if (m_requests_queue.empty()) {
+			return;
+		}
+		const auto request_iter = m_requests_queue.begin();
+		const auto response = handler(*request_iter);
+		m_response_stream_writer(response);
+		m_requests_queue.erase(request_iter);
+	}
+	
+	template <typename Request, typename Response, std::size_t N>
+	inline void ProtobufIpcServer<Request, Response, N>::stop() {
+		m_is_running = false;
+	}
+
+	template <typename Request, typename Response, std::size_t N>
+	inline void ProtobufIpcServer<Request, Response, N>::feed(const pb_byte_t ch) {
+		m_buffer.push_back(ch);
+		auto read_stream = pb_istream_from_buffer(m_buffer.raw_data(), m_buffer.data_size());
+		const auto bytes_to_read_before = read_stream.bytes_left;
+		const auto proto_request_opt = m_request_stream_reader(&read_stream);
+		if (!proto_request_opt.some()) {
+			return;
+		}
+		const auto bytes_to_read_after = read_stream.bytes_left;
+		if (bytes_to_read_after > bytes_to_read_before) {
+			throw std::runtime_error("read buffer somehow got bigger after reading operation");
+		}
+		auto bytes_consumed = bytes_to_read_before - bytes_to_read_after;
+		while (bytes_consumed) {
+			m_buffer.pop_front();
+			--bytes_consumed;
+		}
+		m_requests_queue.push_back(proto_request_opt.get());
+	}
+}
+
+#endif // PROTOBUF_IPC_SERVER_HPP
---
+++ app/stepper_motor_app/src/stepper_protobuf_ipc_server.cpp
@@ -0,0 +1,176 @@
+#include <cstddef>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+
+#include "json/reader.h"
+#include "json/value.h"
+#include "json/writer.h"
+
+#include "cpprest/http_msg.h"
+#include "ipc_option.hpp"
+#include "pplx/pplxtasks.h"
+
+#include "host.hpp"
+#include "http_ipc_server.hpp"
+#include "test_stepper_motor.hpp"
+#include "stepper_motor_manager.hpp"
+#include "stepper_motor_request.hpp"
+#include "stepper_motor_response.hpp"
+
+using namespace manager;
+using namespace manager_tests;
+using namespace host;
+using namespace ipc;
+
+using StepperCreateCfg = std::string;
+using Request = StepperMotorRequest<StepperCreateCfg>;
+using Response = StepperMotorResponse;
+
+static Option<Request> transform_to_request(const web::http::http_request& http_request);
+static web::http::http_response transform_to_response(const Response& response);
+
+int main(void) {
+    const auto uri = std::string("http://127.0.0.1:5000");
+    auto ipc_server = HttpIpcServer<Request, Response>(uri, transform_to_request, transform_to_response);
+
+    const auto motor_ctor = [](const StepperCreateCfg& cfg) {
+		return new TestStepperMotor(
+			[](const StepperMotor::Direction& direction) {
+				const auto dir_to_str_map = std::map<StepperMotor::Direction, std::string> {
+					{StepperMotor::Direction::CCW, "<-CCW"},
+					{StepperMotor::Direction::CW, "CW->"},
+				};
+				std::cout << dir_to_str_map.at(direction);
+				std::cout.flush();
+			}
+		);
+	};
+	const auto delay_gtor = [](const std::size_t& timeout_ms) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(timeout_ms));
+	};
+    auto manager = StepperMotorManager<StepperCreateCfg>(motor_ctor, delay_gtor);
+
+    auto host = Host<Request, Response>(
+        &ipc_server,
+        &manager,
+        [](const std::exception& e) {
+            return Response(Response::ResultCode::EXCEPTION);
+        }
+    );
+
+    host.run();
+
+    return 0;
+}
+
+static inline std::string retrieve_motor_id(const Json::Value& root) {
+    const auto value_ptr = root.find("motor_id");
+    if (!value_ptr || !(value_ptr->isString())) {
+        throw std::invalid_argument("motor_id not found or has invalid format");
+    }
+    return static_cast<std::string>(value_ptr->asString());
+}
+
+inline static typename StepperMotorRequest<StepperCreateCfg>::Type retrieve_request_type(const web::http::http_request& http_request) {
+    using ReqType = typename StepperMotorRequest<StepperCreateCfg>::Type;
+    const auto type_map = std::map<std::string, ReqType> {
+        {"POST",  ReqType::CREATE_STEPPER},
+        {"DEL",  ReqType::DELETE_STEPPER},
+        {"GET",    ReqType::READ_STEPPER},
+        {"PUT",           ReqType::STEPS},
+    };
+    const auto iter = type_map.find(http_request.method());
+    if (type_map.end() == iter) {
+        throw std::invalid_argument("unsupported request type");
+    }
+    return iter->second;
+}
+
+inline static typename StepperMotorRequest<StepperCreateCfg>::Steps retrieve_steps(const Json::Value& steps_cfg) {
+    using ReqSteps = typename StepperMotorRequest<StepperCreateCfg>::Steps;
+    const auto type_map = std::map<std::string, StepperMotor::Direction> {
+        {"CW",  StepperMotor::Direction::CW},
+        {"CCW", StepperMotor::Direction::CCW},
+    };
+    const auto direction_ptr = steps_cfg.find("direction");
+    if (!direction_ptr || !(direction_ptr->isString())) {
+        throw std::invalid_argument("direction not found or has invalid format");
+    }
+    const auto iter = type_map.find(direction_ptr->asString());
+    if (type_map.end() == iter) {
+        throw std::invalid_argument("unsupported direction");
+    }
+    const auto steps_number_ptr = steps_cfg.find("steps_number");
+    if (!steps_number_ptr || !(steps_number_ptr->isInt())) {
+        throw std::invalid_argument("steps_number not found or has invalid format");
+    }
+    const auto step_duration_ptr = steps_cfg.find("step_duration");
+    if (!step_duration_ptr || !(step_duration_ptr->isInt())) {
+        throw std::invalid_argument("step_duration not found or has invalid format");
+    }
+    return ReqSteps {
+        .direction = iter->second,
+        .steps_number = static_cast<std::size_t>(steps_number_ptr->asInt()),
+        .step_duration = static_cast<std::size_t>(step_duration_ptr->asInt()),
+    };
+}
+
+inline Option<StepperMotorRequest<StepperCreateCfg>> transform_to_request(const web::http::http_request& http_request) {
+    const auto request_task = http_request.extract_vector();
+    if (pplx::task_status::completed != request_task.wait()) {
+        throw std::runtime_error("failed to extract request data");
+    }
+    const auto raw_data = request_task.get();
+    auto reader = Json::Reader();
+    auto root = Json::Value();
+    if (!reader.parse(std::string(raw_data.begin(), raw_data.end()), root)) {
+        throw std::invalid_argument("failed to parse data from input");
+    }
+    if (!root.isObject()) {
+        throw std::invalid_argument("wrong data format");
+    }
+
+    const auto request_type = retrieve_request_type(http_request);
+    const auto motor_id = retrieve_motor_id(root);
+
+    auto request = StepperMotorRequest<StepperCreateCfg>(request_type, motor_id);
+    const auto create_cfg_ptr = root.find("create_cfg");
+    if (create_cfg_ptr && create_cfg_ptr->isString()) {
+        request.set_create_config(static_cast<StepperCreateCfg>(create_cfg_ptr->asString()));
+    }
+    const auto steps_cfg_ptr = root.find("steps_cfg");
+    if (steps_cfg_ptr && steps_cfg_ptr->isObject()) {
+        request.set_steps(retrieve_steps(*steps_cfg_ptr));
+    }
+    return Option<StepperMotorRequest<StepperCreateCfg>>(new StepperMotorRequest<StepperCreateCfg>(request));
+}
+
+inline web::http::http_response transform_to_response(const StepperMotorResponse& response) {
+    using RespRes = typename StepperMotorResponse::ResultCode;
+    const auto type_map = std::map<RespRes, web::http::status_code> {
+        {RespRes::OK,           web::http::status_codes::OK},
+        {RespRes::BAD_REQUEST,  web::http::status_codes::BadRequest},
+        {RespRes::NOT_FOUND,    web::http::status_codes::NotFound},
+        {RespRes::UNSUPPORTED,  web::http::status_codes::MethodNotAllowed},
+        {RespRes::EXCEPTION,    web::http::status_codes::InternalError},
+    };
+
+    const auto iter = type_map.find(response.code());
+    if (type_map.end() == iter) {
+        throw std::invalid_argument("unsupported result code");
+    }
+    auto http_resp = web::http::http_response(iter->second);
+    auto resp_body = Json::Value();
+    if (response.has_message()) {
+        resp_body["message"] = response.message();
+    }
+    if (response.has_state()) {
+        resp_body["state"] = static_cast<int>(response.state());
+    }
+    const auto resp_body_raw = Json::FastWriter().write(resp_body);
+    http_resp.set_body(resp_body_raw, utf8string("application/json; charset=utf-8"));
+    return http_resp;
+}
