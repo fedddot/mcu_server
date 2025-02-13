9e53a6d8b903dad049cd941828bada876d806916
fedddot <idanrommm@gmail.com>
2025-02-13 19:12:38 +0000

    ctor implemented

--- ipc/custom_ipc_server/include/custom_ipc_server.hpp
+++ ipc/custom_ipc_server/include/custom_ipc_server.hpp
@@ -1,30 +1,63 @@
 #ifndef	CUSTOM_IPC_SERVER_HPP
 #define	CUSTOM_IPC_SERVER_HPP
 
+#include <functional>
 #include <stdexcept>
 
 #include "ipc_server.hpp"
 
 namespace ipc {
-	/// @brief Customizable synchronous IPC server aggregating an external FIFO buffer
-	template <typename Request, typename Response>
+	/// @brief Customizable synchronous IPC server aggregating an external raw data buffer
+	template <typename Request, typename Response, typename RawDataBuffer>
 	class CustomIpcServer: IpcServer<Request, Response> {
 	public:
 		using RequestCallback = typename IpcServer<Request, Response>::RequestCallback;
-		CustomIpcServer();
+
+		/// @brief Tests receiving data buffer for incoming requests
+		/// @return true if data buffer contains a request data and it can be read from the buffer
+		using RequestCapturer = std::function<bool(const RawDataBuffer&)>;
+		
+		/// @brief Extracts a request from raw data buffer erasing its raw data
+		/// @return extracted request
+		using RequestReader = std::function<Request(RawDataBuffer *)>;
+
+		/// @brief Writes response data into output IPC stream
+		using ResponseWriter = std::function<void(const Response&)>;
+		
+		CustomIpcServer(
+			RawDataBuffer *raw_data,
+			const RequestCapturer& request_capturer,
+			const RequestReader& request_reader,
+			const ResponseWriter& response_writer
+		);
 		CustomIpcServer(const CustomIpcServer&) = delete;
 		CustomIpcServer& operator=(const CustomIpcServer&) = delete;
 		
 		void serve_once(const RequestCallback& request_callback) override;
+	private:
+		RawDataBuffer *m_raw_data;
+		RequestCapturer m_request_capturer;
+		RequestReader m_request_reader;
+		ResponseWriter m_response_writer;
 	};
 
-	template <typename Request, typename Response>
-	inline CustomIpcServer<Request, Response>::CustomIpcServer() {
-		throw std::runtime_error("NOT IMPLEMENTED");
+	template <typename Request, typename Response, typename RawDataBuffer>
+	inline CustomIpcServer<Request, Response, RawDataBuffer>::CustomIpcServer(
+		RawDataBuffer *raw_data,
+		const RequestCapturer& request_capturer,
+		const RequestReader& request_reader,
+		const ResponseWriter& response_writer
+	): m_raw_data(raw_data), m_request_capturer(request_capturer), m_request_reader(request_reader), m_response_writer(response_writer) {
+		if (!m_raw_data) {
+			throw std::invalid_argument("invalid raw data buffer pointer received");
+		}
+		if (!m_request_capturer || !m_request_reader || !m_response_writer) {
+			throw std::invalid_argument("one or more of injected functions are invalid");
+		}
 	}
 
-	template <typename Request, typename Response>
-	inline void CustomIpcServer<Request, Response>::serve_once(const RequestCallback& request_callback) {
+	template <typename Request, typename Response, typename RawDataBuffer>
+	inline void CustomIpcServer<Request, Response, RawDataBuffer>::serve_once(const RequestCallback& request_callback) {
 		throw std::runtime_error("NOT IMPLEMENTED");
 	}
 }
--- ipc/custom_ipc_server/tests/src/ut_custom_ipc_server.cpp
+++ ipc/custom_ipc_server/tests/src/ut_custom_ipc_server.cpp
@@ -1,4 +1,6 @@
 #include "gtest/gtest.h"
+#include <stdexcept>
+#include <vector>
 
 #include "custom_ipc_server.hpp"
 
@@ -6,18 +8,34 @@ using namespace ipc;
 
 using TestRequest = std::string;
 using TestResponse = int;
+using TestRawData = std::vector<char>;
 
-using TestServer = CustomIpcServer<TestRequest, TestResponse>;
+using TestServer = CustomIpcServer<TestRequest, TestResponse, TestRawData>;
 
 TEST(ut_custom_ipc_server, ctor_dtor_sanity) {
     // GIVEN
+    auto capturer = [](const TestRawData&)-> bool {
+        throw std::runtime_error("NOT IMPLEMENTED");
+    };
+    auto reader = [](TestRawData *)-> TestRequest {
+        throw std::runtime_error("NOT IMPLEMENTED");
+    };
+    auto writer = [](const TestResponse&) {
+        throw std::runtime_error("NOT IMPLEMENTED");
+    };
 
     // WHEN
+    auto test_buffer = TestRawData();
     TestServer *instance_ptr(nullptr);
 
     // THEN
     ASSERT_NO_THROW(
-        instance_ptr = new TestServer()
+        instance_ptr = new TestServer(
+            &test_buffer,
+            capturer,
+            reader,
+            writer
+        )
     );
     ASSERT_NO_THROW(delete instance_ptr);
     instance_ptr = nullptr;
