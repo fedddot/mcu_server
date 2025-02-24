3d500f32b5cef10e2caaedd3acd83c21619f4bf5
fedddot <idanrommm@gmail.com>
2025-02-24 15:49:03 +0000

    test objects added

---
+++ ipc/tests/include/test_request_reader.hpp
@@ -0,0 +1,28 @@
+#ifndef	TEST_REQUEST_READER_HPP
+#define	TEST_REQUEST_READER_HPP
+
+#include <functional>
+#include <optional>
+
+#include "request_reader.hpp"
+
+namespace ipc {
+	template <typename Request>
+	class TestRequestReader: RequestReader<Request> {
+	public:
+		using Action = std::function<std::optional<Request>(void)>;
+		TestRequestReader(const Action& action): m_action(action) {
+			
+		}
+		TestRequestReader(const TestRequestReader&) = default;
+		TestRequestReader& operator=(const TestRequestReader&) = default;
+		
+		std::optional<Request> read() override {
+			return m_action();
+		}
+	private:
+		Action m_action;
+	};
+}
+
+#endif // TEST_REQUEST_READER_HPP
---
+++ ipc/tests/include/test_response_writer.hpp
@@ -0,0 +1,25 @@
+#ifndef	TEST_RESPONSE_WRITER_HPP
+#define	TEST_RESPONSE_WRITER_HPP
+
+#include <functional>
+
+namespace ipc {
+	template <typename Response>
+	class TestResponseWriter {
+	public:
+		using Action = std::function<void(const Response&)>;
+		TestResponseWriter(const Action& action): m_action(action) {
+			
+		}
+		TestResponseWriter(const TestResponseWriter&) = default;
+		TestResponseWriter& operator=(const TestResponseWriter&) = default;
+
+		void write(const Response& response) const override {
+			m_action(response);
+		}
+	private:
+		Action m_action;
+	};
+}
+
+#endif // TEST_RESPONSE_WRITER_HPP
