ad2c257d809b874b2f018d578a7bc52160ff284c
fedddot <idanrommm@gmail.com>
2025-02-24 15:36:30 +0000

    interfaces splitted

---
+++ ipc/include/request_reader.hpp
@@ -0,0 +1,18 @@
+#ifndef	REQUEST_READER_HPP
+#define	REQUEST_READER_HPP
+
+#include <optional>
+namespace ipc {
+	/// @brief Synchronous IPC Requests Reader
+	template <typename Request>
+	class RequestReader {
+	public:
+		virtual ~RequestReader() noexcept = default;
+
+		/// @brief Reads a single request (if exists) from an IPC stream and returns it as an optional value 
+		/// @return if there is an incoming request returns non-empty option, an empty one otherwise
+		virtual std::optional<Request> read() = 0;
+	};
+}
+
+#endif // REQUEST_READER_HPP
