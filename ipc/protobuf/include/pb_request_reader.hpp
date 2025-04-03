92fa69f28757ce6438bfb26480e470f05b0741d3
fedddot <idanrommm@gmail.com>
2025-04-03 17:32:21 +0000

    first success

--- ipc/protobuf/include/pb_request_reader.hpp
+++ ipc/protobuf/include/pb_request_reader.hpp
@@ -1,6 +1,7 @@
 #ifndef	PB_REQUEST_READER_HPP
 #define	PB_REQUEST_READER_HPP
 
+#include <functional>
 #include <optional>
 #include <stdexcept>
 
@@ -13,33 +14,40 @@ namespace ipc {
 	template <typename Request>
 	class ProtobufRequestReader: public RequestReader<Request> {
 	public:
+		using InitialRequestCreator = std::function<Request()>;
 		ProtobufRequestReader(
 			pb_istream_t *input_stream_ptr,
-			const pb_msgdesc_t *fields
+			const pb_msgdesc_t *fields,
+			const InitialRequestCreator& init_request_ctor
 		);
 		std::optional<Request> read() override;
 	private:
 		pb_istream_t *m_input_stream_ptr;
 		const pb_msgdesc_t *m_fields;
+		const InitialRequestCreator& m_init_request_ctor;
 	};
 
 	template <typename Request>
 	inline ProtobufRequestReader<Request>::ProtobufRequestReader(
 		pb_istream_t *input_stream_ptr,
-		const pb_msgdesc_t *fields
-	): m_input_stream_ptr(input_stream_ptr), m_fields(fields) {
+		const pb_msgdesc_t *fields,
+		const InitialRequestCreator& init_request_ctor
+	): m_input_stream_ptr(input_stream_ptr), m_fields(fields), m_init_request_ctor(init_request_ctor) {
 		if (!input_stream_ptr) {
 			throw std::invalid_argument("invalid input stream ptr received");
 		}
 		if (!fields) {
 			throw std::invalid_argument("invalid fields object ptr received");
 		}
+		if (!init_request_ctor) {
+			throw std::invalid_argument("invalid init request ctor received");
+		}
 	}
 
 	template <typename Request>
 	inline std::optional<Request> ProtobufRequestReader<Request>::read() {
-		auto request = Request();
-		if (!pb_decode(m_input_stream_ptr, m_fields, &request)) {
+		auto request = m_init_request_ctor();
+		if (!pb_decode_nullterminated(m_input_stream_ptr, m_fields, &request)) {
 			return std::optional<Request>();
 		}
 		return std::optional<Request>(request);
--- ipc/protobuf/tests/src/ut_pb_request_reader.cpp
+++ ipc/protobuf/tests/src/ut_pb_request_reader.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
+#include <cstdio>
 #include <cstring>
 #include <stdexcept>
+#include <vector>
 
 #include "pb.h"
 #include "pb_decode.h"
@@ -21,22 +23,48 @@ TEST(ut_pb_request_reader, ctor_dtor_sanity) {
 	ProtobufRequestReader<example_ExampleRequest> *instance_ptr(nullptr);
 
 	// THEN
-	ASSERT_NO_THROW(instance_ptr = new ProtobufRequestReader<example_ExampleRequest>(&stream, example_ExampleRequest_fields));
+	ASSERT_NO_THROW(
+		instance_ptr = new ProtobufRequestReader<example_ExampleRequest>(
+			&stream,
+			example_ExampleRequest_fields,
+			[]() -> example_ExampleRequest {
+				return example_ExampleRequest {};
+			}
+		)
+	);
 	ASSERT_NO_THROW(delete instance_ptr);
 }
 
 bool encode(pb_ostream_t *stream, const pb_field_t *field, void * const *arg) {
+	if (!pb_encode_tag_for_field(stream, field)) {
+        return false;
+	}
 	auto str = (const char *)(*arg);
 	return pb_encode_string(stream, (const pb_byte_t *)str, std::strlen(str));
+}
+
+bool decode(pb_istream_t *stream, const pb_field_t *field, void **arg) {
+    uint8_t buffer[1024] = {0};
+    
+    /* We could read block-by-block to avoid the large buffer... */
+    if (stream->bytes_left > sizeof(buffer) - 1)
+        return false;
+    
+    if (!pb_read(stream, buffer, stream->bytes_left))
+        return false;
+    
+    /* Print the string, in format comparable with protoc --decode.
+     * Format comes from the arg defined in main().
+     */
+    std::printf((char*)*arg, buffer);
+    return true;
 }
 
 TEST(ut_pb_request_reader, read_sanity) {
 	// GIVEN
 	enum: int { BUFF_SIZE = 1024 };
 	pb_byte_t buff[BUFF_SIZE] = { '\0' };
-	auto stream = pb_istream_from_buffer(buff, BUFF_SIZE);
 	
-
 	auto test_output_stream = pb_ostream_from_buffer(buff, BUFF_SIZE);
 	auto test_string_param = "asdf";
 	auto test_string = pb_callback_t {
@@ -45,16 +73,32 @@ TEST(ut_pb_request_reader, read_sanity) {
 	};
 	auto test_request = example_ExampleRequest {
 		.enum_param = example_EnumParam_TWO,
-		.string_param = test_string,
-		.int_param = 4,
+		.string_param = test_string
 	};
-	ASSERT_TRUE(pb_encode(&test_output_stream, example_ExampleRequest_fields, &test_request));
+	ASSERT_TRUE(pb_encode_nullterminated(&test_output_stream, example_ExampleRequest_fields, &test_request));
+
+	auto stream = pb_istream_from_buffer(buff, BUFF_SIZE);
+	pb_byte_t buffer[BUFF_SIZE] = { '\0' };
 
 	// WHEN
-	ProtobufRequestReader<example_ExampleRequest> instance(&stream, example_ExampleRequest_fields);
+	ProtobufRequestReader<example_ExampleRequest> instance(
+		&stream,
+		example_ExampleRequest_fields,
+		[buffer]() -> example_ExampleRequest {
+			return example_ExampleRequest {
+				.string_param = {
+					.funcs {
+						.decode = decode
+					},
+					.arg = (void *)buffer
+				}
+			};
+		}
+	);
 	auto result = std::optional<example_ExampleRequest>();
 
 	// THEN
-	ASSERT_NO_THROW(result = instance.read());
+	result = instance.read();
+	// ASSERT_NO_THROW();
 	ASSERT_TRUE(result);
 }
