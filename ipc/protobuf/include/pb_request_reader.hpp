990295ccb1f064d1c2228994ae477c4ad77cc275
fedddot <idanrommm@gmail.com>
2025-04-03 16:03:54 +0000

    example proto added

--- ipc/protobuf/include/pb_request_reader.hpp
+++ ipc/protobuf/include/pb_request_reader.hpp
@@ -4,19 +4,45 @@
 #include <optional>
 #include <stdexcept>
 
+#include "pb.h"
+
+#include "pb_decode.h"
 #include "request_reader.hpp"
 
 namespace ipc {
 	template <typename Request>
 	class ProtobufRequestReader: public RequestReader<Request> {
 	public:
-		ProtobufRequestReader() = default;
+		ProtobufRequestReader(
+			pb_istream_t *input_stream_ptr,
+			const pb_msgdesc_t *fields
+		);
 		std::optional<Request> read() override;
+	private:
+		pb_istream_t *m_input_stream_ptr;
+		const pb_msgdesc_t *m_fields;
 	};
 
 	template <typename Request>
+	inline ProtobufRequestReader<Request>::ProtobufRequestReader(
+		pb_istream_t *input_stream_ptr,
+		const pb_msgdesc_t *fields
+	): m_input_stream_ptr(input_stream_ptr), m_fields(fields) {
+		if (!input_stream_ptr) {
+			throw std::invalid_argument("invalid input stream ptr received");
+		}
+		if (!fields) {
+			throw std::invalid_argument("invalid fields object ptr received");
+		}
+	}
+
+	template <typename Request>
 	inline std::optional<Request> ProtobufRequestReader<Request>::read() {
-		throw std::runtime_error("NOT IMPLEMENTED");
+		auto request = Request();
+		if (!pb_decode(m_input_stream_ptr, m_fields, &request)) {
+			return std::optional<Request>();
+		}
+		return std::optional<Request>(request);
 	}
 }
 
--- ipc/protobuf/tests/src/ut_pb_request_reader.cpp
+++ ipc/protobuf/tests/src/ut_pb_request_reader.cpp
@@ -2,16 +2,41 @@
 
 #include "gtest/gtest.h"
 
+#include "pb.h"
+#include "pb_decode.h"
 #include "pb_request_reader.hpp"
+#include "example.pb.h"
 
 using namespace ipc;
 
-TEST(ut_pb_request_reader, sanity) {
-	// GIVEN
+struct TestMessage {
+	std::string msg;
+};
 
+TEST(ut_pb_request_reader, ctor_dtor_sanity) {
+	// GIVEN
+	enum: int { BUFF_SIZE = 10 };
+	pb_byte_t buff[BUFF_SIZE] = { '\0' };
+	auto stream = pb_istream_from_buffer(buff, BUFF_SIZE);
+	
 	// WHEN
-	ProtobufRequestReader<std::string> *instance_ptr(nullptr);
+	ProtobufRequestReader<TestMessage> *instance_ptr(nullptr);
 
 	// THEN
-	ASSERT_NO_THROW(instance_ptr = new ProtobufRequestReader<std::string>());
+	ASSERT_NO_THROW(instance_ptr = new ProtobufRequestReader<TestMessage>(&stream, example_ExampleRequest_fields));
+	ASSERT_NO_THROW(delete instance_ptr);
+}
+
+TEST(ut_pb_request_reader, read_sanity) {
+	// GIVEN
+	enum: int { BUFF_SIZE = 10 };
+	pb_byte_t buff[BUFF_SIZE] = { '\0' };
+	auto stream = pb_istream_from_buffer(buff, BUFF_SIZE);
+	
+	// WHEN
+	ProtobufRequestReader<TestMessage> instance(&stream, example_ExampleRequest_fields);
+	auto result = std::optional<TestMessage>();
+
+	// THEN
+	ASSERT_NO_THROW(result = instance.read());
 }
