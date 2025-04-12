b2b42ff1895e9f4ae05faa20cae172f923a00075
fedddot <idanrommm@gmail.com>
2025-04-12 13:21:28 +0000

    test manager updated

--- manager/tests/include/test_manager.hpp
+++ manager/tests/include/test_manager.hpp
@@ -4,20 +4,24 @@
 #include <functional>
 
 #include "manager.hpp"
+#include "clonable_manager.hpp"
 
 namespace manager_tests {
 	template <typename Request, typename Response>
-	class TestManager: public manager::Manager<Request, Response> {
+	class TestManager: public manager::ClonableManager<Request, Response> {
 	public:
 		using Action = std::function<Response(const Request&)>;
 		TestManager(const Action& action): m_action(action) {
 
 		}
-		TestManager(const TestManager&) = delete;
-		TestManager& operator=(const TestManager&) = delete;
+		TestManager(const TestManager&) = default;
+		TestManager& operator=(const TestManager&) = default;
 		
 		Response run(const Request& request) override {
 			return m_action(request);
+		}
+		manager::Manager<Request, Response> *clone() const {
+			return new TestManager(*this);
 		}
 	private:
 		Action m_action;
