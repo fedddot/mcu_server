a573d714fbea1ed1ce1148350fce72737b73aa79
fedddot <idanrommm@gmail.com>
2025-04-12 09:42:01 +0000

    json_ipc_data_reader reimplemented

--- ipc/json/include/json_ipc_data_reader.hpp
+++ ipc/json/include/json_ipc_data_reader.hpp
@@ -2,6 +2,7 @@
 #define	JSON_IPC_DATA_READER_HPP
 
 #include <functional>
+#include <memory>
 #include <optional>
 #include <stdexcept>
 #include <vector>
@@ -10,48 +11,54 @@
 #include "json/value.h"
 
 #include "ipc_data_reader.hpp"
-#include "shared_ipc_data_reader.hpp"
+#include "clonable_ipc_data_reader.hpp"
 
 namespace ipc {
 	template <typename IpcData>
-	class JsonIpcDataReader: public IpcDataReader<IpcData> {
+	class JsonIpcDataReader: public ClonableIpcDataReader<IpcData> {
 	public:
 		using RawData = std::vector<char>;
 		using IpcDataParserFromJsonData = std::function<IpcData(const Json::Value&)>;
 		
 		JsonIpcDataReader(
-			const SharedIpcDataReader<RawData>& raw_data_reader,
-			const IpcDataParserFromJsonData& request_parser
+			const ClonableIpcDataReader<RawData>& raw_data_reader,
+			const IpcDataParserFromJsonData& ipc_data_parser
 		);
 		JsonIpcDataReader(const JsonIpcDataReader&) = delete;
 		JsonIpcDataReader& operator=(const JsonIpcDataReader&) = delete;
 		std::optional<IpcData> read() override;
+		IpcDataReader<IpcData> *clone() const override;
 	private:
-		SharedIpcDataReader<RawData> m_raw_data_reader;
-		const IpcDataParserFromJsonData m_request_parser;
+		std::shared_ptr<IpcDataReader<RawData>> m_raw_data_reader;
+		const IpcDataParserFromJsonData m_ipc_data_parser;
 
 		static Json::Value parse_raw_data(const RawData& data);
 	};
 
 	template <typename IpcData>
 	inline JsonIpcDataReader<IpcData>::JsonIpcDataReader(
-		const SharedIpcDataReader<RawData>& raw_data_reader,
-		const IpcDataParserFromJsonData& request_parser
-	): m_raw_data_reader(raw_data_reader), m_request_parser(request_parser) {
-		if (!m_request_parser) {
-			throw std::invalid_argument("invalid request parser received");
+		const ClonableIpcDataReader<RawData>& raw_data_reader,
+		const IpcDataParserFromJsonData& ipc_data_parser
+	): m_raw_data_reader(raw_data_reader.clone()), m_ipc_data_parser(ipc_data_parser) {
+		if (!m_ipc_data_parser) {
+			throw std::invalid_argument("invalid ipc_data parser received");
 		}
 	}
 
 	template <typename IpcData>
 	inline std::optional<IpcData> JsonIpcDataReader<IpcData>::read() {
-		const auto raw_data = m_raw_data_reader.read();
+		const auto raw_data = m_raw_data_reader->read();
 		if (!raw_data) {
 			return std::optional<IpcData>();
 		}
 		const auto json_data = parse_raw_data(*raw_data);
-		const auto parsed_request = m_request_parser(json_data);
-		return std::optional<IpcData>(parsed_request);
+		const auto parsed_ipc_data = m_ipc_data_parser(json_data);
+		return std::optional<IpcData>(parsed_ipc_data);
+	}
+
+	template <typename IpcData>
+	inline IpcDataReader<IpcData> *JsonIpcDataReader<IpcData>::clone() const {
+		return new JsonIpcDataReader(*this);
 	}
 
 	template <typename IpcData>
@@ -59,7 +66,7 @@ namespace ipc {
 		Json::Value root;
 	    Json::Reader reader;
 		if (!reader.parse(std::string(data.begin(), data.end()), std::ref(root), true)) {
-			throw std::runtime_error("failed to parse raw request data: " + reader.getFormattedErrorMessages());
+			throw std::runtime_error("failed to parse raw ipc_data data: " + reader.getFormattedErrorMessages());
 		}
 		return root;
 	}
