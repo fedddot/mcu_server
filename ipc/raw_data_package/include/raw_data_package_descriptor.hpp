a0fd92782b371be32f7617c02c8acb9c5f3feaa1
fedddot <idanrommm@gmail.com>
2025-04-12 11:25:56 +0000

    desc updated

--- ipc/raw_data_package/include/raw_data_package_descriptor.hpp
+++ ipc/raw_data_package/include/raw_data_package_descriptor.hpp
@@ -6,80 +6,39 @@
 #include <vector>
 
 namespace ipc {
-	template <typename Input, typename Output>
-	class DataTransformer {
+	class RawDataPackageDescriptor {
 	public:
-		virtual ~DataTransformer() noexcept = default;
-		virtual Output transform(const Input& input) const = 0;
-	};
-
-	class DefaultPackageSizeParser: public DataTransformer<std::vector<char>, std::size_t> {
-	public:
-		DefaultPackageSizeParser(const std::size_t& raw_data_size);
-		DefaultPackageSizeParser(const DefaultPackageSizeParser&) = default;
-		DefaultPackageSizeParser& operator=(const DefaultPackageSizeParser&) = default;
-		std::size_t transform(const std::vector<char>& input) const override;
-		std::size_t raw_data_size() const;
+		using RawData = std::vector<char>;
+		RawDataPackageDescriptor(
+			const RawData& preamble,
+			const std::size_t& encoded_size_length
+		);
+		virtual ~RawDataPackageDescriptor() noexcept = default;
+		RawData preamble() const;
+		std::size_t encoded_size_length() const;
 	private:
-		enum: std::size_t {
-			BITS_IN_BYTE = 8UL,
-		};
-		std::size_t m_raw_data_size;
+		RawData m_preamble;
+		std::size_t m_encoded_size_length;
 	};
 
-	inline DefaultPackageSizeParser::DefaultPackageSizeParser(const std::size_t& raw_data_size): m_raw_data_size(raw_data_size) {
-		if (1UL > m_raw_data_size) {
-			throw std::invalid_argument("received raw data size is too small");
+	inline RawDataPackageDescriptor::RawDataPackageDescriptor(
+		const RawData& preamble,
+		const std::size_t& encoded_size_length
+	): m_preamble(preamble), m_encoded_size_length(encoded_size_length) {
+		if (m_preamble.empty()) {
+			throw std::invalid_argument("empty preamble received");
+		}
+		if (m_encoded_size_length < 1) {
+			throw std::invalid_argument("encoded size length must be at least 1");
 		}
 	}
 
-	inline std::size_t DefaultPackageSizeParser::transform(const std::vector<char>& input) const {
-		if (m_raw_data_size != input.size()) {
-			throw std::invalid_argument("invalid raw data size");
-		}
-		auto decoded_size = std::size_t(0UL);
-		for (auto i = 0; i < m_raw_data_size; ++i) {
-			decoded_size <<= BITS_IN_BYTE;
-			decoded_size |= static_cast<std::size_t>(input[m_raw_data_size - 1 - i]);
-		}
-		return decoded_size;
+	inline typename RawDataPackageDescriptor::RawData RawDataPackageDescriptor::preamble() const {
+		return m_preamble;
 	}
 
-	inline std::size_t DefaultPackageSizeParser::raw_data_size() const {
-		return m_raw_data_size;
-	}
-
-	class DefaultPackageSizeSerializer: public DataTransformer<std::size_t, std::vector<char>> {
-	public:
-		DefaultPackageSizeSerializer(const std::size_t& raw_data_size);
-		DefaultPackageSizeSerializer(const DefaultPackageSizeSerializer&) = default;
-		DefaultPackageSizeSerializer& operator=(const DefaultPackageSizeSerializer&) = default;
-		std::vector<char> transform(const std::size_t& input) const override;
-		std::size_t raw_data_size() const;
-	private:
-		enum: std::size_t {
-			BITS_IN_BYTE = 8UL,
-		};
-		std::size_t m_raw_data_size;
-	};
-
-	inline DefaultPackageSizeSerializer::DefaultPackageSizeSerializer(const std::size_t& raw_data_size): m_raw_data_size(raw_data_size) {
-		if (1UL > m_raw_data_size) {
-			throw std::invalid_argument("received raw data size is too small");
-		}
-	}
-
-	inline std::vector<char> DefaultPackageSizeSerializer::transform(const std::size_t& input) const {
-		auto encoded_size = std::vector<char>(m_raw_data_size, 0UL);
-		for (auto i = 0; i < m_raw_data_size; ++i) {
-			const auto less_sign_byte = static_cast<char>((input >> BITS_IN_BYTE * i) & 0xFF);
-			encoded_size[i] = less_sign_byte;
-		}
-		return encoded_size;
-	}
-
-	inline std::size_t DefaultPackageSizeSerializer::raw_data_size() const {
-		return m_raw_data_size;
+	inline std::size_t RawDataPackageDescriptor::encoded_size_length() const {
+		return m_encoded_size_length;
 	}
 }
 
