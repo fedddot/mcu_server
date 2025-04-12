4ab435c043d17e85e910d2a2d79b584f635cd986
fedddot <idanrommm@gmail.com>
2025-04-12 07:51:34 +0000

    updated

--- ipc/json/include/json_request_reader.hpp
+++ ipc/json/include/json_request_reader.hpp
@@ -1,8 +1,13 @@
 #ifndef	JSON_REQUEST_READER_HPP
 #define	JSON_REQUEST_READER_HPP
 
+#include <functional>
 #include <optional>
 #include <stdexcept>
+#include <vector>
+
+#include "json/reader.h"
+#include "json/value.h"
 
 #include "request_reader.hpp"
 
@@ -10,24 +15,50 @@ namespace ipc {
 	template <typename Request>
 	class JsonRequestReader: public RequestReader<Request> {
 	public:
+		using RequestParser = std::function<Request(const Json::Value&)>;
 		JsonRequestReader(
-
+			const SharedRequestReader<std::vector<char>>& raw_data_reader,
+			const RequestParser& request_parser
 		);
 		JsonRequestReader(const JsonRequestReader&) = delete;
 		JsonRequestReader& operator=(const JsonRequestReader&) = delete;
 		std::optional<Request> read() override;
 	private:
+		SharedRequestReader<std::vector<char>> m_raw_data_reader;
+		const RequestParser m_request_parser;
+
+		static Json::Value parse_raw_data(const std::vector<char>& data);
 	};
 
 	template <typename Request>
 	inline JsonRequestReader<Request>::JsonRequestReader(
-	) {
-		throw std::runtime_error("NOT IMPLEMENTED");
+		const SharedRequestReader<std::vector<char>>& raw_data_reader,
+		const RequestParser& request_parser
+	): m_raw_data_reader(raw_data_reader), m_request_parser(request_parser) {
+		if (!m_request_parser) {
+			throw std::invalid_argument("invalid request parser received");
+		}
 	}
 
 	template <typename Request>
 	inline std::optional<Request> JsonRequestReader<Request>::read() {
-		throw std::runtime_error("NOT IMPLEMENTED");
+		const auto raw_data = m_raw_data_reader.read();
+		if (!raw_data) {
+			return std::optional<Request>();
+		}
+		const auto json_data = parse_raw_data(*raw_data);
+		const auto parsed_request = m_request_parser(json_data);
+		return std::optional<Request>(parsed_request);
+	}
+
+	template <typename Request>
+	Json::Value JsonRequestReader<Request>::parse_raw_data(const std::vector<char>& data) {
+		Json::Value root;
+	    Json::Reader reader;
+		if (!reader.parse(std::string(data.begin(), data.end()), std::ref(root), true)) {
+			throw std::runtime_error("failed to parse raw request data: " + reader.getFormattedErrorMessages());
+		}
+		return root;
 	}
 }
 
--- ipc/json/tests/src/ut_json_request_reader.cpp
+++ ipc/json/tests/src/ut_json_request_reader.cpp
@@ -1,31 +1,69 @@
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 #include "gtest/gtest.h"
+// #include "json/value.h"
 
 #include "json_request_reader.hpp"
+#include "request_reader.hpp"
 
 using namespace ipc;
 
 using TestRequest = std::string;
 
+class TestRawReader: public RequestReader<std::vector<char>> {
+public:
+	TestRawReader() = default;
+	TestRawReader(const TestRawReader&) = default;
+	TestRawReader& operator=(const TestRawReader&) = default;
+	
+	std::optional<std::vector<char>> read() override {
+		return m_data_option;
+	}
+	void update_data(const std::vector<char>& data) {
+		m_data_option = data;
+	}
+private:
+	std::optional<std::vector<char>> m_data_option;
+};
+
 TEST(ut_json_request_reader, ctor_dtor_sanity) {
 	// GIVEN
+	const auto raw_data_reader = SharedRequestReader<std::vector<char>>(new TestRawReader());
 	
 	// WHEN
 	JsonRequestReader<TestRequest> *instance = nullptr;
 
 	// THEN
-	ASSERT_NO_THROW(instance = new JsonRequestReader<TestRequest>());
+	ASSERT_NO_THROW(
+		instance = new JsonRequestReader<TestRequest>(
+			raw_data_reader,
+			[](const std::vector<char>&) -> TestRequest {
+				throw std::runtime_error("NOT IMPLEMENTED");
+			}
+		)
+	);
 	ASSERT_NO_THROW(delete instance);
 	instance = nullptr;
 }
 
+static std::vector<char> serialize_request(const TestRequest& request);
+
 TEST(ut_json_request_reader, read_sanity) {
 	// GIVEN
 	const auto request = TestRequest("request");
+	const auto request_json_raw = serialize_request(request);
 	
 	// WHEN
-	auto instance = JsonRequestReader<TestRequest>();
+	auto raw_reader = TestRawReader();
+	raw_reader.update_data(request_json_raw);
+	auto instance = JsonRequestReader<TestRequest>(
+		SharedRequestReader<std::vector<char>>(new TestRawReader(raw_reader)),
+		[](const std::vector<char>&) -> TestRequest {
+			throw std::runtime_error("NOT IMPLEMENTED");
+		}
+	);
 	auto result = std::optional<TestRequest>();
 
 	// THEN
