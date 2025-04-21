b81cdca803e1c9bc4d427a3e9aa9d88670026fb5
fedddot <idanrommm@gmail.com>
2025-04-21 19:24:29 +0000

    writer infrared

--- app/stepper_motor_app/include/stepper_ipc_data_writer.hpp
+++ app/stepper_motor_app/include/stepper_ipc_data_writer.hpp
@@ -3,12 +3,10 @@
 
 #include <vector>
 
-#include "json/value.h"
-#include "json/writer.h"
-
 #include "clonable_ipc_data_writer.hpp"
 #include "ipc_data_writer.hpp"
 #include "json_ipc_data_writer.hpp"
+#include "stepper_ipc_data_infra.hpp"
 #include "stepper_motor_response.hpp"
 
 namespace ipc {
@@ -22,10 +20,9 @@ namespace ipc {
 		IpcDataWriter<manager::StepperMotorResponse> *clone() const override;
 	private:
 		JsonIpcDataWriter<manager::StepperMotorResponse> m_json_data_writer;
-		static Json::Value response_to_json(const manager::StepperMotorResponse& response);
 	};
 	
-	inline StepperIpcDataWriter::StepperIpcDataWriter(const ClonableIpcDataWriter<RawData>& raw_data_writer): m_json_data_writer(raw_data_writer, response_to_json) {}
+	inline StepperIpcDataWriter::StepperIpcDataWriter(const ClonableIpcDataWriter<RawData>& raw_data_writer): m_json_data_writer(raw_data_writer, stepper_response_to_json_value) {}
 
 	inline void StepperIpcDataWriter::write(const manager::StepperMotorResponse& ipc_data) const {
 		m_json_data_writer.write(ipc_data);
@@ -33,18 +30,6 @@ namespace ipc {
 	
 	inline IpcDataWriter<manager::StepperMotorResponse> *StepperIpcDataWriter::clone() const {
 		return new StepperIpcDataWriter(*this);
-	}
-
-	inline Json::Value StepperIpcDataWriter::response_to_json(const manager::StepperMotorResponse& response) {
-		Json::Value json_data;
-		json_data["result"] = static_cast<int>(response.code());
-		if (response.state()) {
-			json_data["state"] = static_cast<int>(response.state().value());
-		}
-		if (response.message()) {
-			json_data["message"] = response.message().value();
-		}
-		return json_data;
 	}
 }
 
