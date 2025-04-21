093b46ed67148232ab6df67b3392e59ebc358884
fedddot <idanrommm@gmail.com>
2025-04-21 18:45:59 +0000

    operator implemented

--- manager/stepper_motor_manager/data/include/stepper_motor_request.hpp
+++ manager/stepper_motor_manager/data/include/stepper_motor_request.hpp
@@ -11,6 +11,12 @@ namespace manager {
 		std::size_t steps_number;
 		std::size_t step_duration_ms;
 	};
+
+	inline bool operator==(const StepperMotorRequest& lhs, const StepperMotorRequest& rhs) {
+		return lhs.direction == rhs.direction &&
+		       lhs.steps_number == rhs.steps_number &&
+		       lhs.step_duration_ms == rhs.step_duration_ms;
+	}
 }
 
 #endif // STEPPER_MOTOR_REQUEST_HPP
