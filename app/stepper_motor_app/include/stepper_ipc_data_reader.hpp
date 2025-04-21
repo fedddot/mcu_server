cff477258ee7ec289226562027dbb9eba40e1e4e
fedddot <idanrommm@gmail.com>
2025-04-21 18:36:54 +0000

    reader implemented

--- app/stepper_motor_app/include/stepper_ipc_data_reader.hpp
+++ app/stepper_motor_app/include/stepper_ipc_data_reader.hpp
@@ -1,6 +1,7 @@
 #ifndef	STEPPER_IPC_DATA_READER_HPP
 #define	STEPPER_IPC_DATA_READER_HPP
 
+#include <map>
 #include <optional>
 #include <stdexcept>
 #include <vector>
@@ -10,6 +11,7 @@
 #include "clonable_ipc_data_reader.hpp"
 #include "ipc_data_reader.hpp"
 #include "json_ipc_data_reader.hpp"
+#include "stepper_motor.hpp"
 #include "stepper_motor_request.hpp"
 
 namespace ipc {
@@ -27,6 +29,7 @@ namespace ipc {
 	private:
 		JsonIpcDataReader<manager::StepperMotorRequest> m_json_data_reader;
 		static manager::StepperMotorRequest parse_json_data(const Json::Value& json_data);
+		static manager::StepperMotor::Direction parse_direction(const std::string& direction_str);
 	};
 
 	inline StepperIpcDataReader::StepperIpcDataReader(
@@ -44,7 +47,23 @@ namespace ipc {
 	}
 	
 	inline manager::StepperMotorRequest StepperIpcDataReader::parse_json_data(const Json::Value& json_data) {
-		throw std::runtime_error("not implemented");
+		return manager::StepperMotorRequest {
+			.direction = parse_direction(json_data["direction"].asString()),
+			.steps_number = json_data["steps_number"].asUInt64(),
+			.step_duration_ms = json_data["step_duration_ms"].asUInt64()
+		};
+	}
+
+	inline manager::StepperMotor::Direction StepperIpcDataReader::parse_direction(const std::string& direction_str) {
+		const std::map<std::string, manager::StepperMotor::Direction> direction_map = {
+			{"CW", manager::StepperMotor::Direction::CW},
+			{"CCW", manager::StepperMotor::Direction::CCW}
+		};
+		auto it = direction_map.find(direction_str);
+		if (direction_map.end() == it) {
+			throw std::invalid_argument("invalid stepper motor direction: " + direction_str);
+		}
+		return it->second;
 	}
 }
 
