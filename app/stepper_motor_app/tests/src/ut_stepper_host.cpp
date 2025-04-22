5b3df6d59e17ec7faef4ebbb46dcb5bbd174f6fa
fedddot <idanrommm@gmail.com>
2025-04-22 13:52:15 +0000

    TC added

--- app/stepper_motor_app/tests/src/ut_stepper_host.cpp
+++ app/stepper_motor_app/tests/src/ut_stepper_host.cpp
@@ -1,3 +1,4 @@
+#include "json/value.h"
 #include <cstddef>
 #include <stdexcept>
 
@@ -6,6 +7,8 @@
 #include "stepper_host.hpp"
 #include "stepper_ipc_data_infra.hpp"
 #include "stepper_motor_data.hpp"
+#include "stepper_motor_request.hpp"
+#include "stepper_motor_response.hpp"
 #include "test_ipc_data_reader.hpp"
 #include "test_ipc_data_writer.hpp"
 #include "test_stepper_motor.hpp"
@@ -44,4 +47,50 @@ TEST(ut_stepper_host, ctor_dtor_sanity) {
 	ASSERT_NO_THROW(instance = new StepperHost(raw_data_reader, raw_data_writer, stepper_motor_creator, delay_generator));
 	ASSERT_NO_THROW(delete instance);
 	instance = nullptr;
+}
+
+TEST(ut_stepper_host, run_sanity) {
+	// GIVEN
+	const auto test_request = StepperMotorRequest {
+		Direction::CCW,
+		100,
+		10
+	};
+	const auto expected_response = StepperMotorResponse {
+		StepperMotorResponse::ResultCode::OK,
+		std::nullopt,
+		std::nullopt
+	};
+	const auto raw_data_writer = TestIpcDataWriter<RawData>(
+		[expected_response](const RawData& raw_data) {
+			auto json_response = Json::Value();
+			auto reader = Json::Reader();
+			ASSERT_TRUE(reader.parse(std::string(raw_data.begin(), raw_data.end()), std::ref(json_response)));
+			const auto response = json_value_to_stepper_response(json_response);
+			ASSERT_EQ(expected_response, response);
+		}
+	);
+	const auto raw_data_reader = TestIpcDataReader<RawData>(
+		[test_request]() -> std::optional<RawData> {
+			const auto json_request = stepper_request_to_json_value(test_request);
+			const auto serial_request = Json::FastWriter().write(json_request);
+			return RawData(serial_request.begin(), serial_request.end());
+		}
+	);
+	auto stepper_motor_creator = []() -> StepperMotor * {
+		return new manager_tests::TestStepperMotor(
+			[](const Direction&) {
+				throw std::runtime_error("NOT IMPLEMENTED");
+			}
+		);
+	};
+	auto delay_generator = [](const std::size_t&) {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	};
+
+	// WHEN
+	StepperHost instance(raw_data_reader, raw_data_writer, stepper_motor_creator, delay_generator);
+
+	// THEN
+	ASSERT_NO_THROW(instance.run_once());
 }
