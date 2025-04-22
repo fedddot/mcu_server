57779088ab2a2eff72f0ef14dbea4f468fbbe2e3
fedddot <idanrommm@gmail.com>
2025-04-22 13:42:58 +0000

    test fixed

--- app/stepper_motor_app/tests/src/ut_stepper_host.cpp
+++ app/stepper_motor_app/tests/src/ut_stepper_host.cpp
@@ -1,16 +1,14 @@
+#include <cstddef>
 #include <stdexcept>
-#include <string>
-
-#include "json/reader.h"
-#include "json/value.h"
 
 #include "gtest/gtest.h"
 
 #include "stepper_host.hpp"
 #include "stepper_ipc_data_infra.hpp"
-#include "stepper_motor_response.hpp"
+#include "stepper_motor_data.hpp"
 #include "test_ipc_data_reader.hpp"
 #include "test_ipc_data_writer.hpp"
+#include "test_stepper_motor.hpp"
 
 using namespace ipc;
 using namespace manager;
@@ -28,12 +26,22 @@ TEST(ut_stepper_host, ctor_dtor_sanity) {
 			throw std::runtime_error("NOT IMPLEMENTED");
 		}
 	);
+	auto stepper_motor_creator = []() -> StepperMotor * {
+		return new manager_tests::TestStepperMotor(
+			[](const Direction&) {
+				throw std::runtime_error("NOT IMPLEMENTED");
+			}
+		);
+	};
+	auto delay_generator = [](const std::size_t&) {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	};
 
 	// WHEN
 	StepperHost *instance = nullptr;
 
 	// THEN
-	ASSERT_NO_THROW(instance = new StepperHost(raw_data_reader, raw_data_writer));
+	ASSERT_NO_THROW(instance = new StepperHost(raw_data_reader, raw_data_writer, stepper_motor_creator, delay_generator));
 	ASSERT_NO_THROW(delete instance);
 	instance = nullptr;
 }
