f08cdbdf8bb168632ab5741cebfd897f0fca3d30
fedddot <idanrommm@gmail.com>
2025-04-25 13:41:09 +0000

    updated

--- manager/movement_manager/include/linear_movement.hpp
+++ manager/movement_manager/include/linear_movement.hpp
@@ -2,9 +2,7 @@
 #define	LINEAR_MOVEMENT_HPP
 
 #include <cmath>
-#include <iostream>
 #include <limits>
-#include <optional>
 #include <stdexcept>
 #include <vector>
 
@@ -16,7 +14,7 @@ namespace manager {
 	public:
 		LinearMovement(
 			const Vector<double>& destination,
-			const Vector<double>& basis
+			const AxesProperties& axes_properties
 		);
 		LinearMovement(const LinearMovement& other) = default;
 		LinearMovement& operator=(const LinearMovement&) = default;
@@ -24,7 +22,7 @@ namespace manager {
 		std::vector<AxisStep> evaluate_steps() const;
 	private:
 		Vector<double> m_destination;
-		Vector<double> m_basis;
+		AxesProperties m_axes_properties;
 		double m_movement_length;
 
 		bool is_enough(const Vector<double>& current) const;
@@ -32,14 +30,14 @@ namespace manager {
 		Vector<double> add_step(const Vector<double>& current, const AxisStep& step) const;
 		double evaluate_error_metric(const Vector<double>& current) const;
 		
-		static double scalar_product(const Vector<double>& one, const Vector<double>& other);
+		static double inner_product(const Vector<double>& one, const Vector<double>& other);
 	};
 
 	inline LinearMovement::LinearMovement(
 		const Vector<double>& destination,
-		const Vector<double>& basis
-	): m_destination(destination), m_basis(basis) {
-		m_movement_length = std::sqrt(scalar_product(destination, destination));
+		const AxesProperties& axes_properties
+	): m_destination(destination), m_axes_properties(axes_properties) {
+		m_movement_length = std::sqrt(inner_product(destination, destination));
 		if (m_movement_length == 0.0) {
 			throw std::runtime_error("destination vector cannot be zero");
 		}
@@ -49,20 +47,19 @@ namespace manager {
 		auto steps = std::vector<AxisStep>();
 		auto current_position = Vector<double>(0.0, 0.0, 0.0);
 		while (true) {
-			std::cout << "Current position: " << current_position.get(Axis::X) << ", " << current_position.get(Axis::Y) << ", " << current_position.get(Axis::Z) << std::endl;
 			if (is_enough(current_position)) {
 				break;
 			}
 			const auto axis_step = evaluate_axis_step(current_position);
-			if (!axis_step) {
-				break;
-			}
 			steps.push_back(*axis_step);
-			current_position = add_step(current_position, *axis_step);
+			current_position = add_step(current_position, axis_step);
 		}
 		return steps;
 	}
 
+	inline bool LinearMovement::is_enough(const Vector<double>& current) const {
+		throw std::runtime_error("NOT IMPLEMENTED");
+	}
 	inline AxisStep LinearMovement::evaluate_axis_step(const Vector<double>& current) const {
 		auto best_step = AxisStep {};
 		auto best_error_metric = std::numeric_limits<double>::max();
@@ -79,30 +76,27 @@ namespace manager {
 	}
 
 	inline Vector<double> LinearMovement::add_step(const Vector<double>& current, const AxisStep& step) const {
-		auto evaluate_increment = [this](const AxisStep& step) {
-			auto increment = m_basis.get(step.axis);
-			if (step.direction == Direction::NEGATIVE) {
-				increment = -increment;
-			}
-			return increment;
-		};
+		auto increment = m_axes_properties.get_step_length(step.axis);
+		if (step.direction == Direction::NEGATIVE) {
+			increment = -increment;
+		}
 		auto result = current;
-		result.set(step.axis, result.get(step.axis) + evaluate_increment(step));
+		result.set(step.axis, result.get(step.axis) + increment);
 		return result;
 	}
 
 	inline double LinearMovement::evaluate_error_metric(const Vector<double>& current) const {
-		const auto current_len = std::sqrt(scalar_product(current, current));
+		const auto current_len = std::sqrt(inner_product(current, current));
 		if (current_len == 0.0) {
 			throw std::runtime_error("current vector cannot be zero for error metric evaluation");
 		}
-		const auto prod = scalar_product(m_destination, current);
-		const auto cos_angle = prod / m_movement_length / current_len;
-		const auto sin_angle = std::sqrt(1 - cos_angle * cos_angle);
-		return sin_angle;
+		const auto prod_curr_dest = inner_product(m_destination, current);
+		const auto cos_curr_dest = prod_curr_dest / m_movement_length / current_len;
+		const auto sin_curr_dest = std::sqrt(1 - cos_curr_dest * cos_curr_dest);
+		return sin_curr_dest;
 	}
 
-	inline double LinearMovement::scalar_product(const Vector<double>& one, const Vector<double>& other) {
+	inline double LinearMovement::inner_product(const Vector<double>& one, const Vector<double>& other) {
 		auto result = 0.0;
 		for (const auto& axis : {Axis::X, Axis::Y, Axis::Z}) {
 			result += one.get(axis) * other.get(axis);
