9db8d389437fd8cb7a54c133761a91c894746725
fedddot <idanrommm@gmail.com>
2025-04-25 22:59:21 +0000

    test updated

--- app/movement_app/tests/src/ut_movement_host.cpp
+++ app/movement_app/tests/src/ut_movement_host.cpp
@@ -1,26 +1,23 @@
-#include <cstddef>
 #include <iostream>
 #include <stdexcept>
 
 #include "gtest/gtest.h"
 #include "json/value.h"
 
-#include "stepper_host.hpp"
-#include "stepper_ipc_data_infra.hpp"
-#include "stepper_motor_data.hpp"
-#include "stepper_motor_manager.hpp"
-#include "stepper_motor_request.hpp"
-#include "stepper_motor_response.hpp"
+#include "movement_host.hpp"
+#include "movement_ipc_data_infra.hpp"
+#include "movement_manager_data.hpp"
+#include "movement_manager.hpp"
+#include "movement_manager_request.hpp"
+#include "movement_manager_response.hpp"
 #include "test_ipc_data_reader.hpp"
 #include "test_ipc_data_writer.hpp"
-#include "test_stepper_motor.hpp"
 
 using namespace ipc;
 using namespace manager;
-using namespace manager_tests;
 using namespace host;
 
-TEST(ut_stepper_host, ctor_dtor_sanity) {
+TEST(ut_movement_host, ctor_dtor_sanity) {
 	// GIVEN
 	const auto raw_data_writer = TestIpcDataWriter<RawData>(
 		[](const RawData& raw_data) {
@@ -32,42 +29,30 @@ TEST(ut_stepper_host, ctor_dtor_sanity) {
 			throw std::runtime_error("NOT IMPLEMENTED");
 		}
 	);
-	auto stepper_motor_creator = []() -> StepperMotorManager::Steppers {
-		return StepperMotorManager::Steppers {
-			{
-				"motor_1",
-				std::make_shared<TestStepperMotor>(
-					[](const Direction&) {
-						throw std::runtime_error("NOT IMPLEMENTED");
-					}
-				)
-			},
-		};
-	};
-	auto delay_generator = [](const std::size_t&) {
+	auto axes_controller = [](const AxisStep& step) {
 		throw std::runtime_error("NOT IMPLEMENTED");
 	};
+	const auto axes_properties = AxesProperties(0.1, 0.1, 0.1);
 
 	// WHEN
-	StepperHost *instance = nullptr;
+	MovementHost *instance = nullptr;
 
 	// THEN
-	ASSERT_NO_THROW(instance = new StepperHost(raw_data_reader, raw_data_writer, stepper_motor_creator, delay_generator));
+	ASSERT_NO_THROW(instance = new MovementHost(raw_data_reader, raw_data_writer, axes_controller, axes_properties));
 	ASSERT_NO_THROW(delete instance);
 	instance = nullptr;
 }
 
-TEST(ut_stepper_host, run_sanity) {
+TEST(ut_movement_host, run_sanity) {
 	// GIVEN
-	const auto test_request = StepperMotorRequest {
-		"test_motor_id",
-		Direction::CCW,
-		100,
-		10
+	const auto axes_properties = AxesProperties(0.1, 0.1, 0.1);
+	const auto request_data = MovementManagerRequest::LinearMovementData {
+		.destination = Vector<double>(1.0, 2.0, 3.0),
+		.speed = 4.0
 	};
-	const auto expected_response = StepperMotorResponse {
-		StepperMotorResponse::ResultCode::OK,
-		std::nullopt,
+	auto request = MovementManagerRequest(request_data);
+	const auto expected_response = MovementManagerResponse {
+		MovementManagerResponse::ResultCode::OK,
 		std::nullopt
 	};
 	const auto raw_data_writer = TestIpcDataWriter<RawData>(
@@ -76,36 +61,24 @@ TEST(ut_stepper_host, run_sanity) {
 			auto json_response = Json::Value();
 			auto reader = Json::Reader();
 			ASSERT_TRUE(reader.parse(std::string(raw_data.begin(), raw_data.end()), std::ref(json_response)));
-			const auto response = json_value_to_stepper_response(json_response);
-			ASSERT_EQ(expected_response, response);
+			const auto response = json_value_to_movement_response(json_response);
+			ASSERT_EQ(expected_response.code, response.code);
 		}
 	);
 	const auto raw_data_reader = TestIpcDataReader<RawData>(
-		[test_request]() -> std::optional<RawData> {
-			const auto json_request = stepper_request_to_json_value(test_request);
+		[request]() -> std::optional<RawData> {
+			const auto json_request = movement_request_to_json_value(request);
 			const auto serial_request = Json::FastWriter().write(json_request);
 			std::cout << "test raw data reader generated the following raw request data:" << std::endl << serial_request << std::endl;
 			return RawData(serial_request.begin(), serial_request.end());
 		}
 	);
-	auto stepper_motor_creator = [test_request]() -> StepperMotorManager::Steppers {
-		return StepperMotorManager::Steppers {
-			{
-				test_request.motor_id,
-				std::make_shared<TestStepperMotor>(
-					[test_request](const Direction& direction) {
-						ASSERT_EQ(test_request.direction, direction);
-					}
-				)
-			},
-		};
-	};
-	auto delay_generator = [test_request](const std::size_t& timeout_ms) {
-		ASSERT_EQ(test_request.step_duration_ms, timeout_ms);
+	auto axes_controller = [](const AxisStep& step) {
+		throw std::runtime_error("NOT IMPLEMENTED");
 	};
 
 	// WHEN
-	StepperHost instance(raw_data_reader, raw_data_writer, stepper_motor_creator, delay_generator);
+	MovementHost instance(raw_data_reader, raw_data_writer, axes_controller, axes_properties);
 
 	// THEN
 	ASSERT_NO_THROW(instance.run_once());
