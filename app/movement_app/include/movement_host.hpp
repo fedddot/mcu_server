19c91225bea9cca3409dc4e0615fd1ee991a3702
fedddot <idanrommm@gmail.com>
2025-04-25 22:36:29 +0000

    updated

--- app/movement_app/include/movement_host.hpp
+++ app/movement_app/include/movement_host.hpp
@@ -1,21 +1,21 @@
-#ifndef	STEPPER_HOST_HPP
-#define	STEPPER_HOST_HPP
+#ifndef	MOVEMENT_HOST_HPP
+#define	MOVEMENT_HOST_HPP
 
 #include "host.hpp"
-#include "stepper_ipc_data_infra.hpp"
-#include "stepper_ipc_data_reader.hpp"
-#include "stepper_ipc_data_writer.hpp"
-#include "stepper_motor_manager.hpp"
-#include "stepper_motor_request.hpp"
-#include "stepper_motor_response.hpp"
+#include "movement_ipc_data_infra.hpp"
+#include "movement_ipc_data_reader.hpp"
+#include "movement_ipc_data_writer.hpp"
+#include "movement_motor_manager.hpp"
+#include "movement_motor_request.hpp"
+#include "movement_motor_response.hpp"
 
 namespace host {
-	class StepperHost: public Host<manager::StepperMotorRequest, manager::StepperMotorResponse> {
+	class StepperHost: public Host<manager::MovementManagerRequest, manager::MovementManagerResponse> {
 	public:
 		StepperHost(
 			const ipc::ClonableIpcDataReader<ipc::RawData>& ipc_data_reader,
 			const ipc::ClonableIpcDataWriter<ipc::RawData>& ipc_data_writer,
-			const manager::StepperMotorManager::SteppersCreator& stepper_motor_creator,
+			const manager::StepperMotorManager::SteppersCreator& movement_motor_creator,
 			const manager::StepperMotorManager::DelayGenerator& delay_generator
 		);
 		StepperHost(const StepperHost&) = delete;
@@ -25,15 +25,15 @@ namespace host {
 	inline StepperHost::StepperHost(
 		const ipc::ClonableIpcDataReader<ipc::RawData>& ipc_data_reader,
 		const ipc::ClonableIpcDataWriter<ipc::RawData>& ipc_data_writer,
-		const manager::StepperMotorManager::SteppersCreator& stepper_motor_creator,
+		const manager::StepperMotorManager::SteppersCreator& movement_motor_creator,
 		const manager::StepperMotorManager::DelayGenerator& delay_generator
 	): Host(
 		ipc::StepperIpcDataReader(ipc_data_reader),
 		ipc::StepperIpcDataWriter(ipc_data_writer),
-		manager::StepperMotorManager(stepper_motor_creator, delay_generator),
+		manager::StepperMotorManager(movement_motor_creator, delay_generator),
 		[](const std::exception& e) {
-			return manager::StepperMotorResponse {
-				manager::StepperMotorResponse::ResultCode::EXCEPTION,
+			return manager::MovementManagerResponse {
+				manager::MovementManagerResponse::ResultCode::EXCEPTION,
 				std::nullopt,
 				std::string(e.what())
 			};
@@ -41,4 +41,4 @@ namespace host {
 	) {}
 }
 
-#endif // STEPPER_HOST_HPP
+#endif // MOVEMENT_HOST_HPP
--- app/movement_app/include/movement_ipc_data_reader.hpp
+++ app/movement_app/include/movement_ipc_data_reader.hpp
@@ -6,28 +6,28 @@
 #include "clonable_ipc_data_reader.hpp"
 #include "ipc_data_reader.hpp"
 #include "json_ipc_data_reader.hpp"
-#include "stepper_ipc_data_infra.hpp"
-#include "stepper_motor_request.hpp"
+#include "movement_ipc_data_infra.hpp"
+#include "movement_manager_request.hpp"
 
 namespace ipc {
-	class StepperIpcDataReader: public ClonableIpcDataReader<manager::StepperMotorRequest> {
+	class StepperIpcDataReader: public ClonableIpcDataReader<manager::MovementManagerRequest> {
 	public:
 		StepperIpcDataReader(const ClonableIpcDataReader<RawData>& raw_data_reader);
 		StepperIpcDataReader(const StepperIpcDataReader&) = default;
 		StepperIpcDataReader& operator=(const StepperIpcDataReader&) = default;
-		std::optional<manager::StepperMotorRequest> read() override;
-		IpcDataReader<manager::StepperMotorRequest> *clone() const override;
+		std::optional<manager::MovementManagerRequest> read() override;
+		IpcDataReader<manager::MovementManagerRequest> *clone() const override;
 	private:
-		JsonIpcDataReader<manager::StepperMotorRequest> m_json_data_reader;
+		JsonIpcDataReader<manager::MovementManagerRequest> m_json_data_reader;
 	};
 
-	inline StepperIpcDataReader::StepperIpcDataReader(const ClonableIpcDataReader<RawData>& raw_data_reader): m_json_data_reader(raw_data_reader, json_value_to_stepper_request) {}
+	inline StepperIpcDataReader::StepperIpcDataReader(const ClonableIpcDataReader<RawData>& raw_data_reader): m_json_data_reader(raw_data_reader, json_value_to_movement_request) {}
 
-	inline std::optional<manager::StepperMotorRequest> StepperIpcDataReader::read() {
+	inline std::optional<manager::MovementManagerRequest> StepperIpcDataReader::read() {
 		return m_json_data_reader.read();
 	}
 
-	inline IpcDataReader<manager::StepperMotorRequest> *StepperIpcDataReader::clone() const {
+	inline IpcDataReader<manager::MovementManagerRequest> *StepperIpcDataReader::clone() const {
 		return new StepperIpcDataReader(*this);
 	}
 }
