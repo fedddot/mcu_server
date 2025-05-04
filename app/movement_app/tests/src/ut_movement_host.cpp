f764c817e38857f60526c5ba9e655139b6a2587a
fedddot <idanrommm@gmail.com>
2025-05-04 20:32:30 +0000

    test updated

--- app/movement_app/tests/src/ut_movement_host.cpp
+++ app/movement_app/tests/src/ut_movement_host.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <map>
+#include <optional>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 
 #include "gtest/gtest.h"
 #include "json/value.h"
@@ -46,40 +50,75 @@ TEST(ut_movement_host, ctor_dtor_sanity) {
 TEST(ut_movement_host, run_sanity) {
 	// GIVEN
 	const auto axes_properties = AxesProperties(0.1, 0.1, 0.1);
-	const auto request_data = MovementManagerRequest::LinearMovementData {
-		.destination = Vector<double>(1.0, 2.0, 3.0),
-		.speed = 4.0
-	};
-	auto request = MovementManagerRequest(request_data);
-	const auto expected_response = MovementManagerResponse {
-		MovementManagerResponse::ResultCode::OK,
-		std::nullopt
+	const auto test_requests = {
+		MovementManagerRequest(
+			MovementManagerRequest::LinearMovementData {
+				.destination = Vector<double>(1.0, 2.0, 3.0),
+				.speed = 4.0
+			}
+		),
+		MovementManagerRequest(
+			MovementManagerRequest::LinearMovementData {
+				.destination = Vector<double>(-1.0, 2.0, 3.0),
+				.speed = 4.0
+			}
+		),
+		MovementManagerRequest(
+			MovementManagerRequest::LinearMovementData {
+				.destination = Vector<double>(0.0, 0.0, -3.0),
+				.speed = 4.0
+			}
+		),
 	};
 	const auto raw_data_writer = TestIpcDataWriter<RawData>(
-		[expected_response](const RawData& raw_data) {
+		[](const RawData& raw_data) {
 			std::cout << "test raw data writer received the following data to write:" << std::endl << std::string(raw_data.begin(), raw_data.end()) << std::endl;
 			auto json_response = Json::Value();
 			auto reader = Json::Reader();
 			ASSERT_TRUE(reader.parse(std::string(raw_data.begin(), raw_data.end()), std::ref(json_response)));
 			const auto response = json_value_to_movement_response(json_response);
-			ASSERT_EQ(expected_response.code, response.code);
+			ASSERT_EQ(MovementManagerResponse::ResultCode::OK, response.code);
 		}
 	);
+	auto test_requests_iter = test_requests.begin();
 	const auto raw_data_reader = TestIpcDataReader<RawData>(
-		[request]() -> std::optional<RawData> {
-			const auto json_request = movement_request_to_json_value(request);
+		[&test_requests, &test_requests_iter]() -> std::optional<RawData> {
+			if (test_requests.end() == test_requests_iter) {
+				return std::nullopt;
+			}
+			const auto json_request = movement_request_to_json_value(*test_requests_iter);
 			const auto serial_request = Json::FastWriter().write(json_request);
 			std::cout << "test raw data reader generated the following raw request data:" << std::endl << serial_request << std::endl;
+			++test_requests_iter;
 			return RawData(serial_request.begin(), serial_request.end());
 		}
 	);
-	auto axes_controller = [](const AxisStep& step) {
-		throw std::runtime_error("NOT IMPLEMENTED");
+	auto step_to_str = [](const AxisStep& step) -> std::string {
+		const auto axis_mapping = std::map<Axis, std::string> {
+			{Axis::X, "X"},
+			{Axis::Y, "Y"},
+			{Axis::Z, "Z"},
+		};
+		const auto direction_mapping = std::map<Direction, std::string> {
+			{Direction::NEGATIVE, "NEGATIVE"},
+			{Direction::POSITIVE, "POSITIVE"},
+		};
+		std::stringstream stream;
+		stream
+			<< "step axis " << axis_mapping.at(step.axis) << "; "
+			<< "step direction " << direction_mapping.at(step.direction) << "; "
+			<< "step duration " << step.duration << "; ";
+		return stream.str();
+	};
+	auto axes_controller = [step_to_str](const AxisStep& step) {
+		std::cout << "axes_controller makes step: " << step_to_str(step) << std::endl;
 	};
 
 	// WHEN
 	MovementHost instance(raw_data_reader, raw_data_writer, axes_controller, axes_properties);
 
 	// THEN
-	ASSERT_NO_THROW(instance.run_once());
+	while (test_requests_iter != test_requests.end()) {
+		ASSERT_NO_THROW(instance.run_once());
+	}
 }
