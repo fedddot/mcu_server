9024d1ee60ccf488778851f41f4137fdd550ab0e
fedddot <idanrommm@gmail.com>
2025-05-10 16:36:30 +0000

    writer updated

--- ipc/json/include/json_ipc_data_writer.hpp
+++ ipc/json/include/json_ipc_data_writer.hpp
@@ -7,19 +7,18 @@
 #include "json/value.h"
 #include "json/writer.h"
 
-#include "ipc_clonable.hpp"
 #include "ipc_data_writer.hpp"
 #include "ipc_data.hpp"
+#include "ipc_instance.hpp"
 
 namespace ipc {
 	template <typename IpcData>
-	class JsonIpcDataWriter: public IpcDataWriter<IpcData>, public Clonable<IpcDataWriter<IpcData>> {
+	class JsonIpcDataWriter: public IpcDataWriter<IpcData> {
 	public:
 		using IpcDataToJsonDataTransformer = std::function<Json::Value(const IpcData&)>;
-		using ClonableRawDataWriter = Clonable<IpcDataWriter<RawData>>;
-
+		
 		JsonIpcDataWriter(
-			const ClonableRawDataWriter& raw_data_writer,
+			const Instance<IpcDataWriter<RawData>>& raw_data_writer,
 			const IpcDataToJsonDataTransformer& ipc_data_transformer
 		);
 		JsonIpcDataWriter(const JsonIpcDataWriter&) = default;
@@ -27,15 +26,15 @@ namespace ipc {
 		void write(const IpcData& ipc_data) const override;
 		IpcDataWriter<IpcData> *clone() const override;
 	private:
-		std::shared_ptr<IpcDataWriter<RawData>> m_raw_data_writer;
+		Instance<IpcDataWriter<RawData>> m_raw_data_writer;
 		IpcDataToJsonDataTransformer m_ipc_data_transformer;
 	};
 
 	template <typename IpcData>
 	inline JsonIpcDataWriter<IpcData>::JsonIpcDataWriter(
-		const ClonableRawDataWriter& raw_data_writer,
+		const Instance<IpcDataWriter<RawData>>& raw_data_writer,
 		const IpcDataToJsonDataTransformer& ipc_data_transformer
-	): m_raw_data_writer(raw_data_writer.clone()), m_ipc_data_transformer(ipc_data_transformer) {
+	): m_raw_data_writer(raw_data_writer), m_ipc_data_transformer(ipc_data_transformer) {
 		if (!m_ipc_data_transformer) {
 			throw std::invalid_argument("invalid ipc data transformer received");
 		}
@@ -47,7 +46,7 @@ namespace ipc {
 		const auto writer_builder = Json::StreamWriterBuilder();
 		const auto serial_str = Json::writeString(writer_builder, json_val);
 		const auto raw_serial_data = RawData(serial_str.begin(), serial_str.end());
-		m_raw_data_writer->write(raw_serial_data);
+		m_raw_data_writer.get().write(raw_serial_data);
 	}
 
 	template <typename IpcData>
