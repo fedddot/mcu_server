0c78393a2b336be141a7fed41176eaef0dbc3b05
fedddot <idanrommm@gmail.com>
2025-05-10 18:23:14 +0000

    updated

--- app/movement_app/include/movement_host.hpp
+++ app/movement_app/include/movement_host.hpp
@@ -1,11 +1,11 @@
 #ifndef	MOVEMENT_HOST_HPP
 #define	MOVEMENT_HOST_HPP
 
+#include "axes_controller.hpp"
 #include "host.hpp"
 #include "ipc_instance.hpp"
 #include "ipc_data_reader.hpp"
 #include "ipc_data_writer.hpp"
-#include "response_json_transformer.hpp"
 #include "movement_ipc_data_reader.hpp"
 #include "movement_ipc_data_writer.hpp"
 #include "movement_manager.hpp"
@@ -14,27 +14,33 @@
 #include "movement_manager_response.hpp"
 
 namespace host {
+	template <typename AxisControllerConfig>
 	class MovementHost: public Host<manager::MovementManagerRequest, manager::MovementManagerResponse> {
 	public:
 		MovementHost(
 			const ipc::Instance<ipc::IpcDataReader<ipc::RawData>>& ipc_data_reader,
 			const ipc::Instance<ipc::IpcDataWriter<ipc::RawData>>& ipc_data_writer,
-			const manager::MovementManager::AxesController& axes_controller,
+			const manager::AxesController& axes_controller,
 			const manager::AxesProperties& axes_properties
 		);
 		MovementHost(const MovementHost&) = delete;
 		MovementHost& operator=(const MovementHost&) = delete;
 	};
 
-	inline MovementHost::MovementHost(
+	template <typename AxisControllerConfig>
+	inline MovementHost<AxisControllerConfig>::MovementHost(
 		const ipc::Instance<ipc::IpcDataReader<ipc::RawData>>& ipc_data_reader,
 		const ipc::Instance<ipc::IpcDataWriter<ipc::RawData>>& ipc_data_writer,
-		const manager::MovementManager::AxesController& axes_controller,
+		const manager::AxesController& axes_controller,
 		const manager::AxesProperties& axes_properties
 	): Host(
-		ipc::MovementIpcDataReader(ipc_data_reader),
-		ipc::MovementIpcDataWriter(ipc_data_writer),
-		manager::MovementManager(axes_controller, axes_properties),
+		ipc::Instance<ipc::IpcDataReader<manager::MovementManagerRequest>>(
+			new ipc::MovementIpcDataReader<AxisControllerConfig>(ipc_data_reader)
+		),
+		ipc::Instance<ipc::IpcDataWriter<manager::MovementManagerResponse>>(
+			new ipc::MovementIpcDataWriter(ipc_data_writer)
+		),
+		manager::MovementManager<AxisControllerConfig>(axes_controller, axes_properties),
 		[](const std::exception& e) {
 			return manager::MovementManagerResponse {
 				manager::MovementManagerResponse::ResultCode::EXCEPTION,
