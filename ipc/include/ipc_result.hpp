dcf3038290999b9a136a4688d80ccc2ca7c4ee74
fedddot <idanrommm@gmail.com>
2025-05-10 07:12:13 +0000

    ipc result added

---
+++ ipc/include/ipc_result.hpp
@@ -0,0 +1,38 @@
+#ifndef	IPC_RESULT_HPP
+#define	IPC_RESULT_HPP
+
+#include <memory>
+#include <stdexcept>
+
+namespace ipc {
+	template <typename T>
+	class Result {
+	public:
+		Result(T *instance_ptr);
+		Result(const Result&) = default;
+		virtual ~Result() noexcept = default;
+		const T& get() const = 0;
+		T& get() = 0;
+	private:
+		std::shared_ptr<T> m_instance_ptr;
+	};
+
+	template <typename T>
+	inline Result<T>::Result(T *instance_ptr): m_instance_ptr(instance_ptr) {
+		if (!m_instance_ptr) {
+			throw std::invalid_argument("invalid instance ptr received");
+		}
+	}
+
+	template <typename T>
+	inline const T& Result<T>::get() const {
+		return std::ref(*m_instance_ptr);
+	}
+
+	template <typename T>
+	inline T& Result<T>::get() {
+		return std::ref(*m_instance_ptr);
+	}
+}
+
+#endif // IPC_RESULT_HPP
