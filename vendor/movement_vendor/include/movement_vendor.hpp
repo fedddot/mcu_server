dff1357236a9bbef10754c3d3a65dd1b843c4489
fedddot <idanrommm@gmail.com>
2025-05-14 15:32:06 +0000

    ctor implemented

--- vendor/movement_vendor/include/movement_vendor.hpp
+++ vendor/movement_vendor/include/movement_vendor.hpp
@@ -3,24 +3,30 @@
 
 #include <stdexcept>
 
+#include "manager_instance.hpp"
+#include "movement_manager.hpp"
 #include "vendor.hpp"
 
 namespace vendor {
 	template <typename AxisControllerConfig>
 	class MovementVendor: public Vendor {
 	public:
-		MovementVendor();
+		using MovementManagerInstance = manager::Instance<manager::MovementManager<AxisControllerConfig>>;
+		MovementVendor(
+			const MovementManagerInstance& movement_manager
+		);
 		MovementVendor(const MovementVendor& other) = default;
 		MovementVendor& operator=(const MovementVendor&) = delete;
 
 		Instance<ApiResponse> run_api_request(const ApiRequest& request) override;
 	private:
+		MovementManagerInstance m_movement_manager;
 	};
 
 	template <typename AxisControllerConfig>
-	inline MovementVendor<AxisControllerConfig>::MovementVendor() {
-		throw std::invalid_argument("NOT IMPLEMENTED");
-	}
+	inline MovementVendor<AxisControllerConfig>::MovementVendor(
+		const MovementManagerInstance& movement_manager
+	): m_movement_manager(movement_manager) {}
 	
 	template <typename AxisControllerConfig>
 	inline Instance<ApiResponse> MovementVendor<AxisControllerConfig>::run_api_request(const ApiRequest& request) {
