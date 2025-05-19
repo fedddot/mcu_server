c9016ff0be54f5e7d94673fad9b5c486d92a9fe3
fedddot <idanrommm@gmail.com>
2025-05-19 16:35:01 +0000

    vendor iface updated

--- vendor/include/vendor.hpp
+++ vendor/include/vendor.hpp
@@ -1,16 +1,12 @@
 #ifndef	VENDOR_HPP
 #define	VENDOR_HPP
 
-#include "vendor_api_request.hpp"
-#include "vendor_api_response.hpp"
-#include "vendor_instance.hpp"
-
 namespace vendor {
-	template <typename ManagerId, typename Payload>
+	template <typename ApiRequest, typename ApiResponse>
 	class Vendor {
 	public:
 		virtual ~Vendor() noexcept = default;
-		virtual Instance<ApiResponse> run_api_request(const ApiRequest<ManagerId, Payload>& request) = 0;
+		virtual ApiResponse run_api_request(const ApiRequest& request) = 0;
 	};
 }
 
