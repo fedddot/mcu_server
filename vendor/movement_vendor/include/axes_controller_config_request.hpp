1182f817df17bb2954a59efbb48992b891d72e45
fedddot <idanrommm@gmail.com>
2025-05-19 16:55:46 +0000

    movement vendor updated

--- vendor/movement_vendor/include/axes_controller_config_request.hpp
+++ vendor/movement_vendor/include/axes_controller_config_request.hpp
@@ -17,7 +17,7 @@ namespace vendor {
 			return RequestType::CONFIG;
 		}
 		AxesConfig axes_cfg() const {
-
+			return m_axes_cfg;
 		}
 	private:
 		AxesConfig m_axes_cfg;
--- vendor/movement_vendor/include/movement_vendor.hpp
+++ vendor/movement_vendor/include/movement_vendor.hpp
@@ -3,7 +3,8 @@
 
 #include <stdexcept>
 
-#include "manager.hpp"
+#include "axes_controller_config_request.hpp"
+#include "movement_manager.hpp"
 #include "movement_vendor_api_request.hpp"
 #include "movement_vendor_api_response.hpp"
 #include "vendor.hpp"
@@ -13,7 +14,7 @@ namespace vendor {
 	template <typename AxesConfig>
 	class MovementVendor: public Vendor<MovementVendorApiRequest, MovementVendorApiResponse> {
 	public:
-		using MovementManagerInstance = vendor::Instance<manager::Manager>;
+		using MovementManagerInstance = vendor::Instance<manager::MovementManager<AxesConfig>>;
 
 		MovementVendor(
 			const MovementManagerInstance& movement_manager
@@ -24,6 +25,12 @@ namespace vendor {
 		MovementVendorApiResponse run_api_request(const MovementVendorApiRequest& request) override;
 	private:
 		MovementManagerInstance m_movement_manager;
+
+		
+		MovementVendorApiResponse run_cfg_request(const MovementVendorApiRequest& request);
+
+		template <typename T>
+		static const T& cast_request(const MovementVendorApiRequest& request);
 	};
 
 	template <typename AxesConfig>
@@ -33,7 +40,23 @@ namespace vendor {
 	
 	template <typename AxesConfig>
 	inline MovementVendorApiResponse MovementVendor<AxesConfig>::run_api_request(const MovementVendorApiRequest& request) {
-		throw std::runtime_error("NOT IMPLEMENTED");
+		switch (request.type()) {
+		case MovementVendorApiRequest::RequestType::CONFIG:
+			return run_cfg_request(request);
+		default:
+			throw std::invalid_argument("unsupported api request received");
+		}
+	}
+
+	template <typename AxesConfig>
+	inline MovementVendorApiResponse MovementVendor<AxesConfig>::run_cfg_request(const MovementVendorApiRequest& request) {
+		try {
+			auto casted_request = cast_request<AxesControllerConfigApiRequest<AxesConfig>>(request);
+			m_movement_manager.get().init(casted_request.axes_cfg());
+			return MovementVendorApiResponse(MovementVendorApiResponse::Result::SUCCESS);
+		} catch (...) {
+			return MovementVendorApiResponse(MovementVendorApiResponse::Result::FAILURE);
+		}
 	}
 }
 
