ada06e45c938fe96247252e80a1865d9e2b1a5e3
fedddot <idanrommm@gmail.com>
2025-05-19 16:45:17 +0000

    movement manager updated

--- manager/movement_manager/include/movement_manager.hpp
+++ manager/movement_manager/include/movement_manager.hpp
@@ -7,14 +7,10 @@
 #include <stdexcept>
 
 #include "axes_controller.hpp"
-#include "init_request.hpp"
 #include "manager_instance.hpp"
 #include "linear_movement.hpp"
 #include "manager.hpp"
 #include "movement_manager_data.hpp"
-#include "movement_manager_request.hpp"
-#include "movement_manager_response.hpp"
-#include "linear_movement_request.hpp"
 
 namespace manager {
 	template <typename AxisControllerConfig>
@@ -28,17 +24,13 @@ namespace manager {
 		MovementManager(const MovementManager& other) = default;
 		MovementManager& operator=(const MovementManager&) = delete;
 
-		MovementManagerResponse run(const MovementManagerRequest& request);
+		void init(const AxisControllerConfig& axes_config);
+		void linear_movement(const Vector<double>& destination, const double speed);
+		void circular_movement(const Vector<double>& destination, const Vector<double>& rotation_center, const double speed);
 	private:
 		AxesControllerCreator m_axes_controller_ctor;
 		std::optional<Instance<AxesController>> m_axes_controller;
 		AxesProperties m_axes_properties;
-		MovementManagerResponse init(const MovementManagerRequest& request);
-		MovementManagerResponse linear_movement(const MovementManagerRequest& request);
-		MovementManagerResponse circular_movement(const MovementManagerRequest& request) const;
-
-		template <typename T> 
-		static const T& downcast_request(const MovementManagerRequest& request);
 	};
 
 	template <typename AxisControllerConfig>
@@ -50,45 +42,17 @@ namespace manager {
 			throw std::invalid_argument("invalid axes_controller_ctor received");
 		}
 	}
-	
+
 	template <typename AxisControllerConfig>
-	inline MovementManagerResponse MovementManager<AxisControllerConfig>::run(const MovementManagerRequest& request) {
-		switch (request.type()) {
-		case MovementManagerRequest::RequestType::INIT:
-			return init(request);
-		case MovementManagerRequest::RequestType::LINEAR_MOVEMENT:
-			return linear_movement(request);
-		case MovementManagerRequest::RequestType::ROTATIONAL_MOVEMENT:
-			return circular_movement(request);
-		default:
-			return MovementManagerResponse {
-				.code = MovementManagerResponse::ResultCode::BAD_REQUEST,
-				.message = "unsupported movement type received",
-			};
-		}
+	inline void MovementManager<AxisControllerConfig>::init(const AxisControllerConfig& axes_config) {
+		m_axes_controller = m_axes_controller_ctor(axes_config);
 	}
 
 	template <typename AxisControllerConfig>
-	inline MovementManagerResponse MovementManager<AxisControllerConfig>::init(const MovementManagerRequest& request) {
-		const auto& request_casted = downcast_request<InitRequest<AxisControllerConfig>>(request);
-		m_axes_controller = m_axes_controller_ctor(request_casted.axis_controller_config());
-		return MovementManagerResponse {
-			.code = MovementManagerResponse::ResultCode::OK,
-			.message = std::nullopt,
-		};
-	}
-
-	template <typename AxisControllerConfig>
-	inline MovementManagerResponse MovementManager<AxisControllerConfig>::linear_movement(const MovementManagerRequest& request) {
+	inline void MovementManager<AxisControllerConfig>::linear_movement(const Vector<double>& destination, const double speed) {
 		if (!m_axes_controller) {
-			return MovementManagerResponse {
-				.code = MovementManagerResponse::ResultCode::EXCEPTION,
-				.message = "axes controller is not initialized",
-			};
+			throw std::runtime_error("axes controller is not initialized");
 		}
-		const auto& request_casted = downcast_request<LinearMovementRequest>(request);
-		const auto speed = request_casted.speed();
-		const auto destination = request_casted.destination();
 		auto movement = LinearMovement(
 			destination,
 			m_axes_properties,
@@ -103,25 +67,11 @@ namespace manager {
 			m_axes_controller->get().step(*step);
 		}
 		m_axes_controller->get().disable();
-		return MovementManagerResponse {
-			.code = MovementManagerResponse::ResultCode::OK,
-			.message = std::nullopt,
-		};
 	}
 	
 	template <typename AxisControllerConfig>
-	inline MovementManagerResponse MovementManager<AxisControllerConfig>::circular_movement(const MovementManagerRequest& request) const {
+	inline void MovementManager<AxisControllerConfig>::circular_movement(const Vector<double>& destination, const Vector<double>& rotation_center, const double speed) {
 		throw std::runtime_error("not implemented");
-	}
-
-	template <typename AxisControllerConfig>
-	template <typename T>
-	inline const T& MovementManager<AxisControllerConfig>::downcast_request(const MovementManagerRequest& request) {
-		try {
-			return dynamic_cast<const T&>(request);
-		} catch (...) {
-			throw std::invalid_argument("failed to cast received request into target type");
-		}
 	}
 }
 
