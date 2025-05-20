5df7ef0623b58d4c712dbad8c25d97d3e0de9a78
fedddot <idanrommm@gmail.com>
2025-05-20 16:01:37 +0000

    test vendor updated

--- vendor/tests/include/test_vendor.hpp
+++ vendor/tests/include/test_vendor.hpp
@@ -2,36 +2,34 @@
 #define	TEST_VENDOR_HPP
 
 #include <functional>
+#include <stdexcept>
 
-#include "vendor_api_request.hpp"
-#include "vendor_api_response.hpp"
-#include "vendor_instance.hpp"
 #include "vendor.hpp"
 
 namespace vendor {
-	template <typename ManagerId, typename Payload>
-	class TestVendor: public Vendor<ManagerId, Payload> {
+	template <typename ApiRequest, typename ApiResponse>
+	class TestVendor: public Vendor<ApiRequest, ApiResponse> {
 	public:
-		using TestAction = std::function<Instance<ApiResponse>(const ApiRequest<ManagerId, Payload>&)>;
+		using TestAction = std::function<ApiResponse(const ApiRequest&)>;
 
 		TestVendor(const TestAction& test_action);
 		TestVendor(const TestVendor&) = default;
 		TestVendor& operator=(const TestVendor&) = default;
 
-		Instance<ApiResponse> run_api_request(const ApiRequest<ManagerId, Payload>& request) override;
+		ApiResponse run_api_request(const ApiRequest& request) override;
 	private:
 		TestAction m_test_action;
 	};
 
-	template <typename ManagerId, typename Payload>
-	inline TestVendor<ManagerId, Payload>::TestVendor(const TestAction& test_action): m_test_action(test_action) {
+	template <typename ApiRequest, typename ApiResponse>
+	inline TestVendor<ApiRequest, ApiResponse>::TestVendor(const TestAction& test_action): m_test_action(test_action) {
 		if (!m_test_action) {
 			throw std::invalid_argument("invalid test action received");
 		}
 	}
 
-	template <typename ManagerId, typename Payload>
-	inline Instance<ApiResponse> TestVendor<ManagerId, Payload>::run_api_request(const ApiRequest<ManagerId, Payload>& request) {
+	template <typename ApiRequest, typename ApiResponse>
+	inline ApiResponse TestVendor<ApiRequest, ApiResponse>::run_api_request(const ApiRequest& request) {
 		return m_test_action(request);
 	}
 }
