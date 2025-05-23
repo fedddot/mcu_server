089527c6ae157d1c943ea39285c193b04a592a34
fedddot <idanrommm@gmail.com>
2025-05-23 16:21:59 +0000

    movement_manager_data updated

--- manager/movement_manager/data/include/movement_manager_data.hpp
+++ manager/movement_manager/data/include/movement_manager_data.hpp
@@ -15,21 +15,38 @@ namespace manager {
 		POSITIVE = 1,
 	};
 
-	struct AxisStep {
+	class AxisStep {
+	public:
 		AxisStep(const Axis& axis = Axis::X, const Direction& direction = Direction::POSITIVE, const double duration = 1.0);
 		AxisStep(const AxisStep&) = default;
 		AxisStep& operator=(const AxisStep&) = default;
 		virtual ~AxisStep() noexcept = default;
 
-		Axis axis;
-		Direction direction;
-		double duration;
+		Axis axis() const;
+		Direction direction() const;
+		double duration() const;
+	private:
+		Axis m_axis;
+		Direction m_direction;
+		double m_duration;
 	};
 
-	inline AxisStep::AxisStep(const Axis& axis, const Direction& direction, const double duration): axis(axis), direction(direction), duration(duration) {
+	inline AxisStep::AxisStep(const Axis& axis, const Direction& direction, const double duration): m_axis(axis), m_direction(direction), m_duration(duration) {
 		if (duration <= 0.0) {
 			throw std::invalid_argument("duration must be a positive non-zero value");
 		}
+	}
+
+	inline Axis AxisStep::axis() const {
+		return m_axis;
+	}
+
+	inline Direction AxisStep::direction() const {
+		return m_direction;
+	}
+
+	inline double AxisStep::duration() const {
+		return m_duration;
 	}
 }
 
