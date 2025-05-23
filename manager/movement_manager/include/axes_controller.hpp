3f8f3be97e3c24036460f4befd464ad929ac390d
fedddot <idanrommm@gmail.com>
2025-05-23 15:45:49 +0000

    moved

--- manager/movement_manager/include/axes_controller.hpp
+++ manager/movement_manager/include/axes_controller.hpp
@@ -7,10 +7,12 @@ namespace manager {
 	class AxesController {
 	public:
 		virtual ~AxesController() noexcept = default;
-		virtual void step(const AxisStep& step) = 0;
+		virtual void step(const Axis& axis, const Direction& direction, const double duration) = 0;
 		virtual void enable() = 0;
 		virtual void disable() = 0;
+		virtual double get_step_length(const Axis& axis) const = 0;
 		virtual AxesController *clone() const = 0;
+		
 	};
 }
 
