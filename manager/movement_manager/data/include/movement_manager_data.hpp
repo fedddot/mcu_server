9dcd0d317d5d0a2b8ad5bfaf36fa738119c1b868
fedddot <idanrommm@gmail.com>
2025-05-23 16:19:22 +0000

    updated

--- manager/movement_manager/data/include/movement_manager_data.hpp
+++ manager/movement_manager/data/include/movement_manager_data.hpp
@@ -1,6 +1,8 @@
 #ifndef	MOVEMENT_MANAGER_DATA_HPP
 #define	MOVEMENT_MANAGER_DATA_HPP
 
+#include <stdexcept>
+
 namespace manager {
 	enum class Axis: int {
 		X = 0,
@@ -12,6 +14,23 @@ namespace manager {
 		NEGATIVE = 0,
 		POSITIVE = 1,
 	};
+
+	struct AxisStep {
+		AxisStep(const Axis& axis = Axis::X, const Direction& direction = Direction::POSITIVE, const double duration = 1.0);
+		AxisStep(const AxisStep&) = default;
+		AxisStep& operator=(const AxisStep&) = default;
+		virtual ~AxisStep() noexcept = default;
+
+		Axis axis;
+		Direction direction;
+		double duration;
+	};
+
+	inline AxisStep::AxisStep(const Axis& axis, const Direction& direction, const double duration): axis(axis), direction(direction), duration(duration) {
+		if (duration <= 0.0) {
+			throw std::invalid_argument("duration must be a positive non-zero value");
+		}
+	}
 }
 
 #endif // MOVEMENT_MANAGER_DATA_HPP
