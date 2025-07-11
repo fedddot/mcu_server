1ec6a4d33503a268705cc0e9d173b4113f8164ae
fedddot <idanrommm@gmail.com>
2025-07-11 18:40:54 +0000

    updated

--- manager/thermostat_manager/include/thermostat_manager.hpp
+++ manager/thermostat_manager/include/thermostat_manager.hpp
@@ -2,16 +2,42 @@
 #define	THERMOSTAT_MANAGER_HPP
 
 #include "manager.hpp"
+#include "manager_instance.hpp"
+#include <cstddef>
 
 namespace manager {
+	class RelayController {
+	public:
+		virtual ~RelayController() noexcept = default;
+		virtual void set_relay_state(const bool state) = 0;
+	};
+	class TemperatureSensorController {
+	public:
+		virtual ~TemperatureSensorController() noexcept = default;
+		virtual double read_temperature() const = 0;
+	};
 	class ThermostatManager: public Manager {
 	public:
 		ThermostatManager(
-		);
+			const Instance<RelayController>& relay_controller,
+			const Instance<TemperatureSensorController>& temp_sensor_controller
+		): m_relay_controller(relay_controller), m_temp_sensor_controller(temp_sensor_controller), m_is_running(false), m_time_resolution_ms(10) {
+			m_relay_controller.get().set_relay_state(false);
+		}
 		ThermostatManager(const ThermostatManager& other) = default;
 		ThermostatManager& operator=(const ThermostatManager&) = delete;
-
+		
+		void start(const double temp) {
+			if (m_is_running) {
+				throw std::runtime_error("ThermostatManager is already running");
+		}
+		void stop();
+		double get_current_temperature() const;
 	private:
+		Instance<RelayController> m_relay_controller;
+		Instance<TemperatureSensorController> m_temp_sensor_controller;
+		bool m_is_running;
+		std::size_t m_time_resolution_ms;
 	};
 }
 
