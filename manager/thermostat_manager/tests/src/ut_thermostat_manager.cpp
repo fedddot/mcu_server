21b386c917f350c2b620ddbf06f1682cffb48538
fedddot <idanrommm@gmail.com>
2025-07-11 19:22:45 +0000

    test updated

--- manager/thermostat_manager/tests/src/ut_thermostat_manager.cpp
+++ manager/thermostat_manager/tests/src/ut_thermostat_manager.cpp
@@ -1,17 +1,44 @@
-#include <stdexcept>
-#include <thread>
-
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "manager_instance.hpp"
+#include "relay_controller.hpp"
+#include "temperature_sensor_controller.hpp"
 #include "thermostat_manager.hpp"
+#include "timer_scheduler.hpp"
 
 using namespace manager;
+using ::testing::NiceMock;
+
+class MockRelayController : public RelayController {
+public:
+    MOCK_METHOD(void, set_relay_state, (const bool state), (override));
+};
+
+class MockTemperatureSensorController : public TemperatureSensorController {
+public:
+    MOCK_METHOD(double, read_temperature, (), (const, override));
+};
+
+class MockTaskGuard : public TimerScheduler::TaskGuard {
+public:
+    MOCK_METHOD(void, unschedule, (), (override));
+};
+
+class MockTimerScheduler : public TimerScheduler {
+public:
+    MOCK_METHOD(Instance<TaskGuard>, schedule_task, (const Task& task, const std::size_t period_ms), (override));
+};
 
 TEST(ut_thermostat_manager, ctor_dtor_sanity) {
-	// WHEN
-	ThermostatManager *instance(nullptr);
+    // GIVEN
+    auto relay = Instance<RelayController>(new NiceMock<MockRelayController>());
+    auto temp = Instance<TemperatureSensorController>(new NiceMock<MockTemperatureSensorController>());
+    auto timer = Instance<TimerScheduler>(new NiceMock<MockTimerScheduler>());
 
-	// THEN
-	// ASSERT_NO_THROW(instance = new ThermostatManager(create_axes_ctrlr));
-	ASSERT_NE(instance, nullptr);
-	ASSERT_NO_THROW(delete instance);
+    // WHEN
+    ThermostatManager* instance = nullptr;
+    ASSERT_NO_THROW(instance = new ThermostatManager(relay, temp, timer));
+    ASSERT_NE(instance, nullptr);
+    ASSERT_NO_THROW(delete instance);
 }
