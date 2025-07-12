abe02928ee32477b37cd698a578f212c12c678a8
fedddot <idanrommm@gmail.com>
2025-07-12 05:45:41 +0000

    vendor implemented

--- vendor/thermostat_vendor/include/thermostat_vendor.hpp
+++ vendor/thermostat_vendor/include/thermostat_vendor.hpp
@@ -1,6 +1,8 @@
 #ifndef THERMOSTAT_VENDOR_HPP
 #define THERMOSTAT_VENDOR_HPP
 
+#include <optional>
+
 #include "thermostat_api_request.hpp"
 #include "thermostat_api_response.hpp"
 #include "thermostat_manager.hpp"
@@ -14,16 +16,48 @@ namespace vendor {
 
 		ThermostatVendor(
 			const ThermostatManagerInstance& thermostat_manager
-		);
+		): m_thermostat_manager(thermostat_manager) {
+
+        }
 		ThermostatVendor(const ThermostatVendor& other) = default;
 		ThermostatVendor& operator=(const ThermostatVendor&) = delete;
 
-		ThermostatVendorApiResponse run_api_request(const ThermostatVendorApiRequest& request) override;
+		ThermostatVendorApiResponse run_api_request(const ThermostatVendorApiRequest& request) override {
+            switch (request.type()) {
+            case ThermostatVendorApiRequest::RequestType::START:
+                return run_start_request(request);
+            case ThermostatVendorApiRequest::RequestType::STOP:
+                return run_stop_request(request);
+            default:
+                throw std::runtime_error("unsupported request type");
+            }
+        }
 	private:
 		ThermostatManagerInstance m_thermostat_manager;
 		
-		ThermostatVendorApiResponse run_start_request(const ThermostatVendorApiRequest& request);
-		ThermostatVendorApiResponse run_stop_request(const ThermostatVendorApiRequest& request);
+		ThermostatVendorApiResponse run_start_request(const ThermostatVendorApiRequest& request) {
+            if (!request.temperature() || !request.time_resolution_ms()) {
+                return ThermostatVendorApiResponse(
+                    ThermostatVendorApiResponse::Result::FAILURE,
+                    "invalid start request: temperature or time resolution is missing"
+                );
+            }
+            m_thermostat_manager.get().start(
+                *request.temperature(),
+                *request.time_resolution_ms()
+            );
+            return ThermostatVendorApiResponse(
+                ThermostatVendorApiResponse::Result::SUCCESS,
+                std::nullopt
+            );
+        }
+		ThermostatVendorApiResponse run_stop_request(const ThermostatVendorApiRequest& request) {
+            m_thermostat_manager.get().stop();
+            return ThermostatVendorApiResponse(
+                ThermostatVendorApiResponse::Result::SUCCESS,
+                std::nullopt
+            );
+        }
     };
 }
 
