71b1af5af8cd20987fbadfae70cc4c20f2d85414
fedddot <idanrommm@gmail.com>
2025-07-14 20:01:07 +0000

    proto added

--- app/thermostat_app/tests/src/ut_thermostat_host_builder.cpp
+++ app/thermostat_app/tests/src/ut_thermostat_host_builder.cpp
@@ -4,54 +4,48 @@
 
 #include "gtest/gtest.h"
 
-#include "axes_controller.hpp"
 #include "ipc_instance.hpp"
 #include "manager_instance.hpp"
-#include "movement_host_builder.hpp"
-#include "test_ipc_data_reader.hpp"
-#include "test_ipc_data_writer.hpp"
+#include "thermostat_host_builder.hpp"
 
 using namespace ipc;
 using namespace manager;
 using namespace host;
 
-using AxesConfig = std::string;
-using RawData = std::vector<char>;
-
-TEST(ut_movement_host_builder, ctor_dtor_sanity) {
+TEST(ut_thermostat_host_builder, ctor_dtor_sanity) {
 	// WHEN
-	MovementHostBuilder<AxesConfig, RawData> *instance = nullptr;
+	ThermostatHostBuilder<AxesConfig, RawData> *instance = nullptr;
 
 	// THEN
 	ASSERT_NO_THROW(
 		(
-			instance = new MovementHostBuilder<AxesConfig, RawData>()
+			instance = new ThermostatHostBuilder<AxesConfig, RawData>()
 		)
 	);
 	ASSERT_NO_THROW(delete instance);
 	instance = nullptr;
 }
 
-TEST(ut_movement_host_builder, build_sanity) {
+TEST(ut_thermostat_host_builder, build_sanity) {
 	// GIVEN
-	const auto raw_data_reader = MovementHostBuilder<AxesConfig, RawData>::RawDataReaderInstance(
+	const auto raw_data_reader = ThermostatHostBuilder<AxesConfig, RawData>::RawDataReaderInstance(
 		new TestIpcDataReader<RawData>(
 			[]() -> std::optional<ipc::Instance<RawData>> {
 				throw std::runtime_error("NOT IMPLEMENTED");
 			}
 		)
 	);
-	const auto raw_data_writer = MovementHostBuilder<AxesConfig, RawData>::RawDataWriterInstance(
+	const auto raw_data_writer = ThermostatHostBuilder<AxesConfig, RawData>::RawDataWriterInstance(
 		new TestIpcDataWriter<RawData>(
 			[](const RawData&) {
 				throw std::runtime_error("NOT IMPLEMENTED");
 			}
 		)
 	);
-	const auto api_request_parser = [](const RawData&) -> ipc::Instance<MovementHostBuilder<AxesConfig, RawData>::ApiRequest> {
+	const auto api_request_parser = [](const RawData&) -> ipc::Instance<ThermostatHostBuilder<AxesConfig, RawData>::ApiRequest> {
 		throw std::runtime_error("NOT IMPLEMENTED");
 	};
-	const auto api_response_serializer = [](const MovementHostBuilder<AxesConfig, RawData>::ApiResponse&) -> RawData {
+	const auto api_response_serializer = [](const ThermostatHostBuilder<AxesConfig, RawData>::ApiResponse&) -> RawData {
 		throw std::runtime_error("NOT IMPLEMENTED");
 	};
 	const auto axes_controller_ctor = [](const AxesConfig&) -> manager::Instance<AxesController> {
@@ -59,7 +53,7 @@ TEST(ut_movement_host_builder, build_sanity) {
 	};
 	
 	// WHEN
-	MovementHostBuilder<AxesConfig, RawData> instance;
+	ThermostatHostBuilder<AxesConfig, RawData> instance;
 	instance
 		.set_raw_data_reader(raw_data_reader)
 		.set_api_request_parser(api_request_parser)
