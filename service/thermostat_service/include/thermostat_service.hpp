d2ab3a9e5e91b9d3d3bd3ca8871e07c4b83ec025
fedddot <idanrommm@gmail.com>
2025-07-24 16:11:28 +0000

    thermostat_service reimplemented

--- service/thermostat_service/include/thermostat_service.hpp
+++ service/thermostat_service/include/thermostat_service.hpp
@@ -2,19 +2,20 @@
 #define THERMOSTAT_SERVICE_HPP
 
 #include <optional>
+#include <stdexcept>
 
 #include "thermostat_api_request.hpp"
 #include "thermostat_api_response.hpp"
-#include "thermostat_manager_controller.hpp"
+#include "thermostat_controller.hpp"
 #include "service.hpp"
 
 namespace service {
     class ThermostatService: public Service<ThermostatServiceApiRequest, ThermostatServiceApiResponse> {
     public:
-		ThermostatService(
-			manager::ThermostatController *controller_ptr
-		): m_thermostat_manager(controller_ptr) {
-            
+		ThermostatService(ThermostatController *controller_ptr): m_controller_ptr(controller_ptr), m_regulation_task_guard(std::nullopt) {
+            if (!m_controller_ptr) {
+                throw std::runtime_error("invalid controller ptr received");
+            }
         }
 		ThermostatService(const ThermostatService& other) = default;
 		ThermostatService& operator=(const ThermostatService&) = delete;
@@ -35,43 +36,20 @@ namespace service {
             }
         }
 	private:
-		manager::ThermostatManager m_thermostat_manager;
         ThermostatController *m_controller_ptr;
-		std::optional<Instance<ThermostatController::TaskGuard>> m_regulation_task_guard;
-		
-		ThermostatServiceApiResponse run_start_request(const ThermostatServiceApiRequest& request) {
-            if (!request.temperature() || !request.time_resolution_ms()) {
+		std::optional<ThermostatController::TaskGuard *> m_regulation_task_guard;
+
+        ThermostatServiceApiResponse run_start_request(const ThermostatServiceApiRequest& request) {
+            if (!request.temperature().has_value() || !request.time_resolution_ms().has_value()) {
                 return ThermostatServiceApiResponse(
                     ThermostatServiceApiResponse::Result::FAILURE,
                     "invalid start request: temperature or time resolution is missing"
                 );
             }
-            m_thermostat_manager.start(
-                *request.temperature(),
-                *request.time_resolution_ms()
-            );
-            return ThermostatServiceApiResponse(
-                ThermostatServiceApiResponse::Result::SUCCESS,
-                std::nullopt
-            );
-        }
-		ThermostatServiceApiResponse run_stop_request(const ThermostatServiceApiRequest& request) {
-            m_thermostat_manager.stop();
-            return ThermostatServiceApiResponse(
-                ThermostatServiceApiResponse::Result::SUCCESS,
-                std::nullopt
-            );
-        }
-        ThermostatServiceApiResponse run_get_temp_request(const ThermostatServiceApiRequest& request) {
-            return ThermostatServiceApiResponse(
-                ThermostatServiceApiResponse::Result::SUCCESS,
-                std::nullopt,
-                m_thermostat_manager.get_current_temperature()
-            );
-        }
-        		void start(const double temp, const std::size_t time_resolution_ms) {
-			if (m_regulation_task_guard) {
-				m_regulation_task_guard->get().unschedule();
+            const auto temp = request.temperature().value();
+			const auto time_resolution_ms = request.time_resolution_ms().value();
+            if (m_regulation_task_guard) {
+				m_regulation_task_guard.value()->unschedule();
 				m_regulation_task_guard = std::nullopt;
 			}
 			m_regulation_task_guard = m_controller_ptr->schedule_task(
@@ -84,17 +62,30 @@ namespace service {
 				},
 				time_resolution_ms
 			);
-		}
-		void stop() {
-			if (m_regulation_task_guard) {
-				m_regulation_task_guard->get().unschedule();
+            return ThermostatServiceApiResponse(
+                ThermostatServiceApiResponse::Result::SUCCESS,
+                std::nullopt
+            );
+        }
+		ThermostatServiceApiResponse run_stop_request(const ThermostatServiceApiRequest& request) {
+			if (m_regulation_task_guard.has_value()) {
+				m_regulation_task_guard.value()->unschedule();
+                delete m_regulation_task_guard.value();
 				m_regulation_task_guard = std::nullopt;
 			}
 			m_controller_ptr->set_relay_state(false);
-		}
-		double get_current_temperature() const {
-			return m_controller_ptr->read_temperature();
-		}
+            return ThermostatServiceApiResponse(
+                ThermostatServiceApiResponse::Result::SUCCESS,
+                std::nullopt
+            );
+        }
+        ThermostatServiceApiResponse run_get_temp_request(const ThermostatServiceApiRequest& request) {
+            return ThermostatServiceApiResponse(
+                ThermostatServiceApiResponse::Result::SUCCESS,
+                std::nullopt,
+                m_controller_ptr->read_temperature()
+            );
+        }
     };
 }
 
