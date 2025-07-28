07583072c078a99ca30f96ac96b5d746d78f5456
fedddot <idanrommm@gmail.com>
2025-07-28 16:07:02 +0000

    implemented

--- tools/include/ring_data_buffer.hpp
+++ tools/include/ring_data_buffer.hpp
@@ -1,20 +1,57 @@
 #ifndef	RING_DATA_BUFFER_HPP
 #define	RING_DATA_BUFFER_HPP
 
+#include <array>
 #include <cstddef>
-#include <cstdint>
+#include <stdexcept>
 
 #include "data_buffer.hpp"
 
 namespace tools {
-	class RingDataBuffer: public DataBuffer {
+	template <typename T, std::size_t N>
+	class RingDataBuffer: public DataBuffer<T> {
 	public:
-		RingDataBuffer() = default;
-		RingDataBuffer(const RingDataBuffer&) = default;
-		RingDataBuffer& operator=(const RingDataBuffer&) = default;
+		RingDataBuffer(): m_data{}, m_read_index(0), m_write_index(0), m_size(0) {
+
+		}
+		RingDataBuffer(const RingDataBuffer&) = delete;
+		RingDataBuffer& operator=(const RingDataBuffer&) = delete;
 		
-		void read_bytes(std::uint8_t *destination, const std::size_t size) override;
-		void write_bytes(const std::uint8_t *source, const std::size_t size) override;
+		void push_back(const T& elem) override {
+			if (m_size == N) {
+				throw std::overflow_error("buffer overflow");
+			}
+			m_data[m_write_index] = elem;
+			m_write_index = next_index(m_write_index);
+			++m_size;
+		}
+		T pop_first() override {
+			if (m_size == 0) {
+				throw std::runtime_error("buffer is empty");
+			}
+			const auto elem = m_data[m_read_index];
+			m_read_index = next_index(m_read_index);
+			--m_size;
+			return elem;
+		}
+		const T& get(const std::size_t index) const override {
+			if (index >= m_size) {
+				throw std::out_of_range("index out of range");
+			}
+			return m_data[(m_read_index + index) % N];
+		}
+		std::size_t size() const override {
+			return m_size;
+		}
+	private:
+		std::array<T, N> m_data;
+		std::size_t m_read_index;
+		std::size_t m_write_index;
+		std::size_t m_size;
+
+		static std::size_t next_index(const std::size_t& index) {
+			return (index + 1) % N;
+		}
 	};
 }
 
