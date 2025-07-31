dd4ec6d02fa1fb2ab9c0270bee3aec26802f8d19
fedddot <idanrommm@gmail.com>
2025-07-31 15:20:32 +0000

    updated

--- ipc/include/ipc_data_reader.hpp
+++ ipc/include/ipc_data_reader.hpp
@@ -1,16 +1,15 @@
 #ifndef	IPC_DATA_READER_HPP
 #define	IPC_DATA_READER_HPP
 
-#include <optional>
-
-#include "ipc_instance.hpp"
-
 namespace ipc {
-	template <typename IpcData>
-	class IpcDataReader {
+	template <typename Signature>
+	class DataReader;
+
+	template <typename Result, typename... Args>
+	class DataReader<Result(Args...)> {
 	public:
-		virtual ~IpcDataReader() noexcept = default;
-		virtual std::optional<Instance<IpcData>> read() = 0;
+		virtual ~DataReader() noexcept = default;
+		virtual Result read(Args...) const = 0;
 	};
 }
 
