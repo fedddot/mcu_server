f4e2ff5158fa720bf9bde68d4a5676709b6bd4d6
fedddot <idanrommm@gmail.com>
2025-07-31 16:31:13 +0000

    implemented

--- ipc/package_reader/include/package_reader.hpp
+++ ipc/package_reader/include/package_reader.hpp
@@ -2,89 +2,58 @@
 #define	RAW_DATA_PACKAGE_READER_HPP
 
 #include <cstddef>
+#include <cstdint>
 #include <functional>
 #include <optional>
 #include <stdexcept>
+#include <vector>
 
-#include "ipc_data.hpp"
+#include "data_buffer.hpp"
 #include "ipc_data_reader.hpp"
-#include "raw_data_package_descriptor.hpp"
 
 namespace ipc {
-	class RawDataPackageReader: public IpcDataReader<RawData> {
+	template <std::size_t HSIZE>
+	class PackageReader: public DataReader<std::optional<std::vector<std::uint8_t>>(void)> {
 	public:
-		using PackageSizeParser = std::function<std::size_t(const RawDataPackageDescriptor&, const RawData&)>;
-		RawDataPackageReader(
-			RawData *buffer,
-			const RawDataPackageDescriptor& descriptor,
-			const PackageSizeParser& size_parser
-		);
-		RawDataPackageReader(const RawDataPackageReader&) = default;
-		RawDataPackageReader& operator=(const RawDataPackageReader&) = default;
-		std::optional<Instance<RawData>> read() override;
-	private:
-		RawData *m_buffer;
-		RawDataPackageDescriptor m_descriptor;
-		PackageSizeParser m_size_parser;
-		
-		bool validate_preamble();
-	};
-
-	inline RawDataPackageReader::RawDataPackageReader(
-		RawData *buffer,
-		const RawDataPackageDescriptor& descriptor,
-		const PackageSizeParser& size_parser
-	): m_buffer(buffer), m_descriptor(descriptor), m_size_parser(size_parser) {
-		if (!m_buffer) {
-			throw std::invalid_argument("invalid buffer ptr received");
-		}
-		if (!m_size_parser) {
-			throw std::invalid_argument("invalid size parser received");
-		}
-	}
-
-	inline std::optional<Instance<RawData>> RawDataPackageReader::read() {
-		const auto preamble_size = m_descriptor.preamble().size();
-		const auto encoded_size_len = m_descriptor.encoded_size_length();
-		if (!validate_preamble()) {
-			return std::nullopt;
-		}
-		const auto package_size = m_size_parser(
-			m_descriptor,
-			RawData(
-				m_buffer->begin() + preamble_size,
-				m_buffer->begin() + preamble_size + encoded_size_len
-			)
-		);
-		if (m_buffer->size() < preamble_size + encoded_size_len + package_size) {
-			return std::nullopt;
-		}
-		const auto package_data = RawData(
-			m_buffer->begin() + preamble_size + encoded_size_len,
-			m_buffer->begin() + preamble_size + encoded_size_len + package_size
-		);
-		m_buffer->erase(
-			m_buffer->begin(),
-			m_buffer->begin() + preamble_size + encoded_size_len + package_size
-		);
-		return Instance<RawData>(new RawData(package_data));
-	}
-
-	inline bool RawDataPackageReader::validate_preamble() {
-		const auto preamble_size = m_descriptor.preamble().size();
-		const auto encoded_size_len = m_descriptor.encoded_size_length();
-		while (m_buffer->size() >= preamble_size + encoded_size_len) {
-			const auto incoming_preamble = RawData(
-				m_buffer->begin(),
-				m_buffer->begin() + preamble_size
-			);
-			if (m_descriptor.preamble() == incoming_preamble) {
-				return true;
+		using SizeRetriever = std::function<std::size_t(const DataBuffer<std::uint8_t>&)>;
+		PackageReader(
+			DataBuffer<std::uint8_t> *buffer_ptr,
+			const SizeRetriever& size_retriever
+		): m_buffer_ptr(buffer_ptr), m_size_retriever(size_retriever) {
+			if (!m_buffer_ptr || !m_size_retriever) {
+				throw std::invalid_argument("invalid args in package reader received");
 			}
-			m_buffer->erase(m_buffer->begin());
 		}
-		return false;
-	}
+		PackageReader(const PackageReader&) = default;
+		PackageReader& operator=(const PackageReader&) = default;
+		std::optional<std::vector<std::uint8_t>> read() const override {
+			if (m_buffer_ptr->size() < HSIZE) {
+				return std::nullopt;
+			}
+			try {
+				const auto package_size = m_size_retriever(*m_buffer_ptr);
+				if (m_buffer_ptr->size() < package_size + HSIZE) {
+					return std::nullopt;
+				}
+				auto bytes_to_discard = HSIZE;
+				while (bytes_to_discard) {
+					m_buffer_ptr->pop_first();
+					--bytes_to_discard;
+				}
+				std::vector<std::uint8_t> package_data(package_size, 0);
+				for (std::size_t i = 0; i < package_size; ++i) {
+					package_data[i] = m_buffer_ptr->pop_first();
+				}
+				return package_data;
+			} catch (...) {
+				m_buffer_ptr->clear();
+				return std::nullopt;
+			}
+		}
+	private:
+		DataBuffer<std::uint8_t> *m_buffer_ptr;
+		SizeRetriever m_size_retriever;
+	};
 }
 
 #endif // RAW_DATA_PACKAGE_READER_HPP
