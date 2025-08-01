1650b92c68bad2a2739ba1b268c4562cc9cc7214
fedddot <idanrommm@gmail.com>
2025-08-01 18:16:40 +0000

    types fixed

--- ipc/tools/src/package_utils.cpp
+++ ipc/tools/src/package_utils.cpp
@@ -10,7 +10,7 @@ using namespace ipc;
 
 std::vector<std::uint8_t> ipc::serialize_package_size(const std::size_t& package_size, const std::size_t& encoded_size_length) {
 	auto encoded_size = std::vector<std::uint8_t>(encoded_size_length, 0UL);
-	for (auto i = 0; i < encoded_size_length; ++i) {
+	for (std::size_t i = 0; i < encoded_size_length; ++i) {
 		const auto less_sign_byte = static_cast<char>((package_size >> CHAR_BIT * i) & 0xFF);
 		encoded_size[i] = less_sign_byte;
 	}
@@ -23,7 +23,7 @@ std::size_t ipc::parse_package_size(const std::vector<std::uint8_t>& package_siz
 		throw std::invalid_argument("invalid raw data size");
 	}
 	auto decoded_size = std::size_t(0UL);
-	for (auto i = 0; i < package_size_data_size; ++i) {
+	for (std::size_t i = 0; i < package_size_data_size; ++i) {
 		decoded_size <<= CHAR_BIT;
 		decoded_size |= static_cast<std::size_t>(package_size_data[package_size_data_size - 1 - i]);
 	}
