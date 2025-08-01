a13ed9a47558ad4ed966879edacc6c684efc27e9
fedddot <idanrommm@gmail.com>
2025-08-01 17:19:17 +0000

    test updated

--- app/thermostat_app/tests/src/ut_thermostat_app.cpp
+++ app/thermostat_app/tests/src/ut_thermostat_app.cpp
@@ -1,14 +1,16 @@
 #include <cstdint>
 #include <stdexcept>
+#include <vector>
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include <vector>
 
 #include "ipc_queue.hpp"
 #include "ring_queue.hpp"
 #include "thermostat_app.hpp"
 #include "thermostat_controller.hpp"
+#include "package_utils.hpp"
+#include "proto_thermostat_api_request_serializer.hpp"
 
 using namespace host;
 using namespace service;
@@ -29,10 +31,11 @@ public:
 const auto HSIZE = std::size_t(2UL);
 using ApiRequest = ThermostatApp<HSIZE>::ApiRequest;
 using ApiResponse = ThermostatApp<HSIZE>::ApiResponse;
+static void write_request_to_queue(ipc::IpcQueue<std::uint8_t> *queue, const ApiRequest& request);
 
 TEST(ut_thermostat_app, run_once_sanity) {
 	// GIVEN
-	const auto test_request = ApiRequest()
+	const auto test_request = ApiRequest(ApiRequest::RequestType::GET_TEMP);
 	const auto queue_size = 10UL;
 	auto queue = ipc::RingQueue<std::uint8_t, queue_size>();
 	const auto package_size_retriever = [](const ipc::IpcQueue<std::uint8_t>&) -> std::size_t {
@@ -62,7 +65,20 @@ TEST(ut_thermostat_app, run_once_sanity) {
 		&queue,
 		&controller
 	);
+	write_request_to_queue(&queue, test_request);
 
 	// THEN
 	ASSERT_NO_THROW(instance.run_once());
+}
+
+inline void write_request_to_queue(ipc::IpcQueue<std::uint8_t> *queue, const ApiRequest& request) {
+	const auto serializer = ipc::ApiRequestSerializer();
+	auto raw_data = serializer(request);
+	const auto encoded_size = ipc::serialize_package_size(raw_data.size(), HSIZE);
+	for (const auto& byte : encoded_size) {
+		queue->enqueue(byte);
+	}
+	for (const auto& byte : raw_data) {
+		queue->enqueue(byte);
+	}
 }
