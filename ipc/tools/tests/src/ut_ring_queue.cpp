9f69325279863cf076e441dc57974ccaf8e69efe
fedddot <idanrommm@gmail.com>
2025-08-01 04:08:38 +0000

    test updated

--- ipc/tools/tests/src/ut_ring_queue.cpp
+++ ipc/tools/tests/src/ut_ring_queue.cpp
@@ -17,18 +17,18 @@ TEST(ut_ring_queue, sanity) {
 	ASSERT_EQ(instance.size(), 0UL);
 	ASSERT_EQ(instance.capacity(), buff_size);
 	for (const auto& elem : test_data) {
-		instance.push_back(elem);
+		instance.enqueue(elem);
 		const auto elem_index = &elem - test_data;
 		ASSERT_EQ(instance.size(), elem_index + 1);
-		ASSERT_EQ(instance.get(elem_index), elem);
+		ASSERT_EQ(instance.inspect(elem_index), elem);
 	}
 	ASSERT_EQ(instance.size(), buff_size);
-	ASSERT_THROW(instance.push_back('d'), std::overflow_error);
+	ASSERT_THROW(instance.enqueue('d'), std::overflow_error);
 	for (const auto& elem : test_data) {
-		const auto popped_elem = instance.pop_first();
+		const auto popped_elem = instance.dequeue();
 		ASSERT_EQ(popped_elem, elem);
 		ASSERT_EQ(instance.size(), buff_size - (&elem - test_data) - 1);
 	}
 	ASSERT_EQ(instance.size(), 0UL);
-	ASSERT_THROW(instance.pop_first(), std::runtime_error);
+	ASSERT_THROW(instance.dequeue(), std::runtime_error);
 }
