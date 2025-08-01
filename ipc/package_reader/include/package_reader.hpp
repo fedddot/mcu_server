1caa756ca9b24c53dc7a507ae335f94dc50e2f7b
fedddot <idanrommm@gmail.com>
2025-08-01 04:14:16 +0000

    package reader updated

--- ipc/package_reader/include/package_reader.hpp
+++ ipc/package_reader/include/package_reader.hpp
@@ -8,50 +8,50 @@
 #include <stdexcept>
 #include <vector>
 
-#include "data_buffer.hpp"
+#include "ipc_queue.hpp"
 #include "ipc_data_reader.hpp"
 
 namespace ipc {
 	template <std::size_t HSIZE>
 	class PackageReader: public DataReader<std::optional<std::vector<std::uint8_t>>(void)> {
 	public:
-		using SizeRetriever = std::function<std::size_t(const DataBuffer<std::uint8_t>&)>;
+		using SizeRetriever = std::function<std::size_t(const IpcQueue<std::uint8_t>&)>;
 		PackageReader(
-			DataBuffer<std::uint8_t> *buffer_ptr,
+			IpcQueue<std::uint8_t> *queue_ptr,
 			const SizeRetriever& size_retriever
-		): m_buffer_ptr(buffer_ptr), m_size_retriever(size_retriever) {
-			if (!m_buffer_ptr || !m_size_retriever) {
+		): m_queue_ptr(queue_ptr), m_size_retriever(size_retriever) {
+			if (!m_queue_ptr || !m_size_retriever) {
 				throw std::invalid_argument("invalid args in package reader received");
 			}
 		}
 		PackageReader(const PackageReader&) = default;
 		PackageReader& operator=(const PackageReader&) = default;
 		std::optional<std::vector<std::uint8_t>> read() const override {
-			if (m_buffer_ptr->size() < HSIZE) {
+			if (m_queue_ptr->size() < HSIZE) {
 				return std::nullopt;
 			}
 			try {
-				const auto package_size = m_size_retriever(*m_buffer_ptr);
-				if (m_buffer_ptr->size() < package_size + HSIZE) {
+				const auto package_size = m_size_retriever(*m_queue_ptr);
+				if (m_queue_ptr->size() < package_size + HSIZE) {
 					return std::nullopt;
 				}
 				auto bytes_to_discard = HSIZE;
 				while (bytes_to_discard) {
-					m_buffer_ptr->pop_first();
+					m_queue_ptr->dequeue();
 					--bytes_to_discard;
 				}
 				std::vector<std::uint8_t> package_data(package_size, 0);
 				for (std::size_t i = 0; i < package_size; ++i) {
-					package_data[i] = m_buffer_ptr->pop_first();
+					package_data[i] = m_queue_ptr->dequeue();
 				}
 				return package_data;
 			} catch (...) {
-				m_buffer_ptr->clear();
+				m_queue_ptr->clear();
 				return std::nullopt;
 			}
 		}
 	private:
-		DataBuffer<std::uint8_t> *m_buffer_ptr;
+		IpcQueue<std::uint8_t> *m_queue_ptr;
 		SizeRetriever m_size_retriever;
 	};
 }
