cdde2f0a523b4ed9a5abefa53c84a5a19151ecb5
fedddot <idanrommm@gmail.com>
2025-08-01 14:36:53 +0000

    updated

--- host/tests/src/ut_host.cpp
+++ host/tests/src/ut_host.cpp
@@ -55,32 +55,36 @@ TEST(ut_host, ctor_dtor_sanity) {
 	instance = nullptr;
 }
 
-// TEST(ut_host, run_once_sanity) {
-// 	// GIVEN
-// 	const auto test_api_request = ApiRequest("test_request");
-// 	const auto test_api_response = ApiResponse(12);
-// 	const auto ipc_data_reader = [test_api_request]()-> std::optional<ApiRequest> {
-// 		return ApiRequest(test_api_request);
-// 	};
-// 	const auto ipc_data_writer = [test_api_response](const ApiResponse& response){
-// 		ASSERT_EQ(test_api_response, response);
-// 	};
-// 	auto service = TestService<ApiRequest, ApiResponse>(
-// 		[test_api_response](const ApiRequest&) -> ApiResponse {
-// 			return ApiResponse(test_api_response);
-// 		}
-// 	);
+TEST(ut_host, run_once_sanity) {
+	// GIVEN
+	const auto test_api_request = ApiRequest("test_request");
+	const auto test_api_response = ApiResponse(12);
+	const auto ipc_data_reader = TestDataReader<std::optional<ApiRequest>(void)>(
+		[test_api_request](void) -> std::optional<ApiRequest> {
+			return test_api_request;
+		}
+	);
+	const auto ipc_data_writer = TestIpcDataWriter<ApiResponse>(
+		[test_api_response](const ApiResponse& response){
+			ASSERT_EQ(test_api_response, response);
+		}
+	);
+	auto service = TestService<ApiRequest, ApiResponse>(
+		[test_api_response](const ApiRequest&) -> ApiResponse {
+			return ApiResponse(test_api_response);
+		}
+	);
 
-// 	// WHEN:
-// 	TestHost instance(
-// 		ipc_data_reader,
-// 		ipc_data_writer,
-// 		[](const std::exception& e) -> ApiResponse {
-// 			throw std::runtime_error("NOT IMPLEMENTED");
-// 		},
-// 		&service
-// 	);
+	// WHEN:
+	TestHost instance(
+		&ipc_data_reader,
+		&ipc_data_writer,
+		[](const std::exception& e) -> ApiResponse {
+			throw std::runtime_error("NOT IMPLEMENTED");
+		},
+		&service
+	);
 
-// 	// THEN:
-// 	ASSERT_NO_THROW(instance.run_once());
-// }
+	// THEN:
+	ASSERT_NO_THROW(instance.run_once());
+}
