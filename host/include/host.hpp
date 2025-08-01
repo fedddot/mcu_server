f0d1e89b3f3e949c952085e52e6db902e0eaf911
fedddot <idanrommm@gmail.com>
2025-08-01 06:28:47 +0000

    host updated

--- host/include/host.hpp
+++ host/include/host.hpp
@@ -6,20 +6,23 @@
 #include <optional>
 #include <stdexcept>
 
+#include "api_request_reader.hpp"
+#include "ipc_data_reader.hpp"
+#include "ipc_data_writer.hpp"
 #include "service.hpp"
 
 namespace host {
 	template <typename ApiRequest, typename ApiResponse>
 	class Host {
 	public:
-		using ApiRequestReader = std::function<std::optional<ApiRequest>(void)>;
-		using ApiResponseWriter = std::function<void(const ApiResponse&)>;
+		using ApiRequestReader = ipc::DataReader<ApiRequest(void)>;
+		using ApiResponseWriter = ipc::DataWriter<ApiResponse>;
 		using FailureReporter = std::function<ApiResponse(const std::exception&)>;
 		using ServiceInstance = service::Service<ApiRequest, ApiResponse>;
 
 		Host(
-			const ApiRequestReader& api_request_reader,
-			const ApiResponseWriter& api_response_writer,
+			const ApiRequestReader *api_request_reader_ptr,
+			const ApiResponseWriter *api_response_writer_ptr,
 			const FailureReporter& failure_reporter,
 			ServiceInstance *service_ptr
 		);
@@ -29,20 +32,20 @@ namespace host {
 		
 		void run_once();
 	private:
-		ApiRequestReader m_api_request_reader;
-		ApiResponseWriter m_api_response_writer;
+		ApiRequestReader *m_api_request_reader_ptr;
+		ApiResponseWriter *m_api_response_writer_ptr;
 		FailureReporter m_failure_reporter;
 		ServiceInstance *m_service_ptr;
 	};
 
 	template <typename ApiRequest, typename ApiResponse>
 	inline Host<ApiRequest, ApiResponse>::Host(
-		const ApiRequestReader& api_request_reader,
-		const ApiResponseWriter& api_response_writer,
+		const ApiRequestReader *api_request_reader_ptr,
+		const ApiResponseWriter *api_response_writer_ptr,
 		const FailureReporter& failure_reporter,
 		ServiceInstance *service_ptr
-	): m_api_request_reader(api_request_reader), m_api_response_writer(api_response_writer), m_failure_reporter(failure_reporter), m_service_ptr(service_ptr) {
-		if (!m_api_request_reader || !m_api_response_writer || !m_failure_reporter || !m_service_ptr) {
+	): m_api_request_reader_ptr(api_request_reader_ptr), m_api_response_writer_ptr(api_response_writer_ptr), m_failure_reporter(failure_reporter), m_service_ptr(service_ptr) {
+		if (!m_api_request_reader_ptr || !m_api_response_writer_ptr || !m_failure_reporter || !m_service_ptr) {
 			throw std::invalid_argument("invalid args received in Host ctor");
 		}
 	}
@@ -50,15 +53,15 @@ namespace host {
 	template <typename ApiRequest, typename ApiResponse>
 	inline void Host<ApiRequest, ApiResponse>::run_once() {
 		try {
-			const auto request = m_api_request_reader();
+			const auto request = m_api_request_reader_ptr();
 			if (!request.has_value()) {
 				return;
 			}
 			const auto response = m_service_ptr->run_api_request(request.value());
-			m_api_response_writer(response);
+			m_api_response_writer_ptr(response);
 		} catch (const std::exception& e) {
 			const auto response = m_failure_reporter(e);
-			m_api_response_writer(response);
+			m_api_response_writer_ptr(response);
 		}
 	}
 }
