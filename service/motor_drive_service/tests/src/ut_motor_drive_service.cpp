16c93aa69338626bdf66d42fcd8cd20b7636fc72
fedddot <idanrommm@gmail.com>
2025-10-07 08:52:14 +0000

    PwmController mock added

--- service/motor_drive_service/tests/src/ut_motor_drive_service.cpp
+++ service/motor_drive_service/tests/src/ut_motor_drive_service.cpp
@@ -1,19 +1,29 @@
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #include "motor_drive_service.hpp"
 #include "motor_drive_service_api_request.hpp"
 #include "motor_drive_service_api_response.hpp"
+#include "pwm_controller.hpp"
 
 using namespace service;
+using namespace provider;
 
 using TestStatus = std::string;
+
+class MockPwmController : public PwmController {
+public:
+    MOCK_METHOD(void, start, (const double), (override));
+	MOCK_METHOD(void, stop, (), (override));
+};
 
 TEST(ut_motor_drive_service, run_api_request_sanity) {
 	// GIVEN
 	const MotorDriveServiceApiRequest test_request(MotorDriveServiceApiRequest::Type::STATUS);
 
 	// WHEN:
-	MotorDriveService<TestStatus> service;
+	auto pwm_ctrl = testing::NiceMock<MockPwmController>();
+	MotorDriveService<TestStatus> service(&pwm_ctrl);
 	
 	// THEN:
 	const auto response = service.run_api_request(test_request);
