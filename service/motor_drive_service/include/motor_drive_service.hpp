289d75d7ca224f3ea811b05a84baa546a36ea498
fedddot <idanrommm@gmail.com>
2025-10-07 08:48:05 +0000

    updated

--- service/motor_drive_service/include/motor_drive_service.hpp
+++ service/motor_drive_service/include/motor_drive_service.hpp
@@ -12,12 +12,20 @@ namespace service {
 	template <typename Status>
 	class MotorDriveService: public Service<MotorDriveServiceApiRequest, MotorDriveServiceApiResponse<Status>> {
 	public:
-		MotorDriveService() = default;
+		MotorDriveService(
+			provider::PwmController *pwm_controller
+		): m_pwm_controller(pwm_controller) {
+			if (!m_pwm_controller) {
+				throw std::invalid_argument("MotorDriveService ctor: invalid arguments");
+			}
+		}
 		MotorDriveService(const MotorDriveService&) = delete;
 		MotorDriveService& operator=(const MotorDriveService&) = delete;
 		MotorDriveServiceApiResponse<Status> run_api_request(const MotorDriveServiceApiRequest& request) override {
 			throw std::runtime_error("NOT IMPLEMENTED");
 		}
+	private:
+		provider::PwmController *m_pwm_controller;
 	};
 }
 
