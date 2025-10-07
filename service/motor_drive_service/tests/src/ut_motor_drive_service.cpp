f28f5e6c3d1c8a5ba7a8ea02ae11fe4b4d413af7
fedddot <idanrommm@gmail.com>
2025-10-07 09:31:02 +0000

    start test passed

--- service/motor_drive_service/tests/src/ut_motor_drive_service.cpp
+++ service/motor_drive_service/tests/src/ut_motor_drive_service.cpp
@@ -1,9 +1,12 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include <map>
 
+#include "digital_output_controller.hpp"
 #include "motor_drive_service.hpp"
 #include "motor_drive_service_api_request.hpp"
 #include "motor_drive_service_api_response.hpp"
+#include "motor_drive_service_types.hpp"
 #include "pwm_controller.hpp"
 
 using namespace service;
@@ -15,15 +18,30 @@ class MockPwmController : public PwmController {
 public:
     MOCK_METHOD(void, start, (const double), (override));
 	MOCK_METHOD(void, stop, (), (override));
+	MOCK_METHOD(bool, running, (), (const override));
+};
+
+class MockDigitalOutputController : public DigitalOutputController {
+public:
+    MOCK_METHOD(void, set, (const DigitalOutputController::State&), (override));
+	MOCK_METHOD(DigitalOutputController::State, get, (), (const override));
 };
 
 TEST(ut_motor_drive_service, run_api_request_sanity) {
 	// GIVEN
-	const MotorDriveServiceApiRequest test_request(MotorDriveServiceApiRequest::Type::STATUS);
+	const auto max_speed = (double)(15.0);
+	const auto test_speed = (double)(12.0);
+	const auto test_dir = Direction::CCW;
+	const auto dir_states_mapping = std::map<Direction, DigitalOutputController::State> {
+		{ Direction::CW, DigitalOutputController::State::LOW },
+		{ Direction::CCW, DigitalOutputController::State::HIGH },
+	};
+	const MotorDriveServiceApiRequest test_request(MotorDriveServiceApiRequest::Type::START, test_speed, test_dir);
 
 	// WHEN:
 	auto pwm_ctrl = testing::NiceMock<MockPwmController>();
-	MotorDriveService<TestStatus> service(&pwm_ctrl);
+	auto dir_ctrl = testing::NiceMock<MockDigitalOutputController>();
+	MotorDriveService<TestStatus> service(&pwm_ctrl, &dir_ctrl, max_speed, dir_states_mapping);
 	
 	// THEN:
 	const auto response = service.run_api_request(test_request);
